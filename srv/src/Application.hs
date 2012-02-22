1d20c1327e6ad79ad649f1c81183bb8e6036ef8a
Max Taldykin <jorpic@gmail.com>
2012-02-22 16:45:57 +0400

    serving search of contractors in redis DB

--- srv/src/Application.hs
+++ srv/src/Application.hs
@@ -4,8 +4,14 @@ module Application where
 
 import Data.Lens.Template
 import Snap.Snaplet
+import Snap.Snaplet.RedisDB
+import Database.Redis
+
 
 data App = App
+  {_redisDB :: Snaplet RedisDB
+  }
+  
 type AppHandler = Handler App App
 makeLens ''App
 
--- srv/src/Site.hs
+++ srv/src/Site.hs
@@ -9,14 +9,66 @@ import           Control.Applicative
 import           Control.Monad.Trans
 import           Control.Monad.State
 import           Data.ByteString (ByteString)
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Lazy as L
 import qualified Data.Text as T
+import           Data.Lens.Common
+import qualified Data.Map as M
+import           Data.List (foldl', intersect)
+import           Data.Maybe (catMaybes)
+import           Data.Either (rights)
 import           Snap.Core
 import           Snap.Snaplet
 import           Snap.Util.FileServe
-
+import           Snap.Snaplet.RedisDB
+import           Data.Aeson as A
+import           Data.Aeson.Types as A
+import           Data.Attoparsec hiding (take)
+import           Database.Redis
 import           Application
 
 
+
+searchContractors = do
+  ps <- rqParams <$> getRequest
+  let s0 = head $ (M.!) ps "sSearch_0"
+  let s2 = head $ (M.!) ps "sSearch_2"
+  let s4 = head $ (M.!) ps "sSearch_4"
+
+  vals <- runRedisDB redisDB $ do
+    let getMatch (k,s)
+          = if B.null s
+          then return Nothing
+          else Just <$> (keys $ B.concat [k,"*",s,"*"])
+    matchingKeys  <- rights . catMaybes
+        <$> mapM getMatch
+          [("partner:companyName:",s0)
+          ,("partner:contactPerson:",s2)
+          ,("partner:contactPhone:",s4)
+          ]
+    ks <- if null matchingKeys
+          then fromRight <$> keys "partner:*"
+          else return $ foldl' intersect [] matchingKeys
+
+    (catMaybes . fromRight) <$> (mget $ take 100 ks)
+
+  let res = catMaybes $ flip map
+          (catMaybes $ map (A.decode . L.fromChunks .(:[])) vals)
+          $ A.parseMaybe (\o -> mapM (o .:)
+            ["companyName","cityRu"
+            ,"contactPerson","phones"
+            ,"comment"]) :: [[A.Value]]
+
+  let response = A.encode $ object
+        ["iTotalRecords" .= length res
+        ,"iTotalDisplayRecords" .= length res
+        ,"aaData" .= toJSON res
+        ]
+  modifyResponse $ setContentType "application/json"
+  writeLBS response
+
+
+fromRight = either (const []) id
 
 
 ------------------------------------------------------------------------------
@@ -24,6 +76,7 @@ import           Application
 routes :: [(ByteString, Handler App App ())]
 routes = [("/s", serveDirectory "resources")
          ,("/",  serveFile "resources/index.html")
+         ,("/api/search_contractors", method GET searchContractors)
          ]
 
 ------------------------------------------------------------------------------
@@ -31,6 +84,7 @@ routes = [("/s", serveDirectory "resources")
 app :: SnapletInit App App
 app = makeSnaplet "app" "" Nothing $ do
     addRoutes routes
-    return $ App
+    r <- nestSnaplet "" redisDB $ redisDBInit defaultConnectInfo
+    return $ App r
 
 
