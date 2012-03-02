e25d2d07552944ea3f49166fb51b11a5d6232edc
Dmitry Dzhus <dima@dzhus.org>
2012-03-02 21:31:12 +0400

    Start breaking everything (import from metaforms)

--- srv/src/Application.hs
+++ srv/src/Application.hs
@@ -1,18 +1,149 @@
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
 
-module Application where
+{-|
 
+Build application from Redson snaplet and utility handlers.
+
+-}
+
+module Application (appInit)
+
+where
+
+import Prelude hiding (lookup)
+
+import Control.Monad.IO.Class
+import Data.Functor
+import Data.Maybe
+
+import Data.ByteString (ByteString)
+import qualified Data.ByteString.UTF8 as BU (toString)
+import Data.Configurator
 import Data.Lens.Template
+import Data.Time.Clock
+
+import Snap.Core
 import Snap.Snaplet
-import Snap.Snaplet.RedisDB
-import Database.Redis
+import Snap.Snaplet.Auth hiding (session)
+import Snap.Snaplet.Auth.Backends.JsonFile
+import Snap.Snaplet.Heist
+import Snap.Snaplet.Session
+import Snap.Snaplet.Session.Backends.CookieSession
+import Snap.Util.FileServe
+
+import Web.ClientSession
+
+import Snap.Snaplet.Redson
 
 
+------------------------------------------------------------------------------
+-- | Application snaplet state type: Redson, Heist.
 data App = App
-  {_redisDB :: Snaplet RedisDB
-  }
-  
+    { _heist :: Snaplet (Heist App)
+    , _redson :: Snaplet (Redson App)
+    , _session :: Snaplet SessionManager
+    , _auth :: Snaplet (AuthManager App)
+    , _startTime :: UTCTime
+    }
+
 type AppHandler = Handler App App
+
 makeLens ''App
 
+instance HasHeist App where
+    heistLens = subSnaplet heist
 
+
+------------------------------------------------------------------------------
+-- | Render empty form for model.
+emptyForm :: AppHandler ()
+emptyForm = ifTop $ render "index"
+
+
+------------------------------------------------------------------------------
+-- | Redirect using 303 See Other to login form.
+--
+-- Used after unsuccessful access/login attempt or logout.
+redirectToLogin :: MonadSnap m => m a
+redirectToLogin = redirect' "/login/" 303
+
+
+------------------------------------------------------------------------------
+-- | If user is not logged in, redirect to login page, pass to
+-- handler otherwise.
+authOrLogin :: AppHandler () -> AppHandler ()
+authOrLogin h = requireUser auth redirectToLogin h
+
+
+------------------------------------------------------------------------------
+-- | Render empty login form.
+loginForm :: AppHandler ()
+loginForm = do
+  serveFile $ "resources/templates/login.html"
+
+
+------------------------------------------------------------------------------
+-- | Login user.
+doLogin :: AppHandler ()
+doLogin = ifTop $ do
+  l <- fromMaybe "" <$> getParam "login"
+  p <- fromMaybe "" <$> getParam "password"
+  r <- maybe False (const True) <$> getParam "remember"
+  res <- with auth $ loginByUsername l (ClearText p) r
+  case res of
+    Left err -> redirectToLogin
+    Right user -> redirect "/"
+
+
+------------------------------------------------------------------------------
+-- | The application's routes.
+routes :: [(ByteString, AppHandler ())]
+routes = [ ("/", method GET $ authOrLogin empty)
+         , ("/login/", method GET loginForm)
+         , ("/login/", method POST doLogin)
+         , ("/logout/", with auth $ logout >> redirectToLogin)
+         , ("/s/", serveDirectory "resources/static")
+         ]
+
+
+sessionTimeout :: Maybe Int
+sessionTimeout = Nothing
+
+
+------------------------------------------------------------------------------
+-- | The application initializer.
+appInit :: SnapletInit App App
+appInit = makeSnaplet "app" "Forms application" Nothing $ do
+  r <- nestSnaplet "_" redson $ redsonInit auth
+
+  h <- nestSnaplet "heist" heist $ heistInit "resources/templates"
+  addAuthSplices auth
+
+  cfg <- getSnapletUserConfig
+  sesKey <- liftIO $
+            lookupDefault "resources/private/client_session_key.aes"
+                          cfg "session-key"
+  rmbKey <- liftIO $
+            lookupDefault "resources/private/site_key.txt"
+                          cfg "remember-key"
+  rmbPer <- liftIO $
+            lookupDefault 14 
+                          cfg "remember-period"
+  authDb <- liftIO $
+            lookupDefault "resources/private/users.json"
+                          cfg "user-db"
+
+  s <- nestSnaplet "session" session $
+       initCookieSessionManager sesKey "_session" sessionTimeout
+  a <- nestSnaplet "auth" auth $
+       initJsonFileAuthManager
+       defAuthSettings{ asSiteKey = rmbKey
+                      , asRememberPeriod = Just (rmbPer * 24 * 60 * 60)}
+                               session authDb
+
+  sTime <- liftIO getCurrentTime
+
+  addRoutes routes
+
+  return $ App h r s a sTime
--- srv/src/Main.hs
+++ srv/src/Main.hs
@@ -13,7 +13,7 @@ import           Snap.Core
 
 import           System.IO
 
-import           Site
+import           Application
 
 #ifdef DEVELOPMENT
 import           Snap.Loader.Devel
@@ -22,43 +22,6 @@ import           Snap.Loader.Prod
 #endif
 
 
-{-|
-
-This is the entry point for this web server application.  It supports
-easily switching between interpreting source and running statically
-compiled code.
-
-In either mode, the generated program should be run from the root of
-the project tree.  When it is run, it locates its templates, static
-content, and source files in development mode, relative to the current
-working directory.
-
-When compiled with the development flag, only changes to the
-libraries, your cabal file, or this file should require a recompile to
-be picked up.  Everything else is interpreted at runtime.  There are a
-few consequences of this.
-
-First, this is much slower.  Running the interpreter takes a
-significant chunk of time (a couple tenths of a second on the author's
-machine, at this time), regardless of the simplicity of the loaded
-code.  In order to recompile and re-load server state as infrequently
-as possible, the source directories are watched for updates, as are
-any extra directories specified below.
-
-Second, the generated server binary is MUCH larger, since it links in
-the GHC API (via the hint library).
-
-Third, and the reason you would ever want to actually compile with
-development mode, is that it enables a faster development cycle. You
-can simply edit a file, save your changes, and hit reload to see your
-changes reflected immediately.
-
-When this is compiled without the development flag, all the actions
-are statically compiled in.  This results in faster execution, a
-smaller binary size, and having to recompile the server for any code
-change.
-
--}
 main :: IO ()
 main = do
     -- depending on the version of loadSnapTH in scope, this either
@@ -104,6 +67,6 @@ getConf = commandLineConfig defaultConfig
 -- sophisticated code might.
 getActions :: Config Snap () -> IO (Snap (), IO ())
 getActions _ = do
-    (msgs, site, cleanup) <- runSnaplet app
+    (msgs, site, cleanup) <- runSnaplet appInit
     hPutStrLn stderr $ T.unpack msgs
     return (site, cleanup)
