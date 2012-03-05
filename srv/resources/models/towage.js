0e45cb7653fd9490911294e29636ca6e74e1ff2e
Dmitry Dzhus <dima@dzhus.org>
2012-03-05 17:12:22 +0400

    Towage model.

---
+++ srv/resources/models/towage.js
@@ -0,0 +1,63 @@
+{
+    "title": "Эвакуация",
+    "canCreate": true,
+    "canRead": true,
+    "canUpdate": true,
+    "canDelete": true,
+    "fields": [
+        {
+            "name": "towerType",
+            "label": "Тип эвакуатора",
+            "canWrite": true,
+            "canRead": true,
+            "index": true
+        },
+        {
+            "name": "towType",
+            "label": "Вид эвакуации",
+            "canWrite": true,
+            "canRead": true
+        },
+        {
+            "name": "caseAddress",
+            "label": "Адрес места поломки",
+            "canWrite": true,
+            "canRead": true
+        },
+        {
+            "name": "towDealer",
+            "label": "Дилер",
+            "canWrite": true,
+            "canRead": true
+        },
+        {
+            "name": "towAddress",
+            "label": "Адрес доставки",
+            "canWrite": true,
+            "canRead": true
+        },
+        {
+            "name": "towContractor",
+            "label": "Подрядчик",
+            "canWrite": true,
+            "canRead": true
+        },
+        {
+            "name": "wheelsUnblocked",
+            "label": "Колёса не заблокированы",
+            "type": "checkbox",
+            "default": true,
+            "canWrite": true,
+            "canRead": true
+        },
+        {
+            "name": "manipulatorPossible",
+            "label": "Есть место для манипулятора",
+            "type": "checkbox",
+            "default": true,
+            "canWrite": true,
+            "canRead": true
+        }
+        
+    ]
+}
