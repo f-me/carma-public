1bd1d8494d4e2a01d59e65c28e83b88e6cea1b10
Dmitry Dzhus <dima@dzhus.org>
2012-03-08 19:51:22 +0400

    Car model

---
+++ srv/resources/models/car.js
@@ -0,0 +1,64 @@
+{
+    "name": "car",
+    "title": "Информация о машине",
+    "canCreate": true,
+    "canRead": true,
+    "canUpdate": true,
+    "canDelete": true,
+    "fields": [
+        {
+            "name": "vin",
+            "label": "VIN",
+            "canWrite": true,
+            "canRead": true
+        },
+        {
+            "name": "model",
+            "label": "Марка и модель",
+            "canWrite": true,
+            "canRead": true
+        },
+        {
+            "name": "plateNum",
+            "label": "Регистрационный номер автомобиля",
+            "canWrite": true,
+            "canRead": true
+        },
+        {
+            "name": "color",
+            "label": "Цвет",
+            "canWrite": true,
+            "canRead": true
+        },
+        {
+            "name": "transmission",
+            "label": "Коробка передач",
+            "canWrite": true,
+            "canRead": true
+        },
+        {
+            "name": "buyDate",
+            "label": "Дата покупки",
+            "canWrite": true,
+            "canRead": true,
+        },
+        {
+            "name": "mileage",
+            "label": "Текущий пробег",
+            "canWrite": true,
+            "canRead": true
+        },
+        {
+            "name": "checkupDate",
+            "label": "Дата последнего ТО",
+            "canWrite": true,
+            "canRead": true,
+        },
+        {
+            "name": "checkupMileage",
+            "label": "Пробег на последнем ТО",
+            "canWrite": true,
+            "canRead": true
+        }
+    ]
+}
