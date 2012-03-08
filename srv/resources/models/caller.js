cca942fd2e4a8105577099fe6ab6213aa7c8b28b
Dmitry Dzhus <dima@dzhus.org>
2012-03-08 20:17:50 +0400

    Caller model

---
+++ srv/resources/models/caller.js
@@ -0,0 +1,34 @@
+{
+    "name": "person",
+    "title": "Звонящий",
+    "canCreate": true,
+    "canRead": true,
+    "canUpdate": true,
+    "canDelete": true,
+    "fields": [
+        {
+            "name": "callerName",
+            "label": "ФИО звонящего",
+            "canWrite": true,
+            "canRead": true,
+            "index": true,
+            "indexCollate": true
+        },
+        {
+            "name": "ownerName",
+            "label": "ФИО владельца",
+            "canWrite": true,
+            "canRead": true,
+            "index": true,
+            "indexCollate": true
+        },
+        {
+            "name": "phone",
+            "label": "Телефон звонящего",
+            "canWrite": true,
+            "canRead": true,
+            "index": true,
+            "indexCollate": true
+        }
+    ]
+}
