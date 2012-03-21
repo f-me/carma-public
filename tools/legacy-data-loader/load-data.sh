56889dcd91799c96d0561637f6c99d2daab5965c
Dmitry Dzhus <dima@dzhus.org>
2012-03-22 03:12:40 +0400

    Update load-data indices for new naming scheme

--- tools/legacy-data-loader/load-data.sh
+++ tools/legacy-data-loader/load-data.sh
@@ -2,4 +2,4 @@
 echo flushall
 redis-cli flushall
 echo loading data
-./dist/build/hmset-poster/hmset-poster -c $1 -i ownerName -i callDate -i plateNum -i phone -i program && echo OK
+./dist/build/hmset-poster/hmset-poster -c $1 -i caller:ownerName -i callDate -i car:plateNum -i caller:phone -i program && echo OK
