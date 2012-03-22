9d6f48a15bcd5408c49edc067b6cb10a1d1ad7c0
Dmitry Dzhus <dima@dzhus.org>
2012-03-23 02:39:32 +0400

    Some VIN model

---
+++ srv/resources/models/vin.js
@@ -0,0 +1,75 @@
+{
+    "name": "vin",
+    "title": "VIN",
+    "canCreate": false,
+    "canRead": true,
+    "canUpdate": false,
+    "canDelete": false,
+    "fields": [
+        {
+            "name": "program",
+            "canWrite": true,
+            "canRead": true,
+        },
+        {
+            "name": "make",
+            "canWrite": true,
+            "canRead": true
+        },
+        {
+            "name":"model",
+            "canWrite": true,
+            "canRead": true
+        },
+        {
+            "name":"arcModelCode",
+            "canWrite": true,
+            "canRead": true
+        },
+        {
+            "name":"fddsId",
+            "canWrite": true,
+            "canRead": true
+        },
+        {
+            "name":"vin",
+            "canWrite": true,
+            "canRead": true
+        },
+        {
+            "name":"dealerCode",
+            "canWrite": true,
+            "canRead": true
+        },
+        {
+            "name":"dealerNameEn",
+            "canWrite": true,
+            "canRead": true
+        },
+        {
+            "name":"validFrom",
+            "canWrite": true,
+            "canRead": true
+        },
+        {
+            "name":"validUntil",
+            "canWrite": true,
+            "canRead": true
+        },
+        {
+            "name":"plateNumber",
+            "canWrite": true,
+            "canRead": true
+        },
+        {
+            "name":"programRegistartionDate",
+            "canWrite": true,
+            "canRead": true
+        },
+        {
+            "name":"mileageTO",
+            "canWrite": true,
+            "canRead": true
+        }
+    ]
+}
