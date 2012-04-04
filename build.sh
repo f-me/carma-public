18ee9ece8262473d60fe1a35a8b4301e79c7c80f
max taldykin <jorpic@gmail.com>
2012-04-05 01:08:19 +0400

    build scripts now checks if pull&rebuild is required

--- build.sh
+++ build.sh
@@ -1,44 +1,90 @@
 #!/bin/bash
 
-if [[ ! -d deps ]]; then
-  mkdir deps
-  cd deps
-  git clone https://github.com/snapframework/snap.git
-  git clone https://github.com/snapframework/snap-core.git
-  git clone https://github.com/snapframework/snap-server.git
-  git clone https://github.com/snapframework/heist.git
-  git clone https://github.com/dzhus/snaplet-redis.git
-  git clone https://github.com/f-me/snaplet-redson.git
-  cd ..
+CARMA_DIR=`pwd`
+DEPS_DIR=$CARMA_DIR/deps
+
+REPOS=("https://github.com/snapframework/snap.git"
+       "https://github.com/snapframework/snap-core.git"
+       "https://github.com/snapframework/snap-server.git"
+       "https://github.com/snapframework/heist.git"
+       "https://github.com/dzhus/snaplet-redis.git"
+       "https://github.com/f-me/snaplet-redson.git"
+       "https://github.com/f-me/avaya-aes.git")
+
+
+if [[ ! -d "$DEPS_DIR" ]]; then
+  mkdir "$DEPS_DIR"
 fi
 
-cd srv
-cabal-dev clean
-cabal-dev add-source ../deps/snap
-cabal-dev add-source ../deps/snap-core
-cabal-dev add-source ../deps/snap-server
-cabal-dev add-source ../deps/heist
-cabal-dev add-source ../deps/snaplet-redis
-cabal-dev add-source ../deps/snaplet-redson
+cd "$DEPS_DIR"
+repos_to_rebuild=()
 
-cabal-dev install --reinstall snap snap-core snap-server heist
-cabal-dev install --reinstall snaplet-redis
-cabal-dev install --reinstall snaplet-redson
+for repo in "${REPOS[@]}" ; do
+  rPath=${repo##*/}
+  rPath="$DEPS_DIR/${rPath%*.git}"
+  if [[ ! -d "$rPath" ]]; then
+    echo "Cloning dependent repo: $repo to $rPath"
+    git clone $repo
+    repos_to_rebuild=("${repos_to_rebuild[@]}" "$rPath")
+  fi
+done
+
+
+function CheckRepo {
+  repo_path=$1
+  cd $repo_path
+  echo "Checking $repo_path"
+
+  git status -uno | grep "modified" > /dev/null
+  if [[ $? -eq o ]]; then
+    echo "    .. modified locally"
+    repos_to_rebuild=("${repos_to_rebuild[@]}" "`pwd`")
+    return
+  fi
+
+  git remote -v update 2>&1 | grep "up to date.*master" > /dev/null
+  if [[ $? -eq 0 ]]; then
+    echo "    .. up-to date"
+    return
+  fi
+
+  git status -uno | grep "can be fast-forwarded" > /dev/null
+  if [[ $? -eq 0 ]]; then
+    echo "    .. pulling changes from remote"
+    if git pull; then
+      echo "    .. successfully updated"
+      repos_to_rebuild=("${repos_to_rebuild[@]}" "`pwd`")
+      return
+    fi
+  fi
+
+  echo "    .. something went wrong"
+  exit 1
+}
+
+
+
+
+for repo in "${REPOS[@]}"; do
+  rPath=${repo##*/}
+  rPath="$DEPS_DIR/${rPath%*.git}"
+  CheckRepo $rPath
+done
+
+
+cd "$CARMA_DIR/srv"
+for repo in "${repos_to_rebuild[@]}"; do
+  cabal-dev add-source $repo
+done
+
+for repo in "${repos_to_rebuild[@]}"; do
+  package_name=${repo##*/}
+  cabal-dev install --reinstall $package_name
+  if [[ ! $? -eq 0 ]]; then
+    exit 1
+  fi
+done
+
 cabal-dev configure && cabal-dev build
 
-cd ../tools/legacy-data-loader
-
-cabal-dev clean
-cabal-dev add-source ../../deps/snap
-cabal-dev add-source ../../deps/snap-core
-cabal-dev add-source ../../deps/snap-server
-cabal-dev add-source ../../deps/heist
-cabal-dev add-source ../../deps/snaplet-redis
-cabal-dev add-source ../../deps/snaplet-redson
-
-cabal-dev install --reinstall snap-core heist
-cabal-dev install --reinstall snaplet-redis
-cabal-dev install --reinstall snaplet-redson
-cabal-dev configure && cabal-dev build
-
-cd $OLDPWD
+cd $CARMA_DIR
