e56a1f21aa49e578e2c2a95cd4242c148b9b2288
Dmitry Dzhus <dima@dzhus.org>
2012-04-25 02:13:26 +0400

    Number checks

---
+++ srv/src/Snap/Snaplet/Candibober/Number.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE Rank2Types #-}
+
+-- | Generic number checkers.
+
+module Snap.Snaplet.Candibober.String
+    ( -- * Checker combinators
+      compareNumber
+    )
+
+where
+
+import qualified Data.ByteString as B
+
+import Snap.Snaplet.Candibober.Types
+
+import Snap.Snaplet.Redson.Snapless.Metamodel
+
+compareNumber :: Monad m =>
+                 SlotName
+              -> FieldName
+              -> Integer
+              -- ^ Compare integer stored in field value to this number
+              -> CheckBuilderMonad m Checker
+compareNumber slot field ord arg =
+    return $ scopedChecker slot field $
+               \fv -> case B.readInteger fv of
+                        Just (fn, _) -> return $ ord == compare fn arg
+                        Nothing -> error $
+                                   "Could not read integer from field" ++
+                                  (B.unpack fv)
