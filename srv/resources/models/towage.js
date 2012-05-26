d2b570b34f79dadbf098312b886af27f25390190
n013e <a@formalmethods.ru>
2012-05-26 11:41:20 +0400

    Update srv/resources/models/towage.js

--- srv/resources/models/towage.js
+++ srv/resources/models/towage.js
@@ -441,23 +441,6 @@
             }
         },
         {
-            "name": "clientSatisfied",
-            "canRead": [
-                "front",
-                "back",
-                "head",
-                "parguy"
-            ],
-            "canWrite": [
-                "back",
-                "head"
-            ],
-            "type": "checkbox",
-            "meta": {
-                "label": "Клиент доволен"
-            }
-        },
-        {
             "name": "billingDate",
             "canRead": [
                 "head",
@@ -767,6 +750,23 @@
             "meta": {
                 "label": "Статус услуги",
                 "dictionaryName": "ServiceStatuses"
+            }
+        },
+        {
+            "name": "clientSatisfied",
+            "canRead": [
+                "front",
+                "back",
+                "head",
+                "parguy"
+            ],
+            "canWrite": [
+                "back",
+                "head"
+            ],
+            "type": "checkbox",
+            "meta": {
+                "label": "Клиент доволен"
             }
         }
     ]
