6cc244dea37199b4a15c91f3d1cce05d832a9d0c
n013e <a@formalmethods.ru>
2012-05-26 11:45:46 +0400

    Update srv/resources/models/deliverParts.js

--- srv/resources/models/deliverParts.js
+++ srv/resources/models/deliverParts.js
@@ -366,23 +366,6 @@
             }
         },
         {
-            "name": "clientSatisfied",
-            "canRead": [
-                "front",
-                "back",
-                "head",
-                "parguy"
-            ],
-            "canWrite": [
-                "back",
-                "head"
-            ],
-            "type": "checkbox",
-            "meta": {
-                "label": "Клиент доволен"
-            }
-        },
-        {
             "name": "billingDate",
             "canRead": [
                 "head",
@@ -476,6 +459,23 @@
             "meta": {
                 "label": "Статус услуги",
                 "dictionaryName": "ServiceStatuses"
+            }
+        },
+        {
+            "name": "clientSatisfied",
+            "canRead": [
+                "front",
+                "back",
+                "head",
+                "parguy"
+            ],
+            "canWrite": [
+                "back",
+                "head"
+            ],
+            "type": "checkbox",
+            "meta": {
+                "label": "Клиент доволен"
             }
         }
     ]
