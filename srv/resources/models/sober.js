a4ed09e5f76169444fea7d52bbcf1b91da1c5325
n013e <a@formalmethods.ru>
2012-05-26 11:43:41 +0400

    Update srv/resources/models/sober.js

--- srv/resources/models/sober.js
+++ srv/resources/models/sober.js
@@ -407,23 +407,6 @@
             }
         },
         {
-            "name": "clientSatisfied",
-            "canRead": [
-                "front",
-                "back",
-                "head",
-                "parguy"
-            ],
-            "canWrite": [
-                "back",
-                "head"
-            ],
-            "type": "checkbox",
-            "meta": {
-                "label": "Клиент доволен"
-            }
-        },
-        {
             "name": "billingDate",
             "canRead": [
                 "head",
@@ -553,6 +536,23 @@
             "meta": {
                 "label": "Статус услуги",
                 "dictionaryName": "ServiceStatuses"
+            }
+        },
+        {
+            "name": "clientSatisfied",
+            "canRead": [
+                "front",
+                "back",
+                "head",
+                "parguy"
+            ],
+            "canWrite": [
+                "back",
+                "head"
+            ],
+            "type": "checkbox",
+            "meta": {
+                "label": "Клиент доволен"
             }
         }
     ]
