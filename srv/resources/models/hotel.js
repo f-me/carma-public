504d0f7e7545f666e2334b8fe43fe34d0c86add6
n013e <a@formalmethods.ru>
2012-05-26 11:45:19 +0400

    Update srv/resources/models/hotel.js

--- srv/resources/models/hotel.js
+++ srv/resources/models/hotel.js
@@ -380,23 +380,6 @@
             }
         },
         {
-            "name": "clientSatisfied",
-            "canRead": [
-                "front",
-                "back",
-                "head",
-                "parguy"
-            ],
-            "canWrite": [
-                "back",
-                "head"
-            ],
-            "type": "checkbox",
-            "meta": {
-                "label": "Клиент доволен"
-            }
-        },
-        {
             "name": "billingDate",
             "canRead": [
                 "head",
@@ -507,6 +490,23 @@
             "meta": {
                 "label": "Статус услуги",
                 "dictionaryName": "ServiceStatuses"
+            }
+        },
+        {
+            "name": "clientSatisfied",
+            "canRead": [
+                "front",
+                "back",
+                "head",
+                "parguy"
+            ],
+            "canWrite": [
+                "back",
+                "head"
+            ],
+            "type": "checkbox",
+            "meta": {
+                "label": "Клиент доволен"
             }
         }
     ]
