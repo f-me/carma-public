881fac4b3582a6e2a7b568ded8793e1d2c246e78
Элемир <elemir90@mail.ru>
2012-06-06 22:50:57 +0400

    Update avaya-aes snaplet for avaya-aes library

--- srv/src/Snap/Snaplet/AvayaAES.hs
+++ srv/src/Snap/Snaplet/AvayaAES.hs
@@ -17,16 +17,18 @@ import Data.Lens.Template
 import Snap.Core
 import Snap.Snaplet
 import Snap.Snaplet.Auth
+import Network.Avaya 
+import Network.Avaya.Action
 
-import Network.Avaya as A
 import qualified Data.Aeson as AE
 import qualified Data.ByteString as B
 import qualified Data.ByteString.UTF8 as BU
 import qualified Data.HashMap.Lazy as H
+import qualified Data.Text as T
 
 data Avayaplet b = Avayaplet
     { auth :: Lens b (Snaplet (AuthManager b))
-    , _conf :: Conf
+    , _avayaState :: Maybe AvayaState
     }
 makeLens ''Avayaplet
 
@@ -34,37 +36,56 @@ withAuth :: (MonadState (Avayaplet b1) (m b1 v), MonadSnaplet m) =>
             m b1 (AuthManager b1) b -> m b1 v b
 withAuth = (gets auth >>=) . flip withTop
 
+
 callHandler :: Handler b (Avayaplet b) ()
 callHandler = do
-    Just user <- withAuth currentUser
-    let meta = userMeta user
-    let Just (AE.String extension) = H.lookup "avayaExt" meta
-    let Just (AE.String password) = H.lookup "avayaPass" meta
     n <- fromMaybe "" <$> getParam "phone"
-    conf <- gets _conf 
-    let conf' = conf { cExtension = extension, cPassword = password }
-    liftIO $ print conf'
-    liftIO $ print number 
-    liftIO $ A.runClient conf'
-                         print (call $ BU.toString number)
+    Right () <- runAvayaActionWithLens avayaState (call $ BU.toString n)
     return ()
 
+
+startSession :: Handler b (Avayaplet b) ()
+startSession = do
+    conf <- loadConfig
+    Right ast <- liftIO $ startAvaya conf
+    app <- get
+    put $ setL avayaState (Just ast) app 
+    return ()
+  where
+    getEP :: Handler b (Avayaplet b) (T.Text, T.Text)
+    getEP = do
+      Just user <- withAuth currentUser
+      let meta = userMeta user
+      let Just (AE.String exten) = H.lookup "avayaExt" meta
+      let Just (AE.String pass) = H.lookup "avayaPass" meta
+      return (exten, pass)
+
+    loadConfig :: Handler b (Avayaplet b) AvayaConfig
+    loadConfig = do
+      cfg <- getSnapletUserConfig
+      let fromConfig def par = liftIO $ lookupDefault def cfg par
+      host <- fromConfig "127.0.0.1" "host"
+      port <- fromConfig 4721 "port"
+      username <- fromConfig "user" "user"
+      password <- fromConfig "password" "password"
+      delay <- fromConfig "5" "delay"
+      version <- fromConfig "4.2" "version"
+      duration <- fromConfig "180" "duration"
+      callServerIp <- fromConfig "" "callServerIp"
+      (exten, pass) <- getEP
+      return $ AvayaConfig host (fromInteger port) username password delay
+                           version duration callServerIp exten pass
+
+
 routes :: [(B.ByteString, Handler b (Avayaplet b) ())]
-routes = [ ("/call", method POST callHandler) ]
+routes = [ ("/call", method POST callHandler)
+         , ("/start-session", method POST startSession)
+         ]
+
 
 avayaAESInit :: Lens b (Snaplet (AuthManager b)) -> SnapletInit b (Avayaplet b)
 avayaAESInit topAuth =
     makeSnaplet "avaya-aes" "Avaya AES snaplet." Nothing $ do
-      cfg <- getSnapletUserConfig
-      host <- liftIO $ lookupDefault "127.0.0.1" cfg "host"
-      port <- liftIO $ lookupDefault 4721 cfg "port"
-      user <- liftIO $ lookupDefault "user" cfg "user"
-      password <- liftIO $ lookupDefault "password" cfg "password"
-      delay <- liftIO $ lookupDefault "5" cfg "delay"
-      version <- liftIO $ lookupDefault "4.2" cfg "version"
-      duration <- liftIO $ lookupDefault "180" cfg "duration"
-      callServerIp <- liftIO $ lookupDefault "" cfg "callServerIp"
       addRoutes routes
-      return $ Avayaplet topAuth $ 
-                 Conf host (fromInteger port) user password delay
-                      version duration callServerIp "" ""
+      return $ Avayaplet topAuth Nothing
+
