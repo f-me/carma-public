a82dcdb12ccffc6ba31d5279c1b4c6e75b0be43a
DaryaBA <darya@formalmethods.ru>
2012-06-08 16:38:02 +0400

    Update master

--- srv/resources/models/information.js
+++ srv/resources/models/information.js
@@ -378,7 +378,8 @@
             "type": "dictionary",
             "meta": {
                 "dictionaryName": "FalseStatuses",
-                "label": "Ложный вызов"
+                "label": "Ложный вызов",
+                "infoText": "falsecall"
             }
         },
         {
