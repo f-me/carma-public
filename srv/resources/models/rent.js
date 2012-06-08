a03a3b88fc868d4094f7765e670aa10253163c33
DaryaBA <darya@formalmethods.ru>
2012-06-08 16:38:42 +0400

    Update master

--- srv/resources/models/rent.js
+++ srv/resources/models/rent.js
@@ -466,7 +466,8 @@
             "type": "dictionary",
             "meta": {
                 "dictionaryName": "FalseStatuses",
-                "label": "Ложный вызов"
+                "label": "Ложный вызов",
+                "infoText": "falsecall"
             }
         },
         {
