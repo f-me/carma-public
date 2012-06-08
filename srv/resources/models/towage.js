e3d5f85826c0c2d93fa985edabc12469f1b7d345
DaryaBA <darya@formalmethods.ru>
2012-06-08 16:40:12 +0400

    Update master

--- srv/resources/models/towage.js
+++ srv/resources/models/towage.js
@@ -493,7 +493,8 @@
             "type": "dictionary",
             "meta": {
                 "dictionaryName": "FalseStatuses",
-                "label": "Ложный вызов"
+                "label": "Ложный вызов",
+                "infoText": "falsecall"
             }
         },
         {
