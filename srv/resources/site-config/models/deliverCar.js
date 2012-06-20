baf0d8403f7b009c264a7d42d6842d63dd9ef14b
n013e <a@formalmethods.ru>
2012-06-20 09:36:40 +0400

    removed reading permissions for financial fields

--- srv/resources/site-config/models/deliverCar.js
+++ srv/resources/site-config/models/deliverCar.js
@@ -88,7 +88,7 @@
         },
         {
             "name": "payment",
-            "canRead": [ "partner", "front", "back", "head", "parguy" ],
+            "canRead": [ "front", "back", "head", "parguy" ],
             "canWrite": [ "front", "back", "head", "parguy" ],
             "groupName": "payment"
         },
@@ -103,7 +103,7 @@
         },
         {
             "name": "expectedCost",
-            "canRead": [ "partner", "front", "back", "head" ],
+            "canRead": [ "front", "back", "head" ],
             "canWrite": [ "front", "back", "head" ],
             "meta": {
                 "label": "Ожидаемая стоимость",
@@ -119,7 +119,7 @@
         },
         {
             "name": "overcosted",
-            "canRead": [ "partner", "front", "back", "head", "parguy" ],
+            "canRead": [ "front", "back", "head", "parguy" ],
             "type": "checkbox",
             "meta": {
                 "label": "Стоимость превышена?"
