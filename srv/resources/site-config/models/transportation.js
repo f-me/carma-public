b7994a9b8f383c6d2577fc128cc13cc1a640ba4d
n013e <a@formalmethods.ru>
2012-06-21 23:43:09 +0400

    Update no-redson

--- srv/resources/site-config/models/transportation.js
+++ srv/resources/site-config/models/transportation.js
@@ -6,7 +6,7 @@
     "canUpdate": true,
     "canDelete": true,
     "applications": [
-    	{
+        {
             "targets": [
                 "caseAddress_address"
             ],
@@ -14,164 +14,306 @@
                 "label": "Адрес кейса"
             }
         },
-	{
+        {
             "targets": [
                 "fromToAddress_address"
             ],
             "meta": {
                 "label": "Адрес куда/откуда"
             }
-        },		
-        {
-            "targets": ["caseAddress_address","caseAddress_coords","caseAddress_city","caseAddress_comment"],
-            "canRead": ["partner", "front", "back", "head", "parguy"],
-            "canWrite": ["front", "back", "head"]
-        },     
-        {
-            "targets": ["fromToAddress_address","fromToAddress_coords","fromToAddress_city","fromToAddress_comment"],
-            "canRead": ["partner", "front", "back", "head", "parguy"],
-            "canWrite": ["front", "back", "head"]
         },
         {
             "targets": [
-				"payment_expectedCost"
+                "caseAddress_address",
+                "caseAddress_coords",
+                "caseAddress_city",
+                "caseAddress_comment"
             ],
-            "canRead": [ "front", "back", "head" ],
-            "canWrite": [ "front", "back", "head" ]
+            "canRead": [
+                "partner",
+                "front",
+                "back",
+                "head",
+                "parguy"
+            ],
+            "canWrite": [
+                "front",
+                "back",
+                "head"
+            ]
         },
         {
             "targets": [
-				"payment_partnerCost"
+                "fromToAddress_address",
+                "fromToAddress_coords",
+                "fromToAddress_city",
+                "fromToAddress_comment"
             ],
-            "canRead": [ "back", "head", "parguy" ],
-            "canWrite": [ "back", "head" ]
+            "canRead": [
+                "partner",
+                "front",
+                "back",
+                "head",
+                "parguy"
+            ],
+            "canWrite": [
+                "front",
+                "back",
+                "head"
+            ]
         },
         {
             "targets": [
-				"payment_calculatedCost",
-				"payment_overcosted"
+                "payment_expectedCost"
             ],
-            "canRead": [ "front", "back", "head", "parguy" ]
+            "canRead": [
+                "front",
+                "back",
+                "head"
+            ],
+            "canWrite": [
+                "front",
+                "back",
+                "head"
+            ]
         },
         {
             "targets": [
-				"payment_limitedCost",
+                "payment_partnerCost"
             ],
-            "canRead": [ "back", "head" ]
+            "canRead": [
+                "back",
+                "head",
+                "parguy"
+            ],
+            "canWrite": [
+                "back",
+                "head"
+            ]
         },
         {
             "targets": [
-				"payment_payType",
-				"payment_paidByRUAMC",
-				"payment_paidByClient"
+                "payment_calculatedCost",
+                "payment_overcosted"
             ],
-            "canRead": [ "front", "back", "head", "parguy" ],
-            "canWrite": [ "back", "head", "parguy" ]
-        },		
-        {
-            "targets": [
-				"bill_billNumber",
-				"bill_billingCost",
-				"bill_billingDate"
-            ],
-            "canRead": [ "head", "parguy" ],
-            "canWrite": [ "parguy" ]
-        },		
-        {
-            "targets": [
-				"times_expectedServiceStart"
-            ],
-            "canRead": [ "partner", "front", "back", "head" ],
-            "canWrite": [ "front", "back", "head" ]
+            "canRead": [
+                "front",
+                "back",
+                "head",
+                "parguy"
+            ]
         },
         {
             "targets": [
-				"times_factServiceStart",
-				"times_expectedServiceEnd",
-				"times_factServiceEnd",
-				"times_expectedServiceFinancialClosure",
-				"times_factServiceFinancialClosure",
-				"times_expectedDealerInfo",
-				"times_factDealerInfo",
-				"times_expectedServiceClosure",
-				"times_factServiceClosure",
-				"times_repairEndDate"
+                "payment_limitedCost"
             ],
-            "canRead": [ "back", "head" ],
-            "canWrite": [ "back", "head" ]
+            "canRead": [
+                "back",
+                "head"
+            ]
+        },
+        {
+            "targets": [
+                "payment_payType",
+                "payment_paidByRUAMC",
+                "payment_paidByClient"
+            ],
+            "canRead": [
+                "front",
+                "back",
+                "head",
+                "parguy"
+            ],
+            "canWrite": [
+                "back",
+                "head",
+                "parguy"
+            ]
+        },
+        {
+            "targets": [
+                "bill_billNumber",
+                "bill_billingCost",
+                "bill_billingDate"
+            ],
+            "canRead": [
+                "head",
+                "parguy"
+            ],
+            "canWrite": [
+                "parguy"
+            ]
+        },
+        {
+            "targets": [
+                "times_expectedServiceStart"
+            ],
+            "canRead": [
+                "partner",
+                "front",
+                "back",
+                "head"
+            ],
+            "canWrite": [
+                "front",
+                "back",
+                "head"
+            ]
+        },
+        {
+            "targets": [
+                "times_factServiceStart",
+                "times_expectedServiceEnd",
+                "times_factServiceEnd",
+                "times_expectedServiceFinancialClosure",
+                "times_factServiceFinancialClosure",
+                "times_expectedDealerInfo",
+                "times_factDealerInfo",
+                "times_expectedServiceClosure",
+                "times_factServiceClosure",
+                "times_repairEndDate"
+            ],
+            "canRead": [
+                "back",
+                "head"
+            ],
+            "canWrite": [
+                "back",
+                "head"
+            ]
         }
     ],
     "fields": [
         {
-          "name": "parentId",
-          "canRead": true,
-          "canWrite": true,
-          "meta": {
-            "invisible": true
-          }
+            "name": "parentId",
+            "canRead": true,
+            "canWrite": true,
+            "meta": {
+                "invisible": true
+            }
         },
         {
             "name": "payment",
             "groupName": "payment"
-        },	
+        },
         {
             "name": "times",
             "groupName": "times"
-        },	
-		{
+        },
+        {
             "name": "falseCall",
-            "canRead": ["partner", "front", "back", "head", "parguy"],
-            "canWrite": ["front", "back", "head"],
+            "canRead": [
+                "partner",
+                "front",
+                "back",
+                "head",
+                "parguy"
+            ],
+            "canWrite": [
+                "front",
+                "back",
+                "head"
+            ],
             "type": "dictionary",
             "meta": {
                 "dictionaryName": "FalseStatuses",
                 "label": "Ложный вызов",
                 "infoText": "falsecall"
             }
-        },		
+        },
         {
             "name": "bill",
             "groupName": "bill"
-        },		
-		{
+        },
+        {
             "name": "transportType",
-            "canRead": ["partner", "front", "back", "head", "parguy"],
-            "canWrite": ["front", "back", "head"],      			
+            "canRead": [
+                "partner",
+                "front",
+                "back",
+                "head",
+                "parguy"
+            ],
+            "canWrite": [
+                "front",
+                "back",
+                "head"
+            ],
             "type": "dictionary",
             "meta": {
                 "label": "Тип транспортировки",
                 "dictionaryName": "TransportTypes"
             }
-        },		
+        },
         {
             "name": "fromToAddress",
-            "canRead": ["partner", "front", "back", "head", "parguy"],
-            "canWrite": ["front", "back", "head"],            
+            "canRead": [
+                "partner",
+                "front",
+                "back",
+                "head",
+                "parguy"
+            ],
+            "canWrite": [
+                "front",
+                "back",
+                "head"
+            ],
             "groupName": "address"
         },
         {
             "name": "status",
-            "canRead": [ "partner", "front", "back", "head", "parguy" ],
-            "canWrite": [ "front", "back", "head", "parguy" ],
+            "canRead": [
+                "partner",
+                "front",
+                "back",
+                "head",
+                "parguy"
+            ],
+            "canWrite": [
+                "front",
+                "back",
+                "head",
+                "parguy"
+            ],
             "type": "dictionary",
             "meta": {
                 "label": "Статус услуги",
                 "dictionaryName": "ServiceStatuses"
             }
-        },		
+        },
         {
             "name": "clientSatisfied",
-            "canRead": ["partner", "front", "back", "head", "parguy"],
-            "canWrite": ["back", "head"],
+            "canRead": [
+                "partner",
+                "front",
+                "back",
+                "head",
+                "parguy"
+            ],
+            "canWrite": [
+                "back",
+                "head"
+            ],
             "type": "checkbox",
             "meta": {
                 "label": "Клиент доволен"
             }
-        },	
+        },
         {
             "name": "warrantyCase",
-            "canRead": ["partner", "front", "back", "head", "parguy"],
-            "canWrite": ["back", "head", "parguy"],			
+            "canRead": [
+                "partner",
+                "front",
+                "back",
+                "head",
+                "parguy"
+            ],
+            "canWrite": [
+                "back",
+                "head",
+                "parguy"
+            ],
             "type": "checkbox",
             "meta": {
                 "label": "Гарантийный случай"
