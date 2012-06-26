99b38a2a4e9e60d6c9e51ca2fdd1bd2f0d307543
n013e <a@formalmethods.ru>
2012-06-26 11:45:25 +0400

    added payType

--- srv/resources/site-config/models/hotel.js
+++ srv/resources/site-config/models/hotel.js
@@ -58,7 +58,6 @@
         },
         {
             "targets": [
-                "payment_payType",
                 "payment_paidByRUAMC",
                 "payment_paidByClient"
             ],
@@ -191,6 +190,26 @@
                 "invisible": true
             }
         },
+    	{
+            "name": "payType",
+            "canRead": [
+                "partner",
+                "front",
+                "back",
+                "head",
+                "parguy"
+            ],
+            "canWrite": [
+                "front",
+                "back",
+                "head"
+            ],			
+            "type": "dictionary",
+            "meta": {
+                "dictionaryName": "PaymentTypes",
+                "label": "Тип оплаты"
+            }
+        },        
         {
             "name": "payment",
             "groupName": "payment"
