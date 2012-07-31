a107baaa7ef59fb0bf84ef608f5140a4d1e665df
n013e <a@formalmethods.ru>
2012-07-31 15:12:17 +0400

    added ownername field

--- srv/resources/site-config/models/case.js
+++ srv/resources/site-config/models/case.js
@@ -98,6 +98,7 @@
                 "contact_phone2",
                 "contact_phone3",
                 "contact_phone4",
+                "conact_ownerName",
                 "contact_contactOwner",
                 "contact_ownerEmail",
                 "contact_ownerPhone1",
