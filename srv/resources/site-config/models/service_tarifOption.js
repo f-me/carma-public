7c34dd9339449cd8667c250485d35e6071f7497b
cvb <peter@stadalone.su>
2012-08-30 20:13:37 +0400

    add service_tarifOption model

---
+++ srv/resources/site-config/models/service_tarifOption.js
@@ -0,0 +1,35 @@
+{
+    "name": "service_tarifOption",
+    "title": "Тарифная опция",
+    "canCreate": true,
+    "canRead": true,
+    "canUpdate": true,
+    "canDelete": true,
+    "applications": [
+        {
+            "targets": true,
+            "canWrite": true,
+            "canRead": true
+        }
+    ],
+    "fields": [
+        {
+            "name": "optionName",
+            "meta": {
+                "label": "Название опции"
+            }
+        },
+        {
+            "name": "price1",
+            "meta": {
+                "label": "Стоимость за единицу за нал"
+            }
+        },
+        {
+            "name": "price2",
+            "meta": {
+                "label": "Стоимость за единицу по безналу"
+            }
+        }
+    ]
+}
