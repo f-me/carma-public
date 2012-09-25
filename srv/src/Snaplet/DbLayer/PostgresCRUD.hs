3494e42fc55ff8262e0d83dc6ccacc30e090b68f
Alexandr Ruchkin <voidex@live.com>
2012-09-26 00:17:26 +0400

    Minor fixes

--- srv/src/Snaplet/DbLayer/PostgresCRUD.hs
+++ srv/src/Snaplet/DbLayer/PostgresCRUD.hs
@@ -386,5 +386,5 @@ generateReport ms conds tpl file = scope "generate" $ do
     tz <- liftIO getCurrentTimeZone
     dicts <- scope "dictionaries" . liftIO . loadDictionaries $ "resources/site-config/dictionaries"
     -- TODO: Orderby must not be here!
-    withPG (R.createReport (SM.modelsSyncs ms) (functions tz dicts) conds ["case.callDate"] tpl file)
+    withPG (R.createReport (SM.modelsSyncs ms) (functions tz dicts) conds ["case.callDate", "call.callDate"] tpl file)
     log Debug "Report generated"
