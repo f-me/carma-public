1f414b2b7b18fcdd516b87cdfc75241461cd0df0
DaryaBA <darya@formalmethods.ru>
2012-10-31 18:46:47 +0400

    Update srv/resources/site-config/models/information.js
    edited

--- srv/resources/site-config/models/information.js
+++ srv/resources/site-config/models/information.js
@@ -710,8 +710,9 @@
                 "admin",
                 "programman"
             ],
-            "type": "checkbox",
+            "type": "dictionary",
             "meta": {
+                "dictionaryName": "Satisfaction",
                 "label": "Клиент доволен"
             }
         },
