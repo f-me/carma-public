9fcdfa1dfcd4c0049647b2723285a32e2214a669
DaryaBA <darya@formalmethods.ru>
2012-11-06 20:11:50 +0400

    Update srv/resources/site-config/models/hotel.js
    edited

--- srv/resources/site-config/models/hotel.js
+++ srv/resources/site-config/models/hotel.js
@@ -449,6 +449,7 @@
             "type": "dictionary",
             "meta": {
                 "dictionaryName": "PaymentTypes",
+                "bounded":true,
                 "label": "Тип оплаты"
             }
         },
@@ -491,6 +492,7 @@
             "type": "dictionary",
             "meta": {
                 "dictionaryName": "FalseStatuses",
+                "bounded":true,
                 "label": "Ложный вызов",
                 "infoText": "falsecall"
             }
@@ -700,6 +702,7 @@
             "type": "dictionary",
             "meta": {
                 "label": "Статус услуги",
+                "bounded":true,
                 "dictionaryName": "ServiceStatuses"
             }
         },
