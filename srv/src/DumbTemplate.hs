84c7c925efdece1f397f9ab40ed28e65d5cb17ab
max taldykin <jorpic@gmail.com>
2012-11-22 13:23:57 +0400

    Dictionaries in DumbTemplate

--- srv/src/DumbTemplate.hs
+++ srv/src/DumbTemplate.hs
@@ -12,14 +12,21 @@ import qualified Data.Map as Map
 
 
 
-render :: Map Text Text -> Text -> Text
-render varMap = T.concat . loop
+render :: Map Text Text -> Map Text (Map Text Text) -> Text -> Text
+render varMap dicMap = T.concat . loop
   where
     loop tpl = case T.breakOn "$" tpl of
       (txt, "") -> [txt]
-      (txt, tpl') -> case rest of
-        "" -> [txt,varVal]
-        _  -> txt : varVal : loop (T.tail rest)
-        where
-          (var, rest) = T.breakOn "$" $ T.tail tpl'
-          varVal = Map.findWithDefault var var varMap
+      (txt, tpl') -> case T.breakOn "$" $ T.tail tpl' of
+        (exp, "")    -> [txt, evalExp exp]
+        (exp, tpl'') -> txt : evalExp exp : loop (T.tail tpl'')
+
+    evalExp e = case T.breakOn "#" e of
+      (var, "")     -> evalVar var
+      (dic, keyVar) -> evalDic dic (evalVar keyVar)
+
+    evalVar v = Map.findWithDefault v v varMap
+
+    evalDic dicNm k
+      = Map.findWithDefault (T.concat [dicNm, "#", k]) k
+      $ Map.findWithDefault Map.empty dicNm dicMap
