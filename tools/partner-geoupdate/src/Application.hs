d12a5711f0b8534e2861352fead1497065ffa94a
Dmitry Dzhus <dima@dzhus.org>
2012-11-30 02:23:41 +0400

    Use HTTP API to update partner data (thanks to 34294397b37ea73295bfd0969ae3b49c747c96e4)

--- tools/partner-geoupdate/src/Application.hs
+++ tools/partner-geoupdate/src/Application.hs
@@ -9,7 +9,7 @@ Standalone application to update partner coordinates.
 
 -}
 
-module Application 
+module Application
     ( GeoApp
     , geoAppInit)
 
@@ -26,12 +26,14 @@ import Data.Attoparsec.ByteString.Char8
 import Data.ByteString.Char8 (ByteString)
 import qualified Data.ByteString.Char8 as BS
 import qualified Data.ByteString.Lazy.Char8 as BSL
+import Data.Text.Encoding (decodeUtf8)
 
 import Data.Lens.Template
 
 import qualified Database.Redis as R
 
 import qualified Network.HTTP as H
+import Network.URI (parseURI)
 
 import Snap.Core
 import Snap.Snaplet
@@ -60,12 +62,30 @@ routes = [ ("/geo/partner/:pid", method PUT $ updatePosition)
 ------------------------------------------------------------------------------
 -- | Build reverse geocoding request URI.
 nominatimRevQuery :: Double -> Double -> String
-nominatimRevQuery lon lat = 
+nominatimRevQuery lon lat =
     concat ["http://nominatim.openstreetmap.org/reverse.php?"
            , "format=json&accept-language=ru-RU,ru&"
            , "lon=", show lon, "&"
            , "lat=", show lat
            ]
+
+
+------------------------------------------------------------------------------
+-- | URI for PUT request to update partner data.
+partnerUpdateURI :: Int -> String
+partnerUpdateURI pid = "http://localhost:8000/_/partner/" ++ (show pid)
+
+
+------------------------------------------------------------------------------
+-- | Name of address field in partner model.
+addressField :: ByteString
+addressField = "addrDeFacto"
+
+
+------------------------------------------------------------------------------
+-- | Name of coordinates field in partner model.
+coordsField :: ByteString
+coordsField = "coords"
 
 
 ------------------------------------------------------------------------------
@@ -90,29 +110,17 @@ instance FromJSON Address where
 
 
 ------------------------------------------------------------------------------
--- | Name of address field in partner model.
-addressField :: ByteString
-addressField = "addrDeFacto"
-
-
-------------------------------------------------------------------------------
--- | Set new address to partner with given id.
-writeAddress :: Int -> Address -> R.Redis ()
-writeAddress pid (Address addr) = do
-  let key = BS.concat ["partner:", BS.pack . show $ pid]
-  R.hset key addressField addr >> return ()
-
-
-------------------------------------------------------------------------------
--- | Query to update partner position.
---
--- Splice with lon, lat and partner id.
-updateQuery :: Query
-updateQuery = "UPDATE geo_partners SET coords=ST_PointFromText('POINT(? ?)', 4326) WHERE id=?;"
+-- | Derived from 'postRequestWithBody' from HTTP package.
+putRequestWithBody :: String -> String -> String -> H.Request_String
+putRequestWithBody urlString typ body =
+  case parseURI urlString of
+    Nothing -> error ("putRequestWithBody: Not a valid URL - " ++ urlString)
+    Just u  -> H.setRequestBody (H.mkRequest H.PUT u) (typ, body)
 
 
 getMessageQuery :: Query
 getMessageQuery = "SELECT message FROM partnerMessageTbl where partnerId=? order by ctime desc limit 1;"
+
 
 ------------------------------------------------------------------------------
 -- | Update partner position, setting new address if possible.
@@ -120,29 +128,50 @@ updatePosition :: Handler b GeoApp ()
 updatePosition = do
   lon' <- getParamWith double "lon"
   lat' <- getParamWith double "lat"
-  (id' :: Maybe Int) <- getParamWith decimal "pid"
-  case (lon', lat', id') of
-    (Just lon, Just lat, Just id) -> do
-        execute updateQuery (lon, lat, id)
-
+  (pid' :: Maybe Int) <- getParamWith decimal "pid"
+  case (lon', lat', pid') of
+    (Just lon, Just lat, Just pid) -> do
         -- Reverse geocode street address from coordinates.
-        (addr' :: Maybe Address) <- liftIO $ do
+        (addr :: Maybe Address) <- liftIO $ do
           resp <- H.simpleHTTP (H.getRequest $ nominatimRevQuery lon lat)
           body <- H.getResponseBody resp
           return $ decode' $ BSL.pack body
-        case addr' of
-          Just addr -> runRedisDB redis $ writeAddress id addr
-          _ -> return ()
+        liftIO $ updatePartnerData pid lon lat addr
     _ -> error "Bad request"
+
+
+
+-- | Send HTTP PUT request to CaRMa API to update partner data.
+updatePartnerData :: Int
+                  -- ^ Partner id.
+                  -> Double
+                  -- ^ Longitude.
+                  -> Double
+                  -- ^ Latitude.
+                  -> (Maybe Address)
+                  -- ^ New address if available.
+                  -> IO ()
+updatePartnerData pid lon lat addr =
+    let
+        coordString = concat [show lon, ",", show lat]
+        body = BSL.unpack $ encode $ object $
+               [decodeUtf8 coordsField .= coordString] ++
+               (maybe [] (\(Address a) -> [decodeUtf8 addressField .= a]) addr)
+    in 
+      H.simpleHTTP 
+           (putRequestWithBody 
+            (partnerUpdateURI pid) "application/json" body) >> return ()
+
 
 getMessage :: Handler b GeoApp ()
 getMessage = do
-  Just id <- getParam "pid"
-  let partnerId = BS.append "partner:" id
+  Just pid <- getParam "pid"
+  let partnerId = BS.append "partner:" pid
   res <- query getMessageQuery $ Only partnerId
   case res of
     (Only msg):_ -> writeLBS msg
     _ -> writeLBS "{}"
+
 
 geoAppInit :: SnapletInit b GeoApp
 geoAppInit = makeSnaplet "geo" "Geoservices" Nothing $ do
@@ -155,9 +184,9 @@ geoAppInit = makeSnaplet "geo" "Geoservices" Nothing $ do
 
 ------------------------------------------------------------------------------
 -- | Use the supplied parser to read data from request parameter.
-getParamWith :: MonadSnap m => 
+getParamWith :: MonadSnap m =>
                 Parser a
-             -> ByteString 
+             -> ByteString
              -- ^ Parameter name.
              -> m (Maybe a)
 getParamWith parser name = do
