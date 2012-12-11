66ff6189b182bd44e01bb368b4c7baf211b7c111
mvoidex <voidex@live.com>
2012-12-11 20:05:50 +0400

    Added 'warrantyCase' and 'contractor' to all services

--- srv/resources/site-config/models/tech1.js
+++ srv/resources/site-config/models/tech1.js
@@ -708,6 +708,39 @@
             }
         },
         {
+            "name": "warrantyCase",
+            "canRead": [
+                "partner",
+                "front",
+                "back",
+                "head",
+                "supervisor",
+                "director",
+                "analyst",
+                "parguy",
+                "account",
+                "admin",
+                "programman",
+                "parguy"
+            ],
+            "canWrite": [
+                "back",
+                "head",
+                "supervisor",
+                "director",
+                "analyst",
+                "parguy",
+                "account",
+                "admin",
+                "programman",
+                "parguy"
+            ],
+            "type": "checkbox",
+            "meta": {
+                "label": "Гарантийный случай"
+            }
+        },
+        {
             "name": "files",
             "canRead": [
                 "partner",
