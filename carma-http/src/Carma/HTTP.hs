9929e064f803f37507955df46c3df477653f4579
Dmitry Dzhus <dima@dzhus.org>
2013-01-20 20:26:19 +0400

    Few extra wrappers for c/r/u/d operations.

--- carma-http/src/Carma/HTTP.hs
+++ carma-http/src/Carma/HTTP.hs
@@ -11,7 +11,11 @@ module Carma.HTTP
     ( FieldName
     , FieldValue
     , InstanceData
-    , modelRequest
+    , instanceRequest
+    , createInstance
+    , readInstance
+    , updateInstance
+    , deleteInstance
     , instanceExists
     )
 
@@ -21,6 +25,7 @@ import Data.Aeson
 import Data.Functor
 import Data.Map as M
 import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as B8
 import qualified Data.ByteString.Lazy.Char8 as BSL
 
 import Network.HTTP
@@ -34,17 +39,6 @@ type FieldName = BS.ByteString
 
 -- | An instance of a model is a set of key-value pairs.
 type InstanceData = M.Map FieldName FieldValue
-
-
--- | CaRMa JSON response containing "id" field. The rest of fields are
--- ignored.
---
--- TODO: carma-mobile-server contains this code too.
-newtype IdResponse = IdResponse Int deriving Show
-
-instance FromJSON IdResponse where
-    parseJSON (Object v) = IdResponse . read <$> v .: "id"
-    parseJSON _          = error "Malformed CaRMa response"
 
 
 -- | Model API endpoint.
@@ -61,21 +55,21 @@ modelPidURI :: Int -> String -> Int -> String
 modelPidURI cp model pid = (modelURI cp model) ++ (show pid)
 
 
--- | Send request to c/r/u/d an instance of model, possibly using
--- model data, and return its id.
-modelRequest :: Int
-             -- ^ CaRMa port.
-             -> String
-             -- ^ Model name.
-             -> Maybe Int
-             -- ^ Model id.
-             -> RequestMethod
-             -> Maybe InstanceData
-             -- ^ Request payload.
-             -> IO Int
-modelRequest cp model pid rm row = do
+-- | Send request to c/r/u/d an instance of model, possibly using new
+-- instance data. Return id and instance data from server response.
+instanceRequest :: Int
+                -- ^ CaRMa port.
+                -> String
+                -- ^ Model name.
+                -> Maybe Int
+                -- ^ Model id.
+                -> RequestMethod
+                -> Maybe InstanceData
+                -- ^ Request payload.
+                -> IO (Int, Maybe InstanceData)
+instanceRequest cp model rid rm row = do
   let uri =
-          case pid of
+          case rid of
             Just n  -> modelPidURI cp model n
             Nothing -> modelURI cp model
   rs <- simpleHTTP $
@@ -84,15 +78,57 @@ modelRequest cp model pid rm row = do
               mkRequestWithBody uri rm $
               Just ("application/json", BSL.unpack $ encode payload)
           Nothing -> mkRequestWithBody uri rm Nothing
-  rsBody <- getResponseBody rs
-  let Just (IdResponse carmaPid) =
-          case pid of
-            Just n -> Just (IdResponse n)
-            Nothing ->
-                case decode' (BSL.pack rsBody) of
-                  Nothing -> error "CaRMa response contains no id field"
-                  justIdr -> justIdr
-  return carmaPid
+  inst <- (decode' . BSL.pack) <$> getResponseBody rs
+  return $ case rid of
+    -- We already know id
+    Just n -> (n, inst)
+    -- If no id provided, then we expect server to give us one
+    Nothing ->
+        -- Try to parse instance data from server response
+        case inst of
+          Just d -> case M.lookup "id" d of
+                      Just carmaId ->
+                          case B8.readInt carmaId of
+                            Just (n, _) -> (n, Just d)
+                            Nothing     ->
+                                error "Could not read id from CaRMa response"
+                      Nothing -> error "CaRMa response contains no id field"
+          -- Fail if no id provided and could not read response
+          Nothing -> error "Could not read CaRMa response"
+
+
+-- | Wrapper for 'instanceRequest' which requires non-Nothing server
+-- response.
+requireValidResponse :: Monad m =>
+                        (Int, Maybe InstanceData)
+                     -> m (Int, InstanceData)
+requireValidResponse (cid, rs) =
+    case rs of
+      Just d -> return (cid, d)
+      Nothing -> error "No valid CaRMa response"
+
+
+createInstance :: Int -> String -> InstanceData -> IO (Int, InstanceData)
+createInstance cp model row =
+    instanceRequest cp model Nothing POST (Just row)
+                        >>= requireValidResponse
+
+
+readInstance :: Int -> String -> Int -> IO InstanceData
+readInstance cp model rid = 
+    snd <$> (instanceRequest cp model (Just rid) GET Nothing 
+                                 >>= requireValidResponse)
+
+
+updateInstance :: Int -> String -> Int -> InstanceData -> IO InstanceData
+updateInstance cp model rid row =
+    snd <$> (instanceRequest cp model (Just rid) PUT (Just row) 
+                        >>= requireValidResponse)
+
+
+deleteInstance :: Int -> String -> Int -> IO ()
+deleteInstance cp model rid =
+    instanceRequest cp model (Just rid) DELETE Nothing >> return ()
 
 
 -- | Check if instance exists in the CaRMa database.
