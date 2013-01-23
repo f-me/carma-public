e01c0072f62684111264a526de53ddccef0544c9
Dmitry Dzhus <dima@dzhus.org>
2013-01-23 04:08:49 +0400

    Move codes&prices into a separate module.

--- tools/sagai-exporter/src/Carma/SAGAI.hs
+++ tools/sagai-exporter/src/Carma/SAGAI.hs
@@ -43,6 +43,7 @@ import System.Locale
 import Text.Printf
 
 import Carma.HTTP
+import Carma.SAGAI.Codes
 import Carma.SAGAI.Error
 import Carma.SAGAI.Util
 
@@ -345,67 +346,6 @@ timestampToDate input =
       Just time ->
           return $ B8.pack $ formatTime defaultTimeLocale dateFormat time
       Nothing -> exportError $ BadTime input
-
-
-data ExpenseType = Dossier
-                 | FalseCall
-                 | PhoneServ
-                 | Charge
-                 | Condition
-                 | Starter
-                 | Towage
-                 | RepTowage
-                 | Rent
-                   deriving (Eq, Ord)
-
-
--- | Data for particular type of expense.
-data CodeRow = CodeRow { cost             :: Double
-                       , impCode          :: BS.ByteString
-                       , serviceImpCode   :: BS.ByteString
-                       , causeCode        :: BS.ByteString
-                       , defCode          :: BS.ByteString
-                       }
-
-
--- | List of costs and I/D/C codes for all programs and expenses.
-codesData :: M.Map (FieldValue, ExpenseType) CodeRow
-codesData = M.fromList
-    [ (("citroen", Dossier),     CodeRow 1148    "DV1" "DV4" "9938" "G5F")
-    , (("citroen", FalseCall),   CodeRow 574     "DR1" "DR4" "9939" "296")
-    , (("citroen", PhoneServ),   CodeRow 351     "DV1" "DV4" "9939" "G5F")
-    , (("citroen", Charge),      CodeRow 1825    "DR1" "DR4" "996L" "446")
-    , (("citroen", Condition),   CodeRow 1825    "DR1" "DR4" "996D" "446")
-    , (("citroen", Starter),     CodeRow 1825    "DR1" "DR4" "996A" "446")
-    , (("citroen", Towage),      CodeRow 2911    "DR1" "DR4" "9934" "G5F")
-    , (("citroen", RepTowage),   CodeRow 2009    "DR1" "DR4" "9936" "G5F")
-    , (("citroen", Rent),        CodeRow 0       "PV1" "PV4" "9927" "PZD")
-    , (("peugeot", Dossier),     CodeRow 1354.64 "24R" "FCA" "8999" "G5D")
-    , (("peugeot", FalseCall),   CodeRow 677.32  "24E" "FCA" "8990" "G5D")
-    , (("peugeot", PhoneServ),   CodeRow 414.18  "24E" "FCA" "8943" "G5D")
-    , (("peugeot", Charge),      CodeRow 2153.5  "24E" "FCA" "8962" "G5D")
-    , (("peugeot", Condition),   CodeRow 2153.5  "24E" "FCA" "8954" "G5D")
-    , (("peugeot", Starter),     CodeRow 2153.5  "24E" "FCA" "8963" "G5D")
-    , (("peugeot", Towage),      CodeRow 3434.98 "24E" "FCA" "8950" "G5D")
-    , (("peugeot", RepTowage),   CodeRow 2370.62 "2RE" "FCA" "8983" "G5D")
-    , (("peugeot", Rent),        CodeRow 0       "F6R" "FCA" "8997" "PZD")
-    ]
-
-
--- | Daily costs for car rent service.
-rentCosts :: M.Map (FieldValue, FieldValue) Double
-rentCosts = M.fromList
-    [ (("citroen", "psab"),  1758)
-    , (("citroen", "psam1"), 2310)
-    , (("citroen", "psam2"), 3041)
-    , (("citroen", "psah"),  3994)
-    , (("citroen", "psam"),  2310)
-    , (("peugeot", "psab"),  2074.44)
-    , (("peugeot", "psam1"), 2725.8)
-    , (("peugeot", "psam2"), 3588.38)
-    , (("peugeot", "psah"),  4712.92)
-    , (("peugeot", "psam"),  2725.8)
-    ]
 
 
 -- | Search for an entry in 'codesData' using program name of the case
