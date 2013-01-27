5e00edb6f9f80ef4be47c4704bdab2c113e719ac
DaryaBA <darya@formalmethods.ru>
2013-01-27 23:39:34 +0400

    Update srv/resources/site-config/models/tech.js
    Добавлено поле "Номер заказ-наряда"
    #438

--- srv/resources/site-config/models/tech.js
+++ srv/resources/site-config/models/tech.js
@@ -626,6 +626,32 @@
             }
         },
         {
+            "name": "orderNumber",
+            "canRead": [
+                "head",
+                "supervisor",
+                "director",
+                "analyst",
+                "parguy",
+                "account",
+                "admin",
+                "programman"
+            ],
+            "canWrite": [
+                "head",
+                "supervisor",
+                "director",
+                "analyst",
+                "parguy",
+                "account",
+                "admin",
+                "programman"
+            ],
+            "meta": {
+                "label": "Номер заказ-наряда"
+            }
+        },        
+        {
             "name": "urgentService",
             "type": "checkbox",
             "meta": {
