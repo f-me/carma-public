c37e86d0613961d1ae33c8d28fb9b7fd499a1bfa
mvoidex <voidex@live.com>
2013-01-30 03:05:06 +0400

    ARC rewritten

--- srv/src/Snaplet/DbLayer/ARC.hs
+++ srv/src/Snaplet/DbLayer/ARC.hs
@@ -16,7 +16,9 @@ import Control.Arrow
 import Control.Monad
 import Control.Monad.IO.Class
 import Data.Monoid
+import Data.Maybe (fromMaybe)
 import Data.List
+import Data.Function (on)
 import qualified Data.Map as M
 import Data.String
 import qualified Data.Text as T
@@ -26,11 +28,12 @@ import qualified Snap.Snaplet.PostgresqlSimple as PS
 import qualified Database.PostgreSQL.Simple.ToField as PS
 import Snaplet.DbLayer.Dictionary
 import System.Locale
+import Text.Format
 
 import qualified Codec.Xlsx as Xlsx
 import qualified Codec.Xlsx.Writer as Xlsx
 
-import Snap.Snaplet.SimpleLog
+import Snap.Snaplet.SimpleLog hiding ((%=))
 
 query_ :: (PS.HasPostgres m, MonadLog m, PS.FromRow r) => PS.Query -> m [r]
 query_ s = do
@@ -101,63 +104,80 @@ withinDay st field = [
     T.concat [field, " - '", st, "' < '1 day'"],
     T.concat [field, " - '", st, "' >= '0 days'"]]
 
-rows :: [(T.Text, T.Text -> [T.Text] -> PreQuery)]
-rows = queries where
-    cst = preQuery_ ["count(*)"] ["casetbl", "servicetbl"] ["'case:' || casetbl.id = servicetbl.parentId"] [] []
-    inday st = preQuery_ ["count(*)"] [] (withinDay st "servicetbl.createTime") [] []
-    inList :: T.Text -> [T.Text] -> PreQuery
-    inList tbl lst = preQuery ["count(*)"] [tbl] [T.concat [tbl, ".program in ?"]] [] [] [PS.In lst]
-    csday st ps = mconcat [cst, inday st, inList "casetbl" ps]
-    
-    totalCalls st ps = mconcat [inList "calltbl" ps, preQuery_ ["count(*)"] ["calltbl"] (withinDay st "calltbl.callDate") [] []]
-    techServices st ps = mconcat [csday st ps, preQuery [] [] ["servicetbl.type = ?"] [] [] [str "tech"]]
-    towageServicesWithTech st ps = mconcat [csday st ps, preQuery [] [] ["servicetbl.type = ?", "position(? in casetbl.services) != 0"] [] [] [str "towage", str "tech"]]
-    towageServices st ps = mconcat [csday st ps, preQuery [] [] ["servicetbl.type = ?"] [] [] [str "towage"]]
-    towageDtps st ps = mconcat [csday st ps, preQuery [] [] ["servicetbl.type = ?", "casetbl.diagnosis1 = ?"] [] [] [str "towage", str "dtp"]]
-    others st ps = mconcat [csday st ps, preQuery [] [] ["servicetbl.type not in (?, ?)"] [] [] [str "tech", str "towage"]]
-    notSatisfied st ps = mconcat [csday st ps, preQuery [] [] ["servicetbl.clientSatisfied = ?"] [] [] [str "0"]]
-    
-    queries = [
-        ("Total calls", totalCalls),
-        ("Techs", techServices),
-        ("Towage-techs", towageServicesWithTech),
-        ("Towages", towageServices),
-        ("DTPs", towageDtps),
-        ("Others", others),
-        ("Not satisfied", notSatisfied)]
-
 programs :: Dictionary -> [(T.Text, [T.Text])]
 programs = maybe [] (map (id &&& (return . id))) . keys ["Programs"]
 
 -- | Create ARC report for year and month
+-- select count(*) from calltbl where (date_trunc('day', calltbl.callDate) = TIMESTAMP '2012-08-06');
+-- select count (*) from casetbl, servicetbl where ('case:' || casetbl.id = servicetbl.parentId) and (servicetbl.type = 'tech') and (date_trunc('day', servicetbl.createTime) = TIMESTAMP '2012-08-06');
+-- select count(*) from casetbl, servicetbl where ('case:' || casetbl.id = servicetbl.parentId) and (servicetbl.type = 'towage') and (position('tech' in casetbl.services) != 0) and (date_trunc('day', servicetbl.createTime) = TIMESTAMP '2012-08-06');
+-- select count(*) from casetbl, servicetbl where ('case:' || casetbl.id = servicetbl.parentId) and (servicetbl.type = 'towage') and (date_trunc('day', servicetbl.createTime) = TIMESTAMP '2012-08-06');
+-- select count(*) from casetbl, servicetbl where ('case:' || casetbl.id = servicetbl.parentId) and (servicetbl.type = 'towage') and (casetbl.diagnosis1 = 'dtp') and (date_trunc('day', servicetbl.createTime) = TIMESTAMP '2012-08-06');
+-- select count(*) from casetbl, servicetbl where ('case:' || casetbl.id = servicetbl.parentId) and (servicetbl.type not in ('tech', 'towage')) and (date_trunc('day', servicetbl.createTime) = TIMESTAMP '2012-08-06');
+-- select count(*) from casetbl, servicetbl where ('case:' || casetbl.id = servicetbl.parentId) and (servicetbl.clientSatisfied = 'notSatis') and (date_trunc('day', servicetbl.createTime) = TIMESTAMP '2012-08-06');
 arcReport :: (PS.HasPostgres m, MonadLog m) => Dictionary -> Integer -> Int -> m ()
 arcReport d year month = scope "arc" $ do
     log Info $ T.concat ["Creating arc report for ", fromString (formatTime defaultTimeLocale "%m.%Y" (fromGregorian year month 1))]
-    tz <- liftIO getCurrentTimeZone
+    tzMins <- liftM timeZoneMinutes $ liftIO getCurrentTimeZone
     let
         daysCount = gregorianMonthLength year month
-        startOfDay n = localTimeToUTC tz localTm where
-            localTm = LocalTime (fromGregorian year month n) midnight
-        starts :: [T.Text]
-        starts = map (fromString . formatTime defaultTimeLocale "%F %T" . startOfDay) [1..daysCount]
-    reportData <- forM (programs d) $ \(pname, pprog) -> scope pname $ do
-        programData <- forM rows $ \(rname, rquery) -> scope rname $ do
-            counts <- forM (zip [1..daysCount] starts) $ \(nday, st) -> scope (T.pack $ show nday) $ do
-                intQuery [rquery st pprog]
-            log Debug $ T.concat ["Counts for month: ", T.pack $ show counts]
-            return (rname, counts)
-        return (pname, programData)
-    
+        currentMonth = formatTime defaultTimeLocale "%Y-%m-%d" (fromGregorian year month 1)
+        args = [
+            "tz" %= tzMins,
+            "month" %= currentMonth,
+            "serviceSelect" %= (format "select count(*), casetbl.program, date_part('day', servicetbl.createTime + '$tz minutes') dy from casetbl, servicetbl where ('case: || casetbl.id = servicetbl.parentId)" ["tz" %= tzMins]),
+            "inThisMonth" %= (format "(date_trunc('month', servicetbl.createTime) = timestamp '$month')" ["month" %= currentMonth]),
+            "serviceGroup" %= ("group by casetbl.program, dy order by casetbl.program, dy" :: String),
+            "towage" %= ("(servicetbl.type = 'towage')" :: String)]
+
+        groupByProgram :: [(Integer, T.Text, Integer)] -> [(T.Text, [(Integer, Integer)])]
+        groupByProgram = map (first head) . map unzip . groupBy ((==) `on` fst) . map reorder where
+            reorder (cnt, pname, d) = (pname, (d, cnt))
+
+        fillZeroDays :: [(Integer, Integer)] -> [Integer]
+        fillZeroDays dayCounts = map (\i -> fromMaybe 0 (lookup i dayCounts)) [1..toInteger daysCount]
+
+        makeRow :: T.Text -> [(Integer, T.Text, Integer)] -> M.Map T.Text [(T.Text, [Integer])]
+        makeRow name dat = M.fromList $ map (second toRow) $ groupByProgram dat where
+            toRow :: [(Integer, Integer)] -> [(T.Text, [Integer])]
+            toRow rs = [(name, fillZeroDays rs)]
+
+        queryFmt lns = query_ $ fromString $ T.unpack $ format (concat lns) args
+
+    totalCalls <- queryFmt [
+        "select count(*), program, date_part('day', calltbl.callDate + '$tz minutes') dy",
+        " from calltbl where (date_trunc('month', calltbl.callDate + '$tz minutes') = timestamp '$month')",
+        " group by program, dy order by program, dy"]
+    techServices <- queryFmt [
+        "$serviceSelect and (servicetbl.type = 'tech') and $inThisMonth $serviceGroup"]
+    towageServicesWithTech <- queryFmt [
+        "$serviceSelect and $towage and (position('tech' in casetbl.services) != 0) and $inThisMonth $serviceGroup"]
+    towageServices <- queryFmt ["$serviceSelect and $towage and $inThisMonth $serviceGroup"]
+    towageDtps <- queryFmt ["$serviceSelect  and $towage and (casetbl.diagnosis1 = 'dtp') and $inThisMonth $serviceGroup"]
+    others <- queryFmt ["$serviceSelect and (servicetbl.type not in ('tech', 'towage')) and $inThisMonth $serviceGroup"]
+    notSatisfied <- queryFmt ["$serviceSelect and (servicetbl.clientSatisfied = 'notSatis') and $inThisMonth $serviceGroup"]
+
     log Trace "Saving to ARC.xlsx"
+
     let
+        reportData = M.unionsWith (++) [
+            makeRow "Total calls" totalCalls,
+            makeRow "Techs" techServices,
+            makeRow "Towage-techs" towageServicesWithTech,
+            makeRow "Towages" towageServices,
+            makeRow "DTPs" towageDtps,
+            makeRow "Others" others,
+            makeRow "Not satisfied" notSatisfied]
+
         oneRow :: (T.Text, [Integer]) -> [Xlsx.CellValue]
         oneRow (nm, vals) = Xlsx.CellText rowName : map (Xlsx.CellDouble . fromIntegral) vals where
             rowName = maybe nm id $ look ["ARC", nm] d
         oneProgram :: (T.Text, [(T.Text, [Integer])]) -> [[Xlsx.CellValue]]
         oneProgram (nm, rs) = map ((Xlsx.CellText progName :) . oneRow) rs where
             progName = maybe nm id $ look ["Programs", nm] d
-        sheetData = concatMap oneProgram reportData
-        header = replicate 2 (Xlsx.CellText "") ++ map (Xlsx.CellDouble . fromIntegral) [1..daysCount]
+        sheetData = concatMap oneProgram $ M.toList reportData
+        header = replicate 2 (Xlsx.CellText "") ++ map (Xlsx.CellDouble . fromIntegral) [1.. daysCount]
+
     liftIO $ saveXlsx "ARC.xlsx" header sheetData
     log Info "Report saved to ARC.xlsx"
 
@@ -181,12 +201,3 @@ saveArcReport :: FilePath -> [T.Text] -> [[Integer]] -> IO ()
 saveArcReport f ts fs = saveXlsx f names fields where
     names = map (Xlsx.CellText) ts
     fields = map (map (Xlsx.CellDouble . fromIntegral)) fs
-
--- ARC
--- select count(*) from calltbl where (date_trunc('day', calltbl.callDate) = TIMESTAMP '2012-08-06');
--- select count (*) from casetbl, servicetbl where ('case:' || casetbl.id = servicetbl.parentId) and (servicetbl.type = 'tech') and (date_trunc('day', servicetbl.createTime) = TIMESTAMP '2012-08-06');
--- select count(*) from casetbl, servicetbl where ('case:' || casetbl.id = servicetbl.parentId) and (servicetbl.type = 'towage') and (position('tech' in casetbl.services) != 0) and (date_trunc('day', servicetbl.createTime) = TIMESTAMP '2012-08-06');
--- select count(*) from casetbl, servicetbl where ('case:' || casetbl.id = servicetbl.parentId) and (servicetbl.type = 'towage') and (date_trunc('day', servicetbl.createTime) = TIMESTAMP '2012-08-06');
--- select count(*) from casetbl, servicetbl where ('case:' || casetbl.id = servicetbl.parentId) and (servicetbl.type = 'towage') and (casetbl.diagnosis1 = 'dtp') and (date_trunc('day', servicetbl.createTime) = TIMESTAMP '2012-08-06');
--- select count(*) from casetbl, servicetbl where ('case:' || casetbl.id = servicetbl.parentId) and (servicetbl.type not in ('tech', 'towage')) and (date_trunc('day', servicetbl.createTime) = TIMESTAMP '2012-08-06');
--- 
