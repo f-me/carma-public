f75736841126718476caa105df196dcc0764e45f
cvb <peter@standalone.su>
2013-02-11 20:00:56 +0400

    compile coffee without a top-level function wrapper, don't need with requirejs

--- srv/coffee-build.sh
+++ srv/coffee-build.sh
@@ -6,4 +6,4 @@ FROM=resources/static/coffee/
 # you can add custom keys to coffee compiler, like
 # ./coffee-build.hs -wl
 # so it will run watch on FROM and jslint after compile
-coffee -c $@ -o $TO $FROM
+coffee -b -c $@ -o $TO $FROM
