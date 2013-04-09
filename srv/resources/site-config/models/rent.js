d5aea45cd63d1175462508c7255bc82f6c09209b
Max Taldykin <jorpic@gmail.com>
2013-04-09 15:36:29 +0400

    New field #646

--- srv/resources/site-config/models/rent.js
+++ srv/resources/site-config/models/rent.js
@@ -523,6 +523,14 @@
             }
         },
         {
+            "name": "clientCancelReason",
+            "type": "dictionary",
+            "meta": {
+              "dictionaryName": "ClientCancelReason",
+              "label": "Причина отказа клиента"
+            }
+        },
+        {
             "name": "falseCallPercent",
             "canRead":  [ ],
             "canWrite": [ ],
