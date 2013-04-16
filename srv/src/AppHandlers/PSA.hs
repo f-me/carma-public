04f08539369c1bfe259bd8e9b3d95c0deaa111ee
Dmitry Dzhus <dima@dzhus.org>
2013-04-17 01:40:26 +0400

    Fix typo in queries

--- srv/src/AppHandlers/PSA.hs
+++ srv/src/AppHandlers/PSA.hs
@@ -67,11 +67,11 @@ WITH parentcase AS (select calldate, car_vin, comment from casetbl where id=?)
 SELECT c.id FROM casetbl c INNER JOIN towagetbl s
 ON c.id=cast(split_part(s.parentid, ':', 2) as integer)
 WHERE s.parentid is not null
-AND c.car_vin=(SELECT car_vin FROM parentcase);
+AND c.car_vin=(SELECT car_vin FROM parentcase)
 AND (s.status='serviceOk' OR s.status='serviceClosed')
 AND c.calldate >= ((SELECT calldate FROM parentcase) - INTERVAL '30 days')
 AND c.calldate < (SELECT calldate FROM parentcase)
-AND c.comment=(SELECT comment FROM parentcase)
+AND c.comment=(SELECT comment FROM parentcase);
 |]
 
 
@@ -82,12 +82,12 @@ WITH parentcase AS (select calldate, car_vin, comment from casetbl where id=?)
 SELECT c.id FROM casetbl c INNER JOIN techtbl s
 ON c.id=cast(split_part(s.parentid, ':', 2) as integer)
 WHERE s.parentid is not null
-AND c.car_vin=(SELECT car_vin FROM parentcase);
+AND c.car_vin=(SELECT car_vin FROM parentcase)
 AND s.falseCall='bill'
 AND s.techType='charge'
 AND c.calldate >= ((SELECT calldate FROM parentcase) - INTERVAL '1 day')
 AND c.calldate <= (SELECT calldate FROM parentcase)
-AND c.comment=(SELECT comment FROM parentcase)
+AND c.comment=(SELECT comment FROM parentcase);
 |]
 
 
