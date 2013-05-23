9e3b2d49cbb65477633960e1874ffbf401ca794f
cvb <peter@standalone.su>
2013-05-23 14:40:19 +0400

    add view for partner cancel reports

--- database_init.sql
+++ database_init.sql
@@ -175,3 +175,11 @@ create view servicesview as
     where c.id::text = substring(s.parentId, ':(.*)');
 
 GRANT SELECT ON servicesview TO carma_db_sync;
+
+CREATE VIEW partnercancelview AS
+  SELECT pc.*, p.partner
+  FROM      partnercanceltbl pc
+  LEFT JOIN partnertbl       p
+  ON p.id::text = substring(pc.partnerid, ':(.*)');
+
+GRANT SELECT ON partnercancelview TO carma_db_sync;
