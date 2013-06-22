38884f3b44a339754fba44810aa0755456877daf
Dmitry Dzhus <dima@dzhus.org>
2013-06-22 12:19:55 +0400

    Bulk attachments

--- srv/src/Snaplet/FileUpload.hs
+++ srv/src/Snaplet/FileUpload.hs
@@ -16,9 +16,11 @@ module Snaplet.FileUpload
   ) where
 
 import Control.Lens
+import Control.Monad
 import Control.Concurrent.STM
 
-import Data.Aeson as A
+import Data.Aeson as A hiding (Object)
+import Data.Attoparsec.Text as P
 
 import qualified Data.Map as M
 import Data.Maybe
@@ -26,7 +28,9 @@ import Data.Configurator
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Char8 as B8
-import Data.HashSet as HS
+import qualified Data.HashSet as HS
+import Data.Char
+import qualified Data.Text as T
 
 import System.Directory
 import System.FilePath
@@ -56,28 +60,29 @@ data FileUpload b = FU { cfg      :: UploadPolicy
                        }
 
 routes :: [(ByteString, Handler b (FileUpload b) ())]
-routes = [ (":model/:id/:field",       method POST   $ uploadInField)
+routes = [ (":model/bulk/:field",      method POST   $ uploadBulk)
+         , (":model/:id/:field",       method POST   $ uploadInField)
          ]
 
 -- | Lift a DbLayer handler action to FileUpload handler.
 withDb :: Handler b (DbLayer b) a -> Handler b (FileUpload b) a
 withDb = (gets db >>=) . flip withTop
 
+-- | A field of an instance to attach an attachment to.
+type AttachmentTarget = (ModelName, ObjectId, FieldName)
+
 -- | Upload a file, create a new attachment (an instance of
--- @attachment@ model) and add a reference to it in a field of another
--- existing model instance, set by @model@, @id@ and @field@ request
--- parameters.
+-- @attachment@ model), add references to it in a given set of other
+-- instance fields. Return the created attachment object and a list of
+-- attachment targets used.
 --
 -- The file is stored under @attachment/<newid>@ directory hierarchy
--- of finished uploads dir. Serve JSON with @attachment@ instance data
--- in response, including @<newid>@.
-uploadInField :: Handler b (FileUpload b) ()
-uploadInField = do
-  -- 'Just' here for these have already been matched by Snap router
-  Just model <- getParam "model"
-  Just objId <- getParam "id"
-  Just field <- getParam "field"
-
+-- of finished uploads dir.
+uploadInManyFields :: (FilePath -> [AttachmentTarget])
+                   -- ^ Convert the uploaded file name to a list of
+                   -- fields in instances to attach the file to.
+                   -> Handler b (FileUpload b) (Object, [AttachmentTarget])
+uploadInManyFields flds = do
   -- Create empty attachment instance
   attach <- withDb $ DB.create "attachment" M.empty
 
@@ -90,9 +95,48 @@ uploadInField = do
   _ <- withDb $ DB.update "attachment" aid $
                 M.singleton "filename" (stringToB fName)
 
-  attachToField model objId field $ B8.append "attachment:" aid
+  let targets = flds fName
+  forM_ targets $ \(model, objId, field) ->
+      attachToField model objId field $ B8.append "attachment:" aid
 
-  withDb (DB.read "attachment" aid) >>= (writeLBS . A.encode)
+  obj <- withDb $ DB.read "attachment" aid
+  return (obj, targets)
+
+-- | Upload and attach a file (as in 'uploadInManyFields'), but read a
+-- list of instance ids from the file name (@732,123,452.pdf@; all
+-- non-digit characters serve as instance id separators). @model@ and
+-- @field@ are read from request parameters.
+--
+-- Server response is a JSON object with two keys: @attachment@
+-- contains an attachment object, @targets@ contains a list of triples
+-- with attachment targets used.
+uploadBulk :: Handler b (FileUpload b) ()
+uploadBulk = do
+  -- 'Just' here for these have already been matched by Snap router
+  Just model <- getParam "model"
+  Just field <- getParam "field"
+  res <- uploadInManyFields $
+             \fName -> map (\i -> (model, i, field)) (readIds fName)
+  writeLBS $ A.encode $ A.object [ "attachment" A..= fst res
+                                 , "targets"    A..= snd res]
+      where
+        -- Read a list of decimal instance id from a file name,
+        -- skipping everything else.
+        readIds :: FilePath -> [ObjectId]
+        readIds fn =
+            either (const []) (map $ stringToB . (show :: Int -> String)) $
+            parseOnly (many1 $ skipWhile (not . isDigit) >> decimal)
+            (T.pack fn)
+
+-- | Upload and attach a file (as in 'uploadInManyFields') to a single
+-- instance, given by @model@, @id@ and @field@ request parameters.
+uploadInField :: Handler b (FileUpload b) ()
+uploadInField = do
+  Just model <- getParam "model"
+  Just objId <- getParam "id"
+  Just field <- getParam "field"
+  res <- uploadInManyFields $ const [(model, objId, field)]
+  writeLBS $ A.encode $ fst res
 
 -- | Append a reference of form @attachment:213@ to a field of another
 -- instance. This handler is thread-safe.
@@ -117,7 +161,7 @@ attachToField modelName instanceId field ref = do
   -- Append new ref to the target field
   inst <- withDb $ DB.read modelName instanceId
   let newRefs = addRef (M.findWithDefault "" field inst) ref
-  withDb $ DB.update modelName instanceId $ M.insert field newRefs inst
+  _  <- withDb $ DB.update modelName instanceId $ M.insert field newRefs inst
   -- Unlock the field
   liftIO $ atomically $ do
     hs <- readTVar l
@@ -127,7 +171,6 @@ attachToField modelName instanceId field ref = do
       addRef ""    ref = ref
       addRef field ref = BS.concat [field, ",", ref]
       lockName = BS.concat [modelName, ":", instanceId, "/", field]
-
 
 -- | Store a file upload from the request using a provided directory
 -- (relative to finished uploads path), return full path to the
