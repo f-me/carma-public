f-me/carma-public

commit d512b9ede0d713be5b049ad6a879d9740cd2b60c

Store uploaded attachments in attachment/<id> dir of s/fileupload, do not set parentRef
Return full path from doUpload

srv/src/ApplicationHandlers.hs
@@ -62,7 +62,7 @@ import qualified Snaplet.DbLayer.Types as DB
 import qualified Snaplet.DbLayer.ARC as ARC
 import qualified Snaplet.DbLayer.RKC as RKC
 import qualified Snaplet.DbLayer.Dictionary as Dict
-import Snaplet.FileUpload (finished, tmp, doUpload)
+import Snaplet.FileUpload (tmp, doUpload)
 ------------------------------------------------------------------------------
 import Application
 import AppHandlers.Util
@@ -377,17 +377,16 @@ serveUsersList = with db usersListPG >>= writeJSON
 vinUploadData :: AppHandler ()
 vinUploadData = scope "vin" $ scope "upload" $ do
   log Trace "Uploading data"
-  fName <- with fileUpload $ doUpload "vin-upload-data"
+  fPath <- with fileUpload $ doUpload "vin-upload-data"
 
-  finishedRoot <- with fileUpload $ gets finished
-  let f = finishedRoot </> "vin-upload-data" </> fName
+  let fName = takeFileName fPath
 
-  log Trace $ T.concat ["Uploaded to file: ", T.pack f]
+  log Trace $ T.concat ["Uploaded to file: ", T.pack fName]
   prog <- getParam "program"
   case prog of
     Nothing -> log Error "Program not specified"
     Just pgmId -> do
-      log Info $ T.concat ["Uploading ", T.pack f]
+      log Info $ T.concat ["Uploading ", T.pack fName]
       log Trace $ T.concat ["Program: ", T.decodeUtf8 pgmId]
 
       -- Check if this program is available for user
@@ -406,13 +405,13 @@ vinUploadData = scope "vin" $ scope "upload" $ do
 
       log Trace $ T.concat ["VIN format: ", T.decodeUtf8 vF]
 
-      log Trace $ T.concat ["Initializing state for file: ", T.pack f]
-      with vin $ initUploadState f
-      log Trace $ T.concat ["Uploading data from file: ", T.pack f]
+      log Trace $ T.concat ["Initializing state for file: ", T.pack fName]
+      with vin $ initUploadState fName
+      log Trace $ T.concat ["Uploading data from file: ", T.pack fName]
 
       -- Set current user as owner
       let Just (UserId uid) = userId u
-      with vin $ uploadData (T.encodeUtf8 uid) pgmId (T.unpack . T.decodeUtf8 $ vF) f
+      with vin $ uploadData (T.encodeUtf8 uid) pgmId (T.unpack . T.decodeUtf8 $ vF) fPath
 
 vinStateRead :: AppHandler ()
 vinStateRead = scope "vin" $ scope "state" $ scope "get" $ do
@@ -437,15 +436,13 @@ partnerUploadData = scope "partner" $ scope "upload" $ do
   let carmaPort = case getPort sCfg of
                     Just n -> n
                     Nothing -> error "No port"
-  finishedRoot <- with fileUpload $ gets finished
   tmpPath <- with fileUpload $ gets tmp
   (tmpName, _) <- liftIO $ openTempFile tmpPath "last-pimp.csv"
 
   log Trace "Uploading data"
-  fileName <- with fileUpload $ doUpload "partner-upload-data"
+  inPath <- with fileUpload $ doUpload "partner-upload-data"
 
-  let inPath = finishedRoot </> "partner-upload-data" </> fileName
-      outPath = tmpPath </> tmpName
+  let outPath = tmpPath </> tmpName
 
   log Trace $ T.pack $ "Input file " ++ inPath
   log Trace $ T.pack $ "Output file " ++ outPath

srv/src/Snaplet/FileUpload.hs
@@ -22,7 +22,7 @@ import qualified Data.Map as M
 import Data.Maybe
 import Data.Configurator
 import Data.ByteString (ByteString)
-import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as B8
 
 import System.Directory
 import System.FilePath
@@ -49,58 +49,60 @@ data FileUpload b = FU { cfg      :: UploadPolicy
                        }
 
 routes :: [(ByteString, Handler b (FileUpload b) ())]
-routes = [ (":model/:id/:field",       method POST   $ attachToField)
+routes = [ (":model/:id/:field",       method POST   $ uploadInField)
          ]
 
-
--- | Lift DbLayer handler action to FileUpload handler.
+-- | Lift a DbLayer handler action to FileUpload handler.
 withDb :: Handler b (DbLayer b) a -> Handler b (FileUpload b) a
 withDb = (gets db >>=) . flip withTop
 
-
--- | Create a new attachment (an instance of @attachment@ model) and
--- add a reference to it in a field of another model instance, set by
--- @model@, @id@ and @field@ request parameters. Serve JSON with
--- @attachment@ instance data in response.
-attachToField :: Handler b (FileUpload b) ()
-attachToField = do
+-- | Upload a file, create a new attachment (an instance of
+-- @attachment@ model) and add a reference to it in a field of another
+-- existing model instance, set by @model@, @id@ and @field@ request
+-- parameters.
+--
+-- The file is stored under @attachment/<newid>@ directory hierarchy
+-- of finished uploads dir. Serve JSON with @attachment@ instance data
+-- in response, including @<newid>@.
+uploadInField :: Handler b (FileUpload b) ()
+uploadInField = do
   model <- getParamOrDie "model"
   objId <- getParamOrDie "id"
   field <- getParamOrDie "field"
-  fName <- doUpload $ model </> objId </> field
-  let parentRef = BS.concat [stringToB model, ":", stringToB objId]
-  attach <- withDb $
-            DB.create "attachment" $
-            M.fromList [ ("filename", stringToB fName)
-                       , ("parentRef", parentRef)
-                       ]
+
+  -- Create empty attachment instance
+  attach <- withDb $ DB.create "attachment" M.empty
+
+  -- Store the file
+  let aid = attach M.! "id"
+  fPath <- doUpload $ "attachment" </> (B8.unpack aid)
+
+  -- Save filename in attachment
+  let fName = takeFileName fPath
+  _ <- withDb $ DB.update "attachment" aid $
+                M.singleton "filename" (stringToB fName)
+
   -- modifyResponse $ setResponseCode 200
-  writeLBS $ A.encode attach
+  withDb (DB.read "attachment" aid) >>= (writeLBS . A.encode)
 
 -- | Store a file upload from the request using a provided directory
--- (relative to finished uploads path), return its file name.
---
--- Full path to the uploaded file may be obtained as follows (@fu@ is
--- a lens to a FileUpload snaplet):
---
--- > fName <- with fu $ doUpload relPath
--- > finishedRoot <- with fu $ gets finished
--- > let path = finishedRoot </> relPath </> fName
-doUpload :: FilePath -> Handler b (FileUpload b) String
+-- (relative to finished uploads path), return full path to the
+-- uploaded file.
+doUpload :: FilePath -> Handler b (FileUpload b) FilePath
 doUpload relPath = do
   tmpd <- gets tmp
   cfg  <- gets cfg
-  f    <- gets finished
+  root <- gets finished
   fns  <- handleFileUploads tmpd cfg (const $ partPol cfg) $
     liftIO . fmap catMaybes . mapM (\(info, r) -> case r of
       Left _    -> return Nothing
       Right res -> do
         let justFname = U.bToString . fromJust $ partFileName info
-        let path      = f </> relPath
+        let path      = root </> relPath
         createDirectoryIfMissing True path
         copyFile res $ path </> justFname
         return $ Just justFname)
-  return $ head fns
+  return $ root </> relPath </> head fns
 
 getParamOrDie name =
   getParam name >>= \case

commit 412666f06df94eb1fec27bd45872bb481793ef28

Store filename in report model, not a full path
(fixes report templates broken in d512b9ede0 #941 #942)

srv/src/ApplicationHandlers.hs
@@ -357,7 +357,7 @@ createReportHandler = do
   -- because in multipart/form-data requests we do not have
   -- params as usual, see Snap.Util.FileUploads.setProcessFormInputs
   _ <- with db $ DB.update "report" objId $
-    Map.fromList [ ("templates", T.encodeUtf8 $ T.pack f)
+    Map.fromList [ ("templates", T.encodeUtf8 $ T.pack $ takeFileName f)
                  , ("name",      name) ]
   redirect "/#reports"
 