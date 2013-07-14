028c2863cc215f0d2045a3fffcc3840b3a2c69bb
Max Taldykin <jorpic@gmail.com>
2013-07-14 12:43:13 +0400

    Fix heredoc in db patch #963 #1002

--- database/patches/0.0.14-drop-redis-attachments.sh
+++ database/patches/0.0.14-drop-redis-attachments.sh
@@ -1,29 +1,24 @@
 #!/bin/bash -e
 
-SCRIPT=<<EOF
+SCRIPT=$(cat <<EOF
 for _,k in pairs(redis.call('keys', '*')) do
   local r = redis.call('type', k)
   if r.ok == 'hash' then
-    f = redis.call('hget', k, 'files')
-    if f and not string.find(f, 'attachment:') then
+    local x = redis.call('hget', k, 'files')
+    if x and not string.find(x, 'attachment:') then
       redis.call('hdel', k, 'files')
     end
-  end
-end
-
-for _,k in pairs(redis.call('keys', 'program:*')) do
-  local r = redis.call('type', k)
-  if r.ok == 'hash' then
-    r = redis.call('hget', k, 'contracts')
-    if r and not string.find(r, 'attachment:') then
+    x = redis.call('hget', k, 'contracts')
+    if x and not string.find(x, 'attachment:') then
       redis.call('hdel', k, 'contracts')
     end
-    l = redis.call('hget', k, 'logo')
-    if l and not string.find(l, 'attachment:') then
+    x = redis.call('hget', k, 'logo')
+    if x and not string.find(x, 'attachment:') then
       redis.call('hdel', k, 'logo')
     end
   end
 end
 EOF
+)
 
 redis-cli eval "${SCRIPT}" 0
