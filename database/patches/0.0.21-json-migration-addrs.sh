4a87852b201bb327e89d5212c495e037ec9207fa
Dmitry Dzhus <dima@dzhus.org>
2013-07-17 17:37:54 +0400

    Migrate old addrDeFacto to addrs field #922

--- database/patches/0.0.21-json-migration-addrs.sh
+++ database/patches/0.0.21-json-migration-addrs.sh
@@ -6,7 +6,7 @@ OUTPUT=$(mktemp /tmp/addrsXXXXXX-tmp)
 OUTPUT2=$(mktemp /tmp/addrsXXXXXX-out)
 
 curl -s "http://localhost:${CARMA_PORT}/all/partner" | \
-    jq -c -r '.[] | .id + "|\(if .addrDeJure and ((.addrDeJure) == "" | not) then [{key:("jure"), value:.addrDeJure}] else [] end + if .salesAddress and ((.salesAddress) == "" | not) then [{key:("sales"), value:.salesAddress}] else [] end + if .serviceAddress and ((.serviceAddress) == "" | not) then [{key:("serv"), value:.serviceAddress}] else [] end)"' > ${OUTPUT}
+    jq -c -r '.[] | .id + "|\(if .addrDeJure and ((.addrDeJure) == "" | not) then [{key:("jure"), value:.addrDeJure}] else [] end + if .addrDeFacto and ((.addrDeFacto) == "" | not) then [{key:("fact"), value:.addrDeFacto}] else [] end + if .salesAddress and ((.salesAddress) == "" | not) then [{key:("sales"), value:.salesAddress}] else [] end + if .serviceAddress and ((.serviceAddress) == "" | not) then [{key:("serv"), value:.serviceAddress}] else [] end)"' > ${OUTPUT}
 
 while read line
 do
--- srv/resources/site-config/models/partner.js
+++ srv/resources/site-config/models/partner.js
@@ -62,29 +62,6 @@
     },
     {
       "meta": {
-        "label": "Фактический адрес"
-      },
-      "type": null,
-      "groupName": null,
-      "name": "addrDeFacto"
-    },
-    {
-      "meta": {
-        "widget": "picker",
-        "label": "Координаты фактического адреса",
-        "infoText": "coords",
-        "picker": "mapPicker",
-        "targetAddr": "addrDeFacto",
-        "targetCoords": "coords",
-        "cityField": "city",
-        "currentBlipType": "partner"
-      },
-      "type": "coords",
-      "groupName": null,
-      "name": "coords"
-    },
-    {
-      "meta": {
          "label": "Телефоны",
          "jsonSchema": "dict-objects",
          "dictionaryName": "PhoneTypes",
