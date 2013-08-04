b10ee594f15609d38460fc48440c765f6bdc3667
Max Taldykin <jorpic@gmail.com>
2013-08-04 20:03:40 +0400

    New car models #1034 #1059

--- database/baseline/3-dictionaries/5-CarModel.sql
+++ database/baseline/3-dictionaries/5-CarModel.sql
@@ -1921,3 +1921,20 @@ INSERT INTO "CarModel" (value, label, parent)
          id
     FROM "CarMaker" WHERE value = 'ford';
 
+INSERT INTO "CarModel" (value, label, parent)
+  SELECT 'grandCMax',
+         'GRAND C-Max',
+         id
+    FROM "CarMaker" WHERE value = 'ford';
+
+INSERT INTO "CarModel" (value, label, parent)
+  SELECT 'f150',
+         'F150',
+         id
+    FROM "CarMaker" WHERE value = 'ford';
+
+INSERT INTO "CarModel" (value, label, parent)
+  SELECT 'С-Elysee',
+         'С-Elysee',
+         id
+    FROM "CarMaker" WHERE value = 'citroen';
