216af4f55326d519dc766a5848b9ba44328ec03f
Max Taldykin <jorpic@gmail.com>
2013-08-29 11:20:51 +0400

    Reduce boilerpalte in model dispatcher

--- carma-models/src/Carma/Model.hs
+++ carma-models/src/Carma/Model.hs
@@ -1,4 +1,5 @@
 
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE Rank2Types #-}
 module Carma.Model
   (Model
@@ -7,17 +8,23 @@ module Carma.Model
   ) where
 
 import Data.Text (Text)
-import Data.Model
+import qualified Data.Text as T
+import qualified Data.Map as Map
+import Data.Typeable
 
+import Data.Model
 import Carma.Model.Dictionary (Dictionary)
 import Carma.Model.CarMake    (CarMake)
 import Carma.Model.CarModel   (CarModel)
 
 
-dispatch :: Text -> (forall m . Model m => m -> a) -> Maybe a
-dispatch model fn = case model of
-  "Dictionary" -> Just $ fn (undefined :: Dictionary)
-  "CarMake"    -> Just $ fn (undefined :: CarMake)
-  "CarModel"   -> Just $ fn (undefined :: CarModel)
-  _ -> Nothing
-
+dispatch :: forall a . Text -> (forall m . Model m => m -> a) -> Maybe a
+dispatch model fn = Map.lookup model modelMap
+  where
+    add :: Model m => m -> (Text, a)
+    add m = (T.pack $ show $ typeOf m, fn m)
+    modelMap = Map.fromList
+      [add (undefined :: Dictionary)
+      ,add (undefined :: CarMake)
+      ,add (undefined :: CarModel)
+      ]
