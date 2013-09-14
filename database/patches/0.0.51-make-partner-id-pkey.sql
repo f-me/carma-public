455c80007d6a6c1e24466ad772baa3e597aef267
Max Taldykin <jorpic@gmail.com>
2013-09-14 14:32:04 +0400

    Drop duplicates before adding PK in 0.0.51 patch

--- database/patches/0.0.51-make-partner-id-pkey.sql
+++ database/patches/0.0.51-make-partner-id-pkey.sql
@@ -1,1 +1,7 @@
+delete from partnertbl
+  where ctid in
+    (select max(ctid) from partnertbl
+      where id in
+        (select id from partnertbl group by id having count(*) > 1)
+      group by id);
 ALTER TABLE partnertbl ADD PRIMARY KEY (id);
