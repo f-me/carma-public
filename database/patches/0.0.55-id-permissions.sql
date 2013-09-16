770cd346d6a6a00d467b54f57cdf6a8992501703
Max Taldykin <jorpic@gmail.com>
2013-09-16 22:19:10 +0400

    Add permissions to read id

---
+++ database/patches/0.0.55-id-permissions.sql
@@ -0,0 +1,4 @@
+insert into "FieldPermission" (role,model,field,r,w) values
+('all', 'NewCaseField', 'id', true, false),
+('all', 'CarModel', 'id', true, false),
+('all', 'CarMake', 'id', true, false);
