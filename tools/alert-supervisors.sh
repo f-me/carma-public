6b94acb0c809c763fc3bda0efc23bea8767b73c5
i2m <ildr.islmv@gmail.com>
2013-10-10 02:50:58 +0400

    Fixed delayed time in supervisors notification shell script. #1279

--- tools/alert-supervisors.sh
+++ tools/alert-supervisors.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 DB_NAME=${1:-carma}
-MESSAGE=`
+
+UNASSIGNED=`
 psql --html $DB_NAME << EOF
 SELECT
   replace(caseid, 'case:', 'http://carma:8000/#case/') as "URL",
@@ -18,13 +19,32 @@ LEFT JOIN "ActionName" ON actiontbl.name = "ActionName".value
 INNER JOIN casetbl ON split_part(actiontbl.caseid, ':', 2)::int = casetbl.id
 LEFT JOIN "City" ON casetbl.city = "City".value
 LEFT JOIN programtbl ON casetbl.program = programtbl.value
-
 WHERE
-  (localtimestamp > ('5 minutes'::interval + ctime) AND assigntime IS NULL)
-  OR
-  (name = ANY ('{orderService, callMeMaybe, orderServiceAnalyst}')
-   AND (assigntime IS NOT NULL AND closetime IS NULL)
-   AND localtimestamp > ('15 minutes'::interval + assigntime))
+  localtimestamp > ('5 minutes'::interval + ctime) AND assigntime IS NULL
+EOF
+`
+
+OUTSTANDING=`
+psql --html $DB_NAME << EOF
+SELECT
+  replace(caseid, 'case:', 'http://carma:8000/#case/') as "URL",
+  split_part(caseid, ':', 2) as "Номер кейса",
+  assignedto as "Оператор",
+  date_trunc('seconds', localtimestamp - ('15 minutes'::interval + assigntime)) as "Просрочено",
+  "ActionName".label as "Тип действия",
+  duetime as "Запланировано",
+  "City".label as "Город",
+  programtbl.label as "Программа"
+FROM
+  actiontbl
+LEFT JOIN "ActionName" ON actiontbl.name = "ActionName".value
+INNER JOIN casetbl ON split_part(actiontbl.caseid, ':', 2)::int = casetbl.id
+LEFT JOIN "City" ON casetbl.city = "City".value
+LEFT JOIN programtbl ON casetbl.program = programtbl.value
+WHERE
+  name = ANY ('{orderService, callMeMaybe, orderServiceAnalyst}')
+  AND (assigntime IS NOT NULL AND closetime IS NULL)
+  AND localtimestamp > ('15 minutes'::interval + assigntime)
 EOF
 `
 
@@ -40,7 +60,10 @@ MIME-Version: 1.0
 Content-Type: text/html
 Content-Disposition: inline
 
-$MESSAGE
+<h3>Нераспределенные действия</h3>
+$UNASSIGNED
+<h3>Невыполненные действия</h3>
+$OUTSTANDING
 
 EOF
 
