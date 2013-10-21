b1a9f5cebba6aa82f243ca201957854bd2778894
Dmitry Dzhus <dima@dzhus.org>
2013-10-21 04:14:49 +0400

    One last patch fix

--- database/patches/0.0.74-update-action-targetGroup-redis.sh
+++ database/patches/0.0.74-update-action-targetGroup-redis.sh
@@ -6,6 +6,7 @@ MODEL="action"
 FIELD="targetGroup"
 FIELDSIZE=${#FIELD}
 
-${PSQL} -A -t -c "select concat(E'*4\r\n', '$', E'4\r\nHSET\r\n', '$', length(concat('${MODEL}:', id)), E'\r\n', concat('${MODEL}:', id), E'\r\n', '$', ${FIELDSIZE}, E'\r\n${FIELD}\r\n', '$', length(${FIELD}), E'\r\n', ${FIELD}, E'\r') from actiontbl;" > ${REDIS}
+${PSQL} -A -t -c "select concat(E'*4\r\n', '$', E'4\r\nHSET\r\n', '$', length(concat('${MODEL}:', id)), E'\r\n', concat('${MODEL}:', id), E'\r\n', '$', ${FIELDSIZE}, E'\r\n${FIELD}\r\n', '$', length(coalesce(${FIELD}, '')), E'\r\n', ${FIELD}, E'\r') from actiontbl;" > ${REDIS}
 
 cat ${REDIS} | redis-cli --pipe
+rm ${REDIS}
