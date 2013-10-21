3e26ed021790567ebaf29554ccd84f45245b9c5a
Dmitry Dzhus <dima@dzhus.org>
2013-10-21 04:03:39 +0400

    Update 0.0.74 patch

--- database/patches/0.0.74-update-action-targetGroup-redis.sh
+++ database/patches/0.0.74-update-action-targetGroup-redis.sh
@@ -1,29 +1,11 @@
 #!/bin/bash -e
 
-SQL=$(mktemp /tmp/sqlXXXXXX)
 REDIS=$(mktemp /tmp/redisXXXXXX)
-
-psql carma -A -t -F ';' -c "select id,targetGroup from actiontbl" > ${SQL}
 
 MODEL="action"
 FIELD="targetGroup"
 FIELDSIZE=${#FIELD}
 
-function add_hset ()
-{
-    key="$MODEL:$1"
-    keysize=${#key}
-    value=${2}
-    valuesize=${#value}
-    echo -e -n "*4\r\n\$4\r\nHSET\r\n\$${keysize}\r\n${key}\r\n\$${FIELDSIZE}\r\n${FIELD}\r\n\$${valuesize}\r\n${value}\r\n" >> ${REDIS}
-}
-
-while read line
-do
-    key=$(echo ${line} | cut -d';' -f1)
-    value=$(echo ${line} | cut -d';' -f2)
-    add_hset "$key" "$value"
-done < ${SQL}
+${PSQL} -A -t -c "select concat(E'*4\r\n', '$', E'4\r\nHSET\r\n', '$', length(concat('${MODEL}:', id)), E'\r\n', concat('${MODEL}:', id), E'\r\n', '$', ${FIELDSIZE}, E'\r\n${FIELD}\r\n', '$', length(${FIELD}), E'\r\n', ${FIELD}, E'\r') from actiontbl;" > ${REDIS}
 
 cat ${REDIS} | redis-cli --pipe
-rm ${SQL} ${REDIS}
