b042a1b1efc25d17e15f8e77ea26584bbc7ad045
Dmitry Dzhus <dima@dzhus.org>
2013-10-26 18:41:28 +0300

    New role idents (f-me/carma#1209), TH ident list generator (f-me/carma#1316), isBack Role flag (f-me/carma/#1353)

---
+++ carma-models/src/Data/Model/TH.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+module Data.Model.TH
+    ( mkIdents
+    )
+
+where
+
+import Control.Monad
+
+import qualified Data.HashMap.Strict as HM
+
+import Language.Haskell.TH
+
+import Data.Model.Types
+
+
+-- | Generate a list of Ident declarations and bind `idents` to a
+-- hashmap mapping String labels to produced declarations.
+--
+-- > mkIdents 'Role [("all", 5)]
+-- all = Ident 5 :: Ident Role
+-- idents = HM.fromList [("all", all)]
+mkIdents :: (Q Type) 
+         -> [(String, Integer)] 
+         -> Q [Dec]
+mkIdents modelTy names = do
+    Just identTy <- lookupTypeName "Ident"
+    let list = mkName "idents"
+        defTy = (conT identTy) `appT` modelTy
+    identDefs <- forM names $ 
+      \(nm, i) -> do
+        let nm' = mkName nm
+        -- Type sig
+        s <- sigD nm' defTy
+        -- Value def
+        d <- valD (varP nm') 
+             (normalB ((conE 'Ident) `appE` (litE $ integerL i))) []
+        return [s, d]
+    idMapTy  <- sigD list [t|HM.HashMap String $defTy|]
+    let pairs = map (\(nm, _) -> tupE [litE $ stringL nm, varE $ mkName nm])
+                names
+    idMapDef <- valD (varP list) (normalB ((varE 'HM.fromList) `appE` (listE pairs))) []
+    return $ concat $ identDefs ++ [[idMapTy, idMapDef]]
