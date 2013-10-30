9fbf9445eaa277b9b805baae7442643f58dc2d3e
Dmitry Dzhus <dima@dzhus.org>
2013-10-30 20:24:04 +0400

    Add role equations for account, vwfake, psaanalyst, contract_admin #1209

--- database/patches/0.0.77-usermeta-new-roles.sql
+++ database/patches/0.0.77-usermeta-new-roles.sql
@@ -19,6 +19,10 @@ UPDATE usermetatbl u SET roles=u.roles || array['22','12','14','1']
 FROM usermetatbl_tmp t WHERE '11'=ANY(t.roles)
 AND t.id=u.id;
 
+UPDATE usermetatbl u SET roles=u.roles || array['25', '14', '1']
+FROM usermetatbl_tmp t WHERE '15'=ANY(t.roles)
+AND t.id=u.id;
+
 UPDATE usermetatbl u SET roles=u.roles || array['24','14','1'] 
 FROM usermetatbl_tmp t WHERE '5'=ANY(t.roles)
 AND t.id=u.id;
@@ -27,7 +31,7 @@ UPDATE usermetatbl u SET roles=u.roles || array['23','14','1']
 FROM usermetatbl_tmp t WHERE '4'=ANY(t.roles)
 AND t.id=u.id;
 
-UPDATE usermetatbl u SET roles=u.roles || array['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','20'] 
+UPDATE usermetatbl u SET roles=u.roles || array['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','20','33']
 FROM usermetatbl_tmp t WHERE '6'=ANY(t.roles)
 AND t.id=u.id;
 
@@ -59,6 +63,10 @@ UPDATE usermetatbl u SET roles=u.roles || array['19','1']
 FROM usermetatbl_tmp t WHERE '22'=ANY(t.roles)
 AND t.id=u.id;
 
+UPDATE usermetatbl u SET roles=u.roles || array['20', '1']
+FROM usermetatbl_tmp t WHERE '21'=ANY(t.roles)
+AND t.id=u.id;
+
 UPDATE usermetatbl u SET roles=u.roles || array['28','29','14','3','1'] 
 FROM usermetatbl_tmp t WHERE '7'=ANY(t.roles)
 AND t.id=u.id;
@@ -74,3 +82,32 @@ AND t.id=u.id;
 UPDATE usermetatbl u SET roles=u.roles || array['31','14','1'] 
 FROM usermetatbl_tmp t WHERE '20'=ANY(t.roles)
 AND t.id=u.id;
+
+UPDATE usermetatbl u SET roles=u.roles || array['32', '1']
+FROM usermetatbl_tmp t WHERE '24'=ANY(t.roles)
+AND t.id=u.id;
+
+UPDATE usermetatbl u SET roles=u.roles || array['15', '1']
+FROM usermetatbl_tmp t WHERE '14'=ANY(t.roles)
+AND t.id=u.id;
+
+
+DROP FUNCTION IF EXISTS array_unique(anyarray);
+
+CREATE FUNCTION array_unique(non_unique_array_vals anyarray)
+RETURNS TABLE (unique_array anyarray) AS $BODY$
+    BEGIN
+      RETURN QUERY SELECT
+        ARRAY_AGG(unique_vals.val)
+      FROM
+        (
+  SELECT
+  UNNEST(non_unique_array_vals) AS val
+  GROUP BY val) AS unique_vals;
+    END;
+  $BODY$
+LANGUAGE plpgsql;
+
+UPDATE usermetatbl SET roles = array_unique(roles);
+
+DROP FUNCTION IF EXISTS array_unique(anyarray);
