cac0dc35e369f5d4495158b2f97f18056349f7d9
Dmitry Dzhus <dima@dzhus.org>
2013-10-31 05:06:15 +0400

    Fix patch

--- database/patches/0.0.86-update-Role-permissions.sh
+++ database/patches/0.0.86-update-Role-permissions.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -e
 
+$PSQL -c 'drop table "FieldPermission"'
 $PSQL -c 'drop table "Role"'
 $PSQL -f baseline/3-dictionaries/7-Role.sql
+$PSQL -f baseline/3-dictionaries/8-FieldPermission.sql
