96c6b6674e4b5aef6b48bd002011feecad935f4f
Dmitry Dzhus <dima@dzhus.org>
2013-11-25 20:17:38 +0300

    Move Program fields around a bit #1309

--- database/baseline/3-dictionaries/22-Program.sql
+++ database/baseline/3-dictionaries/22-Program.sql
@@ -1,11 +1,9 @@
 CREATE TABLE "Program"
   ( id    SERIAL PRIMARY KEY
-  , value text UNIQUE NOT NULL
   , label text UNIQUE NOT NULL
-  , active bool NOT NULL DEFAULT true
   , client text
-  , clientCode text
   , clientAddress text
+  , clientCode text
   );
 
 GRANT ALL ON "Program" TO carma_db_sync;
--- database/baseline/3-dictionaries/23-SubProgram.sql
+++ database/baseline/3-dictionaries/23-SubProgram.sql
@@ -2,12 +2,14 @@ CREATE TABLE "SubProgram"
   ( id    SERIAL PRIMARY KEY
   , parent int4 REFERENCES "Program" ON DELETE SET NULL
   , label text NOT NULL
+  , active bool NOT NULL DEFAULT true
+  , value text UNIQUE NOT NULL
   , mailAddr text
   , mailPass text
   , contacts text
   , services int4[] DEFAULT array[]::int4[]
   , checkPeriod integer
-  , validUntil date
+  , validUntil integer
   , contract text
   , logo text
   , help text
