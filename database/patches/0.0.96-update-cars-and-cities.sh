1d8884fc5674c2995f0de7b2fb7587d2274f8545
Dmitry Dzhus <dima@dzhus.org>
2013-12-01 22:59:12 +0400

    Drop dependant table prior to dropping CarModel

--- database/patches/0.0.96-update-cars-and-cities.sh
+++ database/patches/0.0.96-update-cars-and-cities.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -e
 
+$PSQL -c 'drop table "SynCarModel"'
 $PSQL -c 'drop table "CarModel"'
 $PSQL -c 'drop table "City"'
 $PSQL -f baseline/3-dictionaries/5-CarModel.sql
 $PSQL -f baseline/3-dictionaries/6-City.sql
+$PSQL -f baseline/3-dictionaries/25-SynCarModel.sq
