aa8548c56f972a895956a9b98e08b9b712371146
Dmitry Dzhus <dima@dzhus.org>
2013-12-01 23:05:17 +0400

    Drop partnercancelview when dropping City

--- database/patches/0.0.96-update-cars-and-cities.sh
+++ database/patches/0.0.96-update-cars-and-cities.sh
@@ -1,8 +1,11 @@
 #!/bin/bash -e
 
+$PSQL -c 'drop view "partnercancelview"'
+$PSQL -c 'drop table "City"'
+$PSQL -f baseline/3-dictionaries/6-City.sql
+$PSQL -f baseline/5-views/1-partnercancel-view.sql
+
 $PSQL -c 'drop table "SynCarModel"'
 $PSQL -c 'drop table "CarModel"'
-$PSQL -c 'drop table "City"'
 $PSQL -f baseline/3-dictionaries/5-CarModel.sql
-$PSQL -f baseline/3-dictionaries/6-City.sql
-$PSQL -f baseline/3-dictionaries/25-SynCarModel.sq
+$PSQL -f baseline/3-dictionaries/25-SynCarModel.sql
