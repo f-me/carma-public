56d46890488ae7b54c9f70c313fc48141f9f7021
i2m <ildr.islmv@gmail.com>
2013-12-03 16:50:49 +0400

    Added 'Colors' model. #1305

---
+++ carma-models/src/Carma/Model/Colors.hs
@@ -0,0 +1,23 @@
+
+module Carma.Model.Colors where
+
+
+import Data.Text (Text)
+import Data.Typeable
+
+import Carma.Model.Types()
+
+import Data.Model
+import Data.Model.View
+
+data Colors = Colors
+  {ident   :: PK Int Colors
+  ,value   :: F Text "value" "Внутреннее название цвета"
+  ,label   :: F Text "label" "Название цвета"
+  } deriving Typeable
+
+instance Model Colors where
+  type TableName Colors = "Colors"
+  modelInfo = mkModelInfo Colors ident
+  modelView _ = modifyView defaultView [readonly value]
+
