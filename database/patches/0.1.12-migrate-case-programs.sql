a48542195ca5df56660c1333dc1108bf461701e9
Dmitry Dzhus <dima@dzhus.org>
2013-12-08 19:29:18 +0400

    Finally, migrate {call,case}/{program,subProgram} to use numeric ids from Program/SubProgram #1309 #1381 #1451
    Note that program field is of type text (legacy) while subprogram is
    integer

---
+++ database/patches/0.1.12-migrate-case-programs.sql
@@ -0,0 +1,9 @@
+-- Migrate cases with known programs
+WITH remap AS 
+(SELECT old.value, p.id AS pid, sp.id AS sid
+ FROM programtbl old, "Program" p, "SubProgram" sp
+ WHERE sp.parent=p.id AND sp.value=old.value)
+UPDATE casetbl
+SET program = (SELECT pid::text FROM remap WHERE remap.value=casetbl.program),
+ subprogram = (SELECT sid FROM remap WHERE remap.value=casetbl.program)
+FROM remap WHERE program IN (SELECT value FROM remap);
---
+++ database/patches/0.1.9-migrate-call-programs.sql
@@ -0,0 +1,9 @@
+-- Migrate calls with known programs
+WITH remap AS 
+(SELECT old.value, p.id AS pid, sp.id AS sid
+ FROM programtbl old, "Program" p, "SubProgram" sp
+ WHERE sp.parent=p.id AND sp.value=old.value)
+UPDATE calltbl
+SET program = (SELECT pid::text FROM remap WHERE remap.value=calltbl.program),
+ subprogram = (SELECT sid FROM remap WHERE remap.value=calltbl.program)
+FROM remap WHERE program IN (SELECT value FROM remap);
