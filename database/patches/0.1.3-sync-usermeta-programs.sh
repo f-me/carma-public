6de2a9e535bbc022696f4d64968020aeb9369831
Dmitry Dzhus <dima@dzhus.org>
2013-12-13 15:55:38 +0400

    Ping 0.1.2-0.1.3 patches to use latest version of Program/SubProgram mappings #1381 #1451

--- database/patches/0.1.3-sync-usermeta-programs.sh
+++ database/patches/0.1.3-sync-usermeta-programs.sh
@@ -7,6 +7,11 @@ MODEL="usermeta"
 FIELD="programs"
 FIELDSIZE=${#FIELD}
 
+$PSQL -c 'drop table "SubProgram"'
+$PSQL -c 'drop table "Program"'
+$PSQL -f baseline/3-dictionaries/22-Program.sql
+$PSQL -f baseline/3-dictionaries/23-SubProgram.sql
+
 ${PSQL} -f patches/0.1.2-usermeta-programs.sql
 
 ${PSQL} -A -t -c "select concat(E'*4\r\n', '$', E'4\r\nHSET\r\n', '$', length(concat('${MODEL}:', id)), E'\r\n', concat('${MODEL}:', id), E'\r\n', '$', ${FIELDSIZE}, E'\r\n${FIELD}\r\n', '$', octet_length(coalesce(array_to_string(${FIELD},','), '')), E'\r\n', array_to_string(${FIELD},','), E'\r') from ${TABLE};" > ${REDIS}
