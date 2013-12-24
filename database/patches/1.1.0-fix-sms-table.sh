869443055a23457176dda41ba28eb7b4c8bf3971
Dmitry Dzhus <dima@dzhus.org>
2013-12-24 20:44:04 +0400

    Fix version dupes

--- database/patches/1.1.0-fix-sms-table.sh
+++ database/patches/1.1.0-fix-sms-table.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-$PSQL -c 'drop table "Sms"'
+$PSQL -c 'drop table  if exists "Sms"'
 $PSQL -f baseline/3-dictionaries/27-Sms.sql
 $PSQL << EOF
 COPY "FieldPermission" (role, model, field, r, w) FROM stdin;
