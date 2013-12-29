ba020adfeb46d15fb3c18bb4abd26a33c74d7595
Dmitry Dzhus <dima@dzhus.org>
2013-12-29 04:08:07 +0400

    Decompose field parameter types and mkVinFormat using VinFieldParameter class.
    Reverted GADTs in VFAccessor datatype (somewhy vinnie fails to
    typecheck when GADT syntax is used)

--- carma-models/src/Carma/Model/VinFormat/Meta.hs
+++ carma-models/src/Carma/Model/VinFormat/Meta.hs
@@ -5,17 +5,18 @@ VinFormat macros and meta helpers.
 The idea is to list a set of annotated 'Contract' fields and generate
 VinFormat model from it. Each source field @f@ is spliced into several
 fields @fLoad@, @fRequired@ etc., depending on the annotation given
-for that field. A source field is mapped to a group of produced
-VinFormat accessors are available using 'VFAccessor' type.
+for that field. 'VFAccessor' type preserves information about how a
+source field has is mapped to a group of produced VinFormat accessors.
 
 -}
 
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE Rank2Types #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TemplateHaskell #-}
 
 module Carma.Model.VinFormat.Meta
-    ( FF(..)
+    ( ContractField(..), FF(..)
     , FormatFieldType(..)
     , mkVinFormat
     , VFAccessor(..)
@@ -24,7 +25,7 @@ module Carma.Model.VinFormat.Meta
 where
 
 import Data.Typeable
-import GHC.TypeLits
+import GHC.TypeLits as GHC
 import Language.Haskell.TH
 
 import Data.Text as T (Text, unpack)
@@ -34,6 +35,116 @@ import Data.Vector (Vector)
 import Data.Model
 
 import Carma.Model.Contract (Contract)
+
+
+-- | Existential wrapper for 'Contract' field accessors.
+data ContractField where
+    CF :: (Typeable t, GHC.SingI n, GHC.SingI d) =>
+          (Contract -> F t n d) -> ContractField
+
+
+fieldProjFormatter :: (ContractField -> String)
+                   -> ContractField
+                   -> String
+                   -- ^ 'printf' format.
+                   -> String
+fieldProjFormatter proj cf format = printf format $ proj cf
+
+
+ns :: TypeQ -> StrictTypeQ
+ns = strictType (return NotStrict)
+
+
+-- | Convert 'TypeRep' to Template Haskell 'Type'.
+typeRepToType :: TypeRep -> Type
+typeRepToType tr =
+    let
+        tyConToType tyCon = ConT $ mkName $ tyConName tyCon
+        (conTr, trs)      = splitTyConApp tr
+    in
+      foldl (\t m -> AppT t (typeRepToType m)) (tyConToType conTr) trs
+
+
+-- | VIN format field parameter.
+--
+-- Minimal definition includes 'nameFormat' and 'descFormat'.
+class VinFieldParameter a where
+    type ParamType a
+    paramType  :: Typeable (ParamType a) =>
+                  a -> ContractField -> TypeRep
+    nameFormat :: a -> String
+    descFormat :: a -> String
+
+    name       :: a -> ContractField -> String
+    desc       :: a -> ContractField -> String
+    mkAcc      :: Typeable (ParamType a) =>
+                  a -> ContractField -> VarStrictTypeQ
+
+    type ParamType a = Bool
+    paramType _ _    = typeOf $ (undefined :: ParamType a)
+
+    name a cf  = fieldProjFormatter (\(CF f) -> T.unpack $ fieldName f) cf $
+                 nameFormat a
+    desc a cf  = fieldProjFormatter (\(CF f) -> T.unpack $ fieldDesc f) cf $
+                 descFormat a
+
+    mkAcc a cf =
+        varStrictType (mkName n) $ ns $
+        [t|
+         F $(return $ typeRepToType t)
+         $(litT $ strTyLit n)
+         $(litT $ strTyLit d)|]
+        where
+          n = name a cf
+          d = desc a cf
+          t = paramType a cf
+
+
+data Load
+instance VinFieldParameter Load where
+    nameFormat _ = "%sLoad"
+    descFormat _ = "Загружать поле «%s»"
+
+
+data Required
+instance VinFieldParameter Required where
+    nameFormat _ = "%sRequired"
+    descFormat _ = "Поле «%s» обязательно"
+
+
+data CodeTitle
+instance VinFieldParameter CodeTitle where
+    type ParamType CodeTitle = Maybe Text
+    nameFormat _ = "%sCodeTitle"
+    descFormat _ = "Заголовок кода дилера для поля «%s»"
+
+
+data Title
+instance VinFieldParameter Title where
+    type ParamType Title = Text
+    nameFormat _ = "%sTitle"
+    descFormat _ = "Заголовок поля «%s»"
+
+
+data NameTitles
+instance VinFieldParameter NameTitles where
+    type ParamType NameTitles = Vector Text
+    nameFormat _ = "%sTitles"
+    descFormat _ = "Заголовки поля «%s»"
+
+
+data DateFormat
+instance VinFieldParameter DateFormat where
+    type ParamType DateFormat = Maybe Text
+    nameFormat _ = "%sFormat"
+    descFormat _ = "Формат даты для поля «%s»"
+
+
+data Default
+instance VinFieldParameter Default where
+    paramType _ (CF acc) = fieldType acc
+    nameFormat _ = "%sDefault"
+    descFormat _ = "Значение по умолчанию для поля «%s»"
 
 
 -- | Semantic annotations for 'Contract' fields.
@@ -57,18 +168,8 @@ data FormatFieldType = Raw
 
 -- | Annotated field of the 'Contract' model.
 data FF where
-    FF :: (Typeable t, SingI n, SingI d) =>
+    FF :: (Typeable t, GHC.SingI n, GHC.SingI d) =>
           FormatFieldType -> (Contract -> F t n d) -> FF
-
-
--- | Convert 'TypeRep' to Template Haskell 'Type'.
-typeRepToType :: TypeRep -> Type
-typeRepToType tr =
-    let
-        tyConToType tyCon = ConT $ mkName $ tyConName tyCon
-        (conTr, trs)      = splitTyConApp tr
-    in
-      foldl (\t m -> AppT t (typeRepToType m)) (tyConToType conTr) trs
 
 
 -- | Generate VinFormat model. Haskell field accessor names equal
@@ -80,34 +181,16 @@ mkVinFormat :: [FF]
 mkVinFormat formatFields =
     let
         -- Type & constructor name
-        name = mkName "VinFormat"
-
-        loadSuffix = "Load"
-        requiredSuffix = "Required"
-        titleSuffix (FF fft _) =
-            case fft of
-              Name -> "Titles"
-              _    -> "Title"
-        codeSuffix = "CodeTitle"
-        formatSuffix = "Format"
-        defaultSuffix = "Default"
-
-        ns = strictType (return NotStrict)
+        typeName = mkName "VinFormat"
 
         -- Primary key and label
         idnt = [ varStrictType (mkName "ident") $
                  ns $
-                 appT (appT [t|PK Int|] (conT name)) [t|"Формат VIN"|]
+                 appT (appT [t|PK Int|] (conT typeName)) [t|"Формат VIN"|]
                , varStrictType (mkName "label") $
                  ns $
                  [t|F Text "label" "Название формата"|]
                ]
-
-        fnWithSuffix (FF _ proj) suf =
-            T.unpack (fieldName proj) ++ suf
-
-        fdFormatted (FF _ proj) format =
-            printf format $ T.unpack $ fieldDesc proj
 
         -- Use specified fields of 'Contract' model in constructor.
         -- Collect accessors that were produced from every source
@@ -117,104 +200,62 @@ mkVinFormat formatFields =
             map
             (\ff@(FF fft proj) ->
              let
-                 loadName     = fnWithSuffix ff loadSuffix
-                 requiredName = fnWithSuffix ff requiredSuffix
-                 titleName    = fnWithSuffix ff $ titleSuffix ff
-                 codeName     = fnWithSuffix ff codeSuffix
-                 formatName   = fnWithSuffix ff formatSuffix
-                 defaultName  = fnWithSuffix ff defaultSuffix
+                 acc = CF proj
+                 loadName     = name (undefined :: Load)     acc
+                 requiredName = name (undefined :: Required) acc
              in
                (,) ff $ (,)
              [e|
               VFAcc
-              $(varE $ mkName $ T.unpack $ fieldName proj)
+              $(appE [e|CF|] (varE $ mkName $ T.unpack $ fieldName proj))
               $(varE $ mkName loadName)
               $(varE $ mkName requiredName)
               |] $
              -- fLoad,fRequired,fTitle(s) fields
-             [ varStrictType (mkName loadName) $
-                 ns $
-                 [t|
-                  F Bool
-                  $(litT $ strTyLit loadName)
-                  $(litT $ strTyLit $ fdFormatted ff "Загружать поле «%s»")|]
-             , varStrictType (mkName requiredName) $
-                 ns $
-                 [t|
-                  F Bool
-                  $(litT $ strTyLit requiredName)
-                  $(litT $ strTyLit $
-                    fdFormatted ff "Поле «%s» обязательно")|]
-             , varStrictType (mkName titleName) $
-                 ns $
-                 [t|
-                  F $(case fft of
-                        Name -> [t|Vector Text|]
-                        _    -> [t|Text|])
-                  $(litT $ strTyLit titleName)
-                  $(litT $ strTyLit $ fdFormatted ff "Заголовок поля «%s»")|]
+             [ mkAcc (undefined :: Load)     acc
+             , mkAcc (undefined :: Required) acc
              ]
              ++
-             -- Extra fCodeTitle field for Dealer fields
              case fft of
-               Dealer ->
-                   [ varStrictType (mkName codeName) $
-                     ns $
-                     [t|
-                      F Text
-                      $(litT $ strTyLit codeName)
-                      $(litT $ strTyLit $ fdFormatted ff
-                        "Заголовок кода дилера для поля «%s»")|]
-                     ]
-               _ -> []
+               Name -> [mkAcc (undefined :: NameTitles)  acc]
+               _    -> [mkAcc (undefined :: Title)       acc]
              ++
-             -- Extra fFormat field for Date fields
+             -- Extra fCodeTitle field for Dealer fields, extra
+             -- fFormat field for Date fields
              case fft of
-               Date ->
-                   [ varStrictType (mkName formatName) $
-                     ns $
-                     [t|
-                      F (Maybe Text)
-                      $(litT $ strTyLit formatName)
-                      $(litT $ strTyLit $ fdFormatted ff
-                        "Формат даты для поля «%s»")|]
-                   ]
-               _ -> []
+               Dealer -> [mkAcc (undefined :: CodeTitle)  acc]
+               Date   -> [mkAcc (undefined :: DateFormat) acc]
+               _      -> []
              ++
              -- Default field except for Subprogram
              case fft of
                Subprogram -> []
-               _ ->
-                   [ varStrictType (mkName defaultName) $
-                     ns $
-                     [t|
-                      F $(return $ typeRepToType $ fieldType proj)
-                      $(litT $ strTyLit defaultName)
-                      $(litT $ strTyLit $
-                        fdFormatted ff "Значение поля «%s» по умолчанию")|]
-                   ]
+               _          -> [mkAcc (undefined :: Default) acc]
             )
             $
             formatFields
 
-        constructor = [recC name $ idnt ++ (concat $ map (snd . snd) fields)]
+        constructor = [recC typeName $ idnt ++ (concat $ map (snd . snd) fields)]
 
         vfas = map (fst . snd) fields
     in do
-      d  <- dataD (cxt []) name [] constructor [''Typeable]
+      d  <- dataD (cxt []) typeName [] constructor [''Typeable]
       d' <- [d|
-             vinFormatAccessors :: [VFAccessor $(conT name)]
+             vinFormatAccessors :: [VFAccessor $(conT typeName)]
              vinFormatAccessors = $(listE vfas)|]
       return $ [d] ++ d'
 
 
 -- | Base VinFormat accessors produced from a single source 'Contract'
 -- field, common to all field types.
-data VFAccessor m where
-    VFAcc :: { proj     :: (Typeable t, SingI n, SingI d) => Contract -> F t n d
-             -- ^ Original field @f@.
-             , load     :: (SingI n1, SingI d1) => m -> F Bool n1 d1
-             -- ^ @fLoad@ accessor.
-             , required :: (SingI n2, SingI d2) => m -> F Bool n2 d2
-             -- ^ @fRequired@ accessor.
-             } -> VFAccessor m
+data VFAccessor m =
+    forall n1 n2 d1 d2.
+    (SingI n1, SingI d1,
+     SingI n2, SingI d2) =>
+    VFAcc { proj     :: ContractField
+          -- ^ Original field @f@.
+          , load     :: m -> F Bool n1 d1
+          -- ^ @fLoad@ accessor.
+          , required :: m -> F Bool n2 d2
+          -- ^ @fRequired@ accessor.
+          }
