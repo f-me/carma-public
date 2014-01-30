360a46b048f3af9fc60bd2db21c4e1f12ede1511
cvb <peter@standalone.su>
2014-01-30 13:31:13 +0400

    handle appied indexes on create

--- database/patches/1.8.0-add-idxs-rkc.sql
+++ database/patches/1.8.0-add-idxs-rkc.sql
@@ -1,5 +1,8 @@
+DROP INDEX IF EXISTS actiontbl_closetime_idx;
 create index actiontbl_closetime_idx on actiontbl (closetime);
 
+DROP INDEX IF EXISTS actiontbl_duetime_full_idx;
 create index actiontbl_duetime_full_idx on actiontbl (duetime);
 
+DROP INDEX IF EXISTS casetbl_city_idx;
 create index casetbl_city_idx on casetbl (city);
