2d5ebfaf45def29a3f994107269b334a83bf4949
Max Taldykin <jorpic@gmail.com>
2014-02-06 11:39:14 +0400

    Fix Sms table
    - fix bug with `default 'now()'`
    - add carma_sms role
      - sms_svc will connect with this role
    - grant all on Sms to carma_sms
      - carma_sms don't add/delete rows, so it don't need permissions
      to Sms_id_seq

--- database/baseline/3-dictionaries/27-Sms.sql
+++ database/baseline/3-dictionaries/27-Sms.sql
@@ -1,12 +1,12 @@
 
 CREATE TABLE "Sms"
   (id        SERIAL PRIMARY KEY
-  ,ctime     timestamptz NOT NULL DEFAULT 'now()'
-  ,mtime     timestamptz NOT NULL DEFAULT 'now()'
+  ,ctime     timestamptz NOT NULL DEFAULT now()
+  ,mtime     timestamptz NOT NULL DEFAULT now()
   ,status    text NOT NULL DEFAUlT 'draft'
   ,caseRef   text -- REFERENCES "casetbl"
   ,phone     text NOT NULL
-  ,sender    text
+  ,sender    text NOT NULL DEFAULT 'RAMC'
   ,template  int4 REFERENCES "SmsTemplate"
   ,msgText   text NOT NULL
   ,foreignId text
@@ -14,5 +14,7 @@ CREATE TABLE "Sms"
 
 GRANT ALL ON "Sms" TO carma_search;
 GRANT ALL ON "Sms" TO carma_db_sync;
+GRANT ALL ON "Sms" TO carma_sms;
 GRANT ALL ON "Sms_id_seq" TO carma_search;
 GRANT ALL ON "Sms_id_seq" TO carma_db_sync;
+
