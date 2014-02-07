0ae6c0c72279fe5a570de6f975215a7223da28fd
cvb <peter@standalone.su>
2014-02-07 21:43:06 +0400

    refactor search

--- srv/src/Snaplet/Search.hs
+++ srv/src/Snaplet/Search.hs
@@ -1,52 +1,62 @@
+
 {-# LANGUAGE QuasiQuotes,
              TemplateHaskell,
              ScopedTypeVariables,
-             DeriveGeneric
+             DeriveGeneric,
+             TypeOperators,
+             FlexibleInstances,
+             FlexibleContexts,
+             OverlappingInstances,
+             UndecidableInstances
  #-}
 
 module Snaplet.Search (Search, searchInit)  where
 
-import Prelude hiding (pred)
-import Control.Applicative
-import Control.Monad
-import Control.Monad.State
-import Control.Lens hiding (from)
+import           Prelude hiding (pred)
+import           Control.Applicative ((<$>), (<*>))
+import           Control.Monad
+import           Control.Monad.State
+import           Control.Lens (makeLenses)
 
 import qualified Data.Map as M
-import Data.Monoid
-import Data.Maybe
-import Data.Either
-import Data.String (fromString)
-import Data.Pool
-import Data.Text (Text, toLower)
+import           Data.Monoid
+import           Data.Maybe
+import           Data.Either
+import           Data.String (fromString)
+import           Data.Pool
+import           Data.Text (Text, toLower)
 import qualified Data.Text             as T
 import qualified Data.ByteString.Char8 as B
 import qualified Data.ByteString.Lazy  as LB
 import qualified Data.HashMap.Strict   as HM
-import Text.Printf
+import           Text.Printf
 
-import qualified Data.Aeson as Aeson
-import           Data.Aeson (FromJSON)
+import           Data.Aeson
 
-import Snap.Core
-import Snap.Snaplet
-import Snap.Snaplet.Auth
-import Database.PostgreSQL.Simple as PG
-import Database.PostgreSQL.Simple.SqlQQ
+import           Snap.Core
+import           Snap.Snaplet
+import           Snap.Snaplet.Auth hiding (Role)
+import           Database.PostgreSQL.Simple as PG
+import           Database.PostgreSQL.Simple.SqlQQ
+import           Database.PostgreSQL.Simple.Types ((:.))
 
-import GHC.Generics
+import           GHC.Generics
 
-import Util
-import Utils.HttpErrors
+import           Util
+import           Utils.HttpErrors
 
 import qualified Data.Model       as M
 import qualified Data.Model.Types as M
 
-import Carma.Model
-import Carma.Model.Case
-import Carma.Model.Service
-import Carma.Model.Service.Towage
-import Carma.Model.Search
+import           Data.Model.Patch
+
+import           Carma.Model
+import           Carma.Model.Role
+import           Carma.Model.Case
+import           Carma.Model.Service
+import           Carma.Model.Service.Towage
+import           Carma.Model.Search
+import           Carma.Model.FieldPermission
 
 
 data Search b = Search
@@ -59,7 +69,7 @@ makeLenses ''Search
 type SearchHandler b t = Handler b (Search b) t
 
 
-data SearchReq = SearchReq { predicates :: Aeson.Value
+data SearchReq = SearchReq { predicates :: Value
                            , sorts      :: SearchSorts
                            } deriving (Show, Generic)
 
@@ -76,37 +86,46 @@ data SimpleField = SimpleField { name :: Text, model :: Text }
 
 instance FromJSON SimpleField
 
-
-modelFields :: UserId -> Text -> PG.Connection -> IO [Text]
-modelFields uid modelName c
-  = concat <$> query c q (modelName, unUid uid)
-  where
-    q = [sql|
-      with
-        model_name as (select (? || 'tbl')::text as table),
-        usr_role as
-          (select unnest(roles) as role from usermetatbl where uid = ?),
-        svc_field as
-          (select column_name as column
-            from information_schema.columns, model_name
-            where table_name = model_name.table),
-        svc_model as
-          (select c1.relname as table
-            from pg_inherits p, pg_class c1, pg_class c2, model_name
-            where p.inhrelid = c1.oid
-              and p.inhparent = c2.oid
-              and c2.relname = model_name.table)
-        select distinct field
-          from "FieldPermission" p, svc_field f, usr_role r,
-            (select * from svc_model union select * from model_name) m
-          where (model || 'tbl') = m.table
-            and field ilike f.column
-            and p.role = r.role :: int
-            and p.r
-      |]
+data SearchResult t = SearchResult 
+  { values :: [t]
+  , limit  :: Int
+  , offset :: Int
+  } deriving (Generic)
+instance ToJSON t => ToJSON (SearchResult t)
 
 
-caseSearch :: SearchHandler b (Either String Aeson.Value)
+instance forall m.(Model m) => ToJSON (Patch m :. ()) where
+  toJSON (p :. ()) =
+    object [(M.modelName (M.modelInfo :: M.ModelInfo m)) .= toJSON p]
+
+instance forall m b.(Model m, ToJSON b) => ToJSON (Patch m :. b) where
+  toJSON (p :. ps) = merge
+    (object [(M.modelName (M.modelInfo :: M.ModelInfo m)) .= toJSON p])
+    (toJSON ps)
+    where
+      merge :: Value -> Value -> Value
+      merge (Object o1) (Object o2) =
+        Object $ HM.fromList $ (HM.toList o1) ++ (HM.toList o2)
+
+
+class StripRead p where
+  stripRead :: Connection -> [IdentI Role] -> p -> IO p
+
+instance (Model m, Model (M.Parent m)) => StripRead (Patch m) where
+  stripRead = stripReadPatch
+instance (Model m, Model (M.Parent m)) => StripRead (Patch m :. ()) where
+  stripRead c rs (p :. ()) = do
+    s <- stripReadPatch c rs p
+    return $ s :. ()
+instance (Model m, Model (M.Parent m), StripRead ps)
+         => StripRead (Patch m :. ps) where
+  stripRead c rs (p :. ps) = do
+    s  <- stripReadPatch c rs p
+    ss <-  stripRead c rs ps
+    return $ s :. ss
+
+
+caseSearch :: SearchHandler b (Either String Value)
 caseSearch = do
   Just usr <- with auth currentUser
   let Just uid = userId usr
@@ -114,56 +133,43 @@ caseSearch = do
   offset   <- getOffset
   args     <- getJsonBody
   let fs = fields $ sorts args
-      ord = order $ sorts args
-      ms = map (\(SimpleField n m) -> (n, dispatch m getModelTable )) fs
-      ms' = T.intercalate ", " $ map (printOrder ord) ms
   withPG $ \c -> do
-    cse_fields <- modelFields uid "case" c
-    svc_fields <- modelFields uid "service" c
-    tow_fields <- modelFields uid "towage" c
     casePreds  <- predicatesFromParams c (predicates args) caseSearchParams
     srvPreds   <- predicatesFromParams c (predicates args) serviceSearchParams
     towPreds   <- predicatesFromParams c (predicates args) towageSearchParams
     case partitionEithers [casePreds, srvPreds, towPreds] of
       ([], preds) -> do
-        s <- query_ c (mkQuery (concatPredStrings preds) lim offset (renderOrder ms'))
-        let s' = map (filterResults cse_fields svc_fields tow_fields . parse) s
-        return $ Right $ reply lim offset $ merge s'
+        s  <- query_ c (mkQuery (concatPredStrings preds) 1 offset "")
+        s' <- mapM (parsePatch c []) s
+        return $ Right $ toJSON $ SearchResult s' lim offset
       (errs, _) -> return $ Left $ foldl (++) "" errs
   where
-    -- FIXME: do something so Postgresql.Simple can parse json to Aeson.Value
-    getModelTable :: forall m.Model m => m -> Text
-    getModelTable _ = M.tableName (M.modelInfo :: M.ModelInfo m)
-    printOrder o (n, t) = T.pack $ printf "%s.%s %s"
-                          (T.unpack $ fromJust t)
-                          (T.unpack n)
-                          (T.unpack o)
-    renderOrder "" = ""
-    renderOrder v = T.pack $ printf "ORDER BY %s" $ T.unpack v
-    parse (a, b, c) =
-      (fromMaybe (Aeson.object []) $ Aeson.decode a
-      ,Aeson.decode (fromMaybe "" b)
-      ,Aeson.decode (fromMaybe "" c)
-      )
-    filterResults cse svc _   (c, Just s, Nothing) =
-      (fltModel cse c, fltModel svc s, Aeson.Object HM.empty)
-    filterResults cse _    _  (c, Nothing, Nothing) =
-      (fltModel cse c, Aeson.Object HM.empty, Aeson.Object HM.empty)
-    filterResults cse svc tow (c, Just s, Just t) =
-      (fltModel cse c, fltModel svc s, fltModel tow t)
-    fltModel :: [Text] -> Aeson.Value -> Aeson.Value
-    fltModel fs (Aeson.Object o) = Aeson.Object $
-      foldl (\a k -> maybe a (\v -> HM.insert k v a) $ HM.lookup k o)
-        HM.empty $ map toLower fs -- use lowercase fieldsnames because
-                                  -- so will postgres
-    merge [] = []
-    merge (x:xs) = (merge' x) : merge xs
-    merge' (c, s, t) = Aeson.object [ ("case",    c)
-                                    , ("service", Aeson.toJSON s)
-                                    , ("towage",  Aeson.toJSON t)
-                                    ]
+    parsePatch conn [] [c, s, t] = stripRead conn [] $
+      (parse c :: Patch Case)    :.
+      (parse s :: Patch Service) :.
+      (parse t :: Patch Towage)  :.
+      ()
+    parse :: Model m => Maybe LB.ByteString -> Patch m
+    parse (Just v) = parsePgJson v
+    parse Nothing  = empty
 
-search :: SearchHandler b (Either String Aeson.Value) -> SearchHandler b ()
+
+parsePgJson :: forall m.Model m => LB.ByteString -> Patch m
+parsePgJson bs =
+  fromMaybe empty $ decode bs >>= decodeJs >>= fromResult . fromJSON
+  where
+    fromResult (Error s)   = Nothing
+    fromResult (Success r) = Just r
+    decodeJs (Object obj) =
+      Just $ Object $ HM.foldlWithKey' fixName HM.empty obj
+    decodeJs _ = Nothing
+    fsMap    = M.modelFieldsMap (M.modelInfo :: M.ModelInfo m)
+    namesMap =
+      foldl (\a k -> HM.insert (toLower k) k a) HM.empty $ HM.keys fsMap
+    fixName h k v = maybe h (\f -> HM.insert f v h) $ HM.lookup k namesMap
+
+
+search :: SearchHandler b (Either String Value) -> SearchHandler b ()
 search = (>>= either (finishWithError 500) writeJSON)
 
 
@@ -188,22 +194,13 @@ searchInit conn sessionMgr = makeSnaplet "search" "Search snaplet" Nothing $ do
   addRoutes [("services", method POST $ search caseSearch)]
   return $ Search conn sessionMgr
 
-reply :: Int -> Int -> [Aeson.Value] -> Aeson.Value
-reply lim offset val =
-  let next = if length val < lim then Nothing else Just (offset + lim)
-      prev = if offset <= 0      then Nothing else Just (offset - lim)
-  in Aeson.object [ ("values", Aeson.toJSON val)
-                  , ("next", Aeson.toJSON next)
-                  , ("prev", Aeson.toJSON prev)
-                  ]
-
 
 -- Utils
 ----------------------------------------------------------------------
 withPG :: (Connection -> IO a) -> SearchHandler b a
 withPG f = gets postgres >>= liftIO . (`withResource` f)
 
-getJsonBody :: Aeson.FromJSON v => SearchHandler b v
+getJsonBody :: FromJSON v => SearchHandler b v
 getJsonBody = Util.readJSONfromLBS <$> readRequestBody 4096
 
 getLimit :: SearchHandler b Int
@@ -216,7 +213,7 @@ getOffset
   = fromMaybe 0 . (>>= fmap fst . B.readInt)
   <$> getParam "offset"
 
-writeJSON :: Aeson.ToJSON v => v -> Handler a b ()
+writeJSON :: ToJSON v => v -> Handler a b ()
 writeJSON v = do
   modifyResponse $ setContentType "application/json"
-  writeLBS $ Aeson.encode v
+  writeLBS $ encode v
