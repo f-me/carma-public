00b401071fcacffb81fa7319b8ba92188e4f4b88
Max Taldykin <jorpic@gmail.com>
2014-02-21 23:52:48 +0400

    Fix db patches

--- database/patches/1.9.0-update-sms-table.sh
+++ database/patches/1.9.0-update-sms-table.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-$PSQL -c 'drop table "Sms"'
+$PSQL -c 'drop table if exists "Sms"'
 HAS_ROLE=`$PSQL -t -c "select * from pg_roles where rolname='carma_sms'"`
 if [[ $HAS_ROLE == "" ]] ; then
   $PSQL -c "create role carma_sms with password 'pass' login"
