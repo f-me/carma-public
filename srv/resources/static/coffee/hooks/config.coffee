f-me/carma-public

commit d9fdc1005eaa17a6837b6f1f5b6acc1ea1641343

IdentList client support and subprogram services #1330
IdentList type is a new reference type (one-to-many). Referenced
instances must be of the same model (set by meta.model, which is
supported by carma-models `IdentList m` type). Referenced model must
have parent field in which parent id is stored as a number.

Server-side support has been implemented in f-me/carma-models@c70399b.

Comparison between reference and IdentList types:

reference value: "foo:1,bar:2,baz:3"
IdentList value: [1, 2, 3]

reference parent: "case:32189"
IdentList parent: 32189

Subprogram.services field is of type int[] again

Refactored bindRemove to bindRemoveHook

srv/resources/static/coffee/hooks/config.coffee
@@ -50,7 +50,10 @@ define [ "hooks/common"
 
       "ken" : [ s.openPartnerSearch ]
 
-      "partner" : [p.bindRemoveService, p.serviceRepeat, p.factAddr]
+      "partner" : [ c.bindRemoveHook 'services'
+                  , p.serviceRepeat
+                  , p.factAddr
+                  ]
 
       "partner_service" : [ p.bindTitleServiceName
                           , p.partnerServiceRepeat
@@ -108,3 +111,5 @@ define [ "hooks/common"
                  , s.costsMark
                  , s.openPartnerSearch
                  ]
+
+      "SubProgram" : [ c.bindRemoveHook 'services' ]

database/baseline/3-dictionaries/22-Program.sql
@@ -4,7 +4,7 @@ CREATE TABLE "Program"
   , client text
   , clientAddress text
   , clientCode text
-  , managers int4[] -- ELEMENT REFERENCES
+  , managers int4[] -- ELEMENT REFERENCES Usermeta
   , pType int4 REFERENCES "ProgramType"
   );
 

database/baseline/3-dictionaries/23-SubProgram.sql
@@ -8,7 +8,7 @@ CREATE TABLE "SubProgram"
   , mailAddr text
   , mailPass text
   , contacts text
-  , services text
+  , services int4[] -- ELEMENT REFERENCES SubProgramService
   , checkPeriod integer
   , validFor integer
   , contract text

database/baseline/3-dictionaries/36-SubProgramService.sql
@@ -1,6 +1,6 @@
 CREATE TABLE "SubProgramService"
   ( id    SERIAL PRIMARY KEY
-  , parent int4 REFERENCES "SubProgram"
+  , parent int4 REFERENCES "SubProgram" NOT NULL
   , type int4 REFERENCES "ServiceNames"
   , maxCost text
   , maxDistance integer

database/patches/1.37.0-SubProgramService-parent-not-null.sql
@@ -1,0 +1,2 @@
+ALTER TABLE "SubProgramService" DROP COLUMN parent;
+ALTER TABLE "SubProgramService" ADD COLUMN parent int4 REFERENCES "SubProgram" NOT NULL;

database/patches/1.38.0-SubProgram-services-int.sql
@@ -1,0 +1,2 @@
+ALTER TABLE "SubProgram" DROP COLUMN services;
+ALTER TABLE "SubProgram" ADD COLUMN services int4[];

srv/resources/static/coffee/hooks/common.coffee
@@ -278,3 +278,7 @@ define [ "utils"
       e.parents(".accordion-group")[0].scrollIntoView()
     f = e.find(".focusable")[0]
     f and f.focus()
+
+  bindRemoveHook: (fieldName) ->
+    (model, kvm) ->
+      kvm[fieldName].subscribe -> u.bindRemove kvm, fieldName

srv/resources/static/coffee/hooks/partner.coffee
@@ -10,9 +10,6 @@ define ["utils"], (u) ->
   bindTitleServiceName: (model, kvm) ->
     kvm['modelTitle'] = kvm['serviceNameLocal']
 
-  bindRemoveService: (model, kvm) ->
-    kvm['services'].subscribe -> u.bindRemove kvm, 'services'
-
   serviceRepeat: (model, kvm) ->
     kvm['serviceRepeat'] = ko.observableArray([])
     kvm['services'].subscribe -> setServiceRepeat()

commit 04fdbd3c947933440330f7839c7e3e95ba7806e6

Fix #partner hooks broken in d9fdc10

srv/resources/static/coffee/hooks/config.coffee
@@ -50,7 +50,7 @@ define [ "hooks/common"
 
       "ken" : [ s.openPartnerSearch ]
 
-      "partner" : [ c.bindRemoveHook 'services'
+      "partner" : [ (c.bindRemoveHook 'services')
                   , p.serviceRepeat
                   , p.factAddr
                   ]