0f9c4b2ed48b34849c6c6ad002fc2c2921dad343
Dmitry Dzhus <dima@dzhus.org>
2014-04-07 20:08:46 +0400

    Merge fordPlus & ford #1451

--- database/patches/0.1.12-migrate-case-programs.sql
+++ database/patches/0.1.12-migrate-case-programs.sql
@@ -1,8 +1,11 @@
 -- Merge opeldo & opel programs
 UPDATE casetbl SET program = 'opel' WHERE program = 'opeldo';
 
+-- Merge fordPlus & ford
+UPDATE casetbl SET program = 'ford' WHERE program = 'fordPlus';
+
 -- Migrate cases with known programs
-WITH remap AS 
+WITH remap AS
 (SELECT old.value, p.id AS pid, sp.id AS sid
  FROM programtbl old, "Program" p, "SubProgram" sp
  WHERE sp.parent=p.id AND sp.value=old.value)
