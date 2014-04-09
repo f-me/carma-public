6d4c129cde693a7f903c539ecd1efb31b51b9021
Dmitry Dzhus <dima@dzhus.org>
2014-04-10 00:03:13 +0400

    Transfer partner synonyms from VINNIE #1326 #1604

--- tools/transfer-vinnie-data.sh
+++ tools/transfer-vinnie-data.sh
@@ -17,12 +17,13 @@ create table "CarModel_syn" as select id, synonyms from "CarModel" where synonym
 create table "Engine_syn" as select id, synonyms from "Engine" where synonyms is not null;
 create table "Transmission_syn" as select id, synonyms from "Transmission" where synonyms is not null;
 create table "SubProgram_syn" as select id, synonyms from "SubProgram" where synonyms is not null;
+create table "partnertbl_syn" as select id, synonyms from "partnertbl" where synonyms is not null;
 EOF
 )
 
 psql -h ${FROMHOST} -U carma_db_sync ${FROMDB} -c "${DUMP}"
-pg_dump -h ${FROMHOST} -U carma_db_sync ${FROMDB} -t '"Contract_tmp"' -t '"VinFormat_tmp"' -t '"CarMake_syn"' -t '"CarModel_syn"' -t '"Engine_syn"' -t '"Transmission_syn"' -t '"SubProgram_syn"' > /tmp/Transfer.sql
-psql -h ${FROMHOST} -U carma_db_sync ${FROMDB} -c 'DROP TABLE "Contract_tmp","VinFormat_tmp","CarMake_syn","CarModel_syn","Engine_syn","Transmission_syn","SubProgram_syn";'
+pg_dump -h ${FROMHOST} -U carma_db_sync ${FROMDB} -t '"Contract_tmp"' -t '"VinFormat_tmp"' -t '"CarMake_syn"' -t '"CarModel_syn"' -t '"Engine_syn"' -t '"Transmission_syn"' -t '"SubProgram_syn"' -t '"partnertbl_syn"' > /tmp/Transfer.sql
+psql -h ${FROMHOST} -U carma_db_sync ${FROMDB} -c 'DROP TABLE "Contract_tmp","VinFormat_tmp","CarMake_syn","CarModel_syn","Engine_syn","Transmission_syn","SubProgram_syn","partnertbl_syn";'
 
 
 # TARGET:
@@ -34,7 +35,8 @@ update "CarModel" set synonyms = "CarModel_syn".synonyms from "CarModel_syn" whe
 update "Engine" set synonyms = "Engine_syn".synonyms from "Engine_syn" where "Engine".id="Engine_syn".id;
 update "Transmission" set synonyms = "Transmission_syn".synonyms from "Transmission_syn" where "Transmission".id="Transmission_syn".id;
 update "SubProgram" set synonyms = "SubProgram_syn".synonyms from "SubProgram_syn" where "SubProgram".id="SubProgram_syn".id;
-DROP TABLE "Contract_tmp","VinFormat_tmp","CarMake_syn","CarModel_syn","Engine_syn","Transmission_syn","SubProgram_syn";
+update "partnertbl" set synonyms = "partnertbl_syn".synonyms from "partnertbl_syn" where "partnertbl".id="partnertbl_syn".id;
+DROP TABLE "Contract_tmp","VinFormat_tmp","CarMake_syn","CarModel_syn","Engine_syn","Transmission_syn","SubProgram_syn","partnertbl_syn";
 EOF
 )
 
