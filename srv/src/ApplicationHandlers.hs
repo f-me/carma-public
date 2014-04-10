5843cc9ef908fc05b7ba383d2ad0304e7c334118
Dmitry Dzhus <dima@dzhus.org>
2014-04-10 13:11:35 +0400

    Revert "Mimic DB.update in updateHandler too #1352"
    This reverts commit 796bda9ffa44a67b699f1af2959ac95cfb6a0fa8.
    
    DB.update returns only changed fields, returning all fields breaks the client (#1801)

--- srv/src/ApplicationHandlers.hs
+++ srv/src/ApplicationHandlers.hs
@@ -245,28 +245,19 @@ updateHandler :: AppHandler ()
 updateHandler = do
   Just model <- getParam "model"
   Just objId <- getParam "id"
-  let updateModel :: forall m . Model m => m -> AppHandler (Either Int Aeson.Value)
+  let updateModel :: forall m . Model m => m -> AppHandler ()
       updateModel _ = do
         let ident = readIdent objId :: IdentI m
         commit <- getJSONBody :: AppHandler (Patch m)
-        s   <- PS.getPostgresState
         res <- with db $ do
+          s   <- PS.getPostgresState
           liftIO $ withResource (PS.pgPool s) (Patch.update ident commit)
         case res of
-          0 -> return $ Left 404
-          _ -> do
-            res <- liftIO $ withResource (PS.pgPool s) (Patch.read ident)
-            -- TODO Cut out fields from original commit like DB.update
-            -- does
-            case (Aeson.decode $ Aeson.encode res) of
-              Just [obj] -> return $ Right obj
-              err        -> error $ "BUG in updateHandler: " ++ show err
+          0 -> handleError 404
+          _ -> writeJSON $ Aeson.object []
   -- See also Utils.NotDbLayer.update
   case Carma.Model.dispatch (T.decodeUtf8 model) updateModel of
-    Just fn ->
-        fn >>= \case
-           Left n -> handleError n
-           Right o -> logResp $ return o
+    Just fn -> fn
     Nothing -> do
       commit <- getJSONBody
       logReq commit
