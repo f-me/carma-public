80290635083c0b23b0f42b0ccafca4646e0617c4
Dmitry Dzhus <dima@dzhus.org>
2014-04-12 01:08:57 +0400

    Fix Rent.carClass type and metas f-me/carma#1831

--- carma-models/src/Carma/Model/Service/Rent.hs
+++ carma-models/src/Carma/Model/Service/Rent.hs
@@ -1,12 +1,13 @@
-
 module Carma.Model.Service.Rent where
 
+import Data.Aeson as Aeson
 import Data.Text
 import Data.Typeable
 
 import Data.Model
 import Data.Model.View
 import Carma.Model.LegacyTypes
+import Carma.Model.CarClass (CarClass)
 import Carma.Model.Service (Service)
 
 
@@ -21,7 +22,7 @@ data Rent = Rent
   , rentAddress_coords  :: F PickerField "rentAddress_coords" "Координаты"
   , rentAddress_map     :: F MapField "rentAddress_map" ""
   , vinRent             :: F Text "vinRent" "VIN подменного автомобиля"
-  , carClass            :: F (IdentT CarClasses) "carClass" "Класс автомобиля"
+  , carClass            :: F (IdentT CarClass) "carClass" "Класс автомобиля"
   , providedFor         :: F Text "providedFor"
                            "Срок, на который предоставлен автомобиль (дней)"
   , rentedMake          :: F (IdentT CarMakers) "rentedMake"
@@ -41,6 +42,8 @@ instance Model Rent where
     Just mv -> Just
       $ modifyView (mv {mv_title = "Подменный автомобиль"})
       $ setType "dictionary" towDealer_partnerId
+      : setMeta "dictionaryType" (Aeson.String "ModelDict") carClass
+      : setMeta "dictionaryStringify" (Aeson.Bool True) carClass
       : setMeta "widget" "partner" towDealer_partner
       : invisible towDealer_partnerId
       : invisible towDealer_coords
