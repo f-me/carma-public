00e0f195d7c143021a8a595bf9d11c89b47b4112
Dmitry Dzhus <dima@dzhus.org>
2014-04-20 18:04:57 +0400

    SmsTokenName idents

--- carma-models/src/Carma/Model/SmsTokenName.hs
+++ carma-models/src/Carma/Model/SmsTokenName.hs
@@ -1,11 +1,16 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Carma.Model.SmsTokenName where
 
 import Data.Text
 import Data.Typeable
+
 import Data.Model
+import Data.Model.TH
 import Data.Model.View
+
 import Carma.Model.Types()
 import Carma.Model.PgTypes()
+
 
 data SmsTokenName = SmsTokenName
   { ident   :: PK Int SmsTokenName "Переменные шаблонов СМС"
@@ -13,6 +18,14 @@ data SmsTokenName = SmsTokenName
   , varName :: F Text "var_name" "Название переменной"
   }
   deriving Typeable
+
+
+mkIdents [t|SmsTokenName|]
+ [ ("program_contact_info", 1)
+ , ("program_from_name", 2)
+ , ("program_info", 3)
+ ]
+
 
 instance Model SmsTokenName where
   type TableName SmsTokenName = "SmsTokenName"
