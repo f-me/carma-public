9122ccb3fa8570e29e759f3a062c6960015390b2
cvb <peter@standalone.su>
2014-05-15 19:49:24 +0400

    better timeline drawing #1893

--- srv/src/AppHandlers/Users.hs
+++ srv/src/AppHandlers/Users.hs
@@ -166,8 +166,10 @@ serveUserStates = do
   to    <- readDay <$> getParam "to"
   states <- withPG pg_search $ \c -> do
     query c (fromString $ printf
+      -- Get more then asked, we need this during drawing of timeline
       ("SELECT %s FROM \"UserState\" WHERE userId = ? " ++
-       " AND ctime BETWEEN ? AND timestamp ? + interval '1 day'" ++
+       " AND ctime BETWEEN timestamp ? - interval '1 week' " ++
+       "           AND     timestamp ? + interval '1 week' " ++
        " ORDER BY id ASC"
       )
       (T.unpack $ mkSel (undefined :: Patch UserState))) $
