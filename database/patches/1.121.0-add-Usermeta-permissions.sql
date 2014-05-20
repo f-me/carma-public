4ad929561cb3341269e126a5090594ac1fffd454
cvb <peter@standalone.su>
2014-05-20 14:18:15 +0400

    add permissions for Usermeta model

---
+++ database/patches/1.121.0-add-Usermeta-permissions.sql
@@ -0,0 +1,12 @@
+BEGIN;
+  INSERT INTO "FieldPermission" (role, model, field, r, w)
+  SELECT role, 'Usermeta' as model, field, r, w
+  FROM "FieldPermission"
+  WHERE model = 'usermeta';
+
+  -- 12 - supervisor
+  INSERT INTO "FieldPermission" (role, model, field, r, w)
+  VALUES (12, 'Usermeta', 'bocities', 't', 't'),
+         (12, 'Usermeta', 'boprograms', 't', 't'),
+         (12, 'Usermeta', 'businessRole', 't', 't');
+COMMIT;
