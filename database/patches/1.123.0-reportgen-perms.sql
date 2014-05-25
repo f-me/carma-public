31b2e67cfd4d53d1882545da2259956f61d8dd1d
Dmitry Dzhus <dima@dzhus.org>
2014-05-25 15:07:53 +0400

    reportgen permissions #1946

---
+++ database/patches/1.123.0-reportgen-perms.sql
@@ -0,0 +1,6 @@
+GRANT SELECT ON "Program" TO reportgen;
+GRANT SELECT ON "SubProgram" TO reportgen;
+GRANT SELECT ON "CarMake" TO reportgen;
+GRANT SELECT ON "CarModel" TO reportgen;
+GRANT SELECT ON "ProgramType" TO reportgen;
+GRANT SELECT ON partnertbl TO reportgen;
