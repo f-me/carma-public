ea30bda44ad54242cfbefbdeb4934c4ae8f43298
Dmitry Dzhus <dima@dzhus.org>
2014-05-25 14:53:22 +0400

    New views #1939

---
+++ database/baseline/5-views/5-ru-partners.sql
@@ -0,0 +1,75 @@
+﻿CREATE VIEW "Партнёры" AS
+Select
+--ФУНКЦИЯ YESNO
+        (CASE
+                WHEN
+                        partnertbl.isActive = true
+                THEN
+                        '+'
+                ELSE
+                        '-'
+                END)
+        AS "Патрнер активен",
+--ФУНКЦИЯ YESNO
+        (CASE
+                WHEN
+                        partnertbl.isDealer = true
+                THEN
+                        '+'
+                ELSE
+                        '-'
+                END)
+        AS "Дилер",
+--ФУНКЦИЯ YESNO
+        (CASE
+                WHEN
+                        partnertbl.isMobile = true
+                THEN
+                        '+'
+                ELSE
+                        '-'
+                END)
+        AS "Мобильный партнёр",
+partnertbl.name AS "Название партнёра",
+partnertbl.code AS "Код",
+--LOOKUP(DealerCities, partnertbl.city) вместо dictionaries/DealerCities.json
+        "City".label AS "Город",
+makes AS "Обслуживаемые марки",
+partnertbl.personInCharge AS "Ответственное лицо",
+--partnertbl.taxScheme AS "Форма налогообложения",
+        "TaxScheme".label AS "Форма налогообложения",
+--ФУНКЦИЯ YESNO
+        (CASE
+                WHEN
+                        partnertbl.isPayBackConfirmed = true
+                THEN
+                        '+'
+                ELSE
+                        '-'
+                END)
+        AS "Соглашение о вознаграждении",
+partnertbl.comment AS "Комментарий",
+--Далее идут поля, которых нету в шаблоне отчета по партнерам:
+partnertbl.services AS "Услуги",
+--ЧТО ЭТО?
+        --partnertbl.garbage,
+coords AS "Координаты",
+--ЧТО ЭТО? Чем отличается от makes?
+        --makers, --СТАРОЕ ПОЛЕ
+--ЧТО ЭТО?
+        --mtime, -- время последнего обновления данных о партнёре через партнёрское приложение
+--JSON
+addrs AS "Адреса",
+--JSON
+phones AS "Телефоны",
+emails AS "Электронная почта",
+--ЧТО ЭТО?
+        --isfree, -- свободен ли партнёр (используется для мобильных партнёров, у них кнопочка в приложении есть)
+foreignident AS "Интеграционный код",
+synonyms AS "Синонимы"
+FROM partnertbl
+LEFT JOIN "City" ON partnertbl.city = "City".value
+LEFT JOIN "TaxScheme" ON partnertbl.taxScheme::Integer = "TaxScheme".id;
+
+GRANT SELECT ON "Партнёры" TO reportgen;
+GRANT ALL ON "Партнёры" TO analyst;
---
+++ database/baseline/5-views/6-ru-contracts.sql
@@ -0,0 +1,62 @@
+﻿CREATE VIEW "Контракты" AS
+SELECT "Contract".id AS "Номер контракта",
+--ФУНКЦИИ TIME (ВРЕМЯ ПО МОСКВЕ) НЕ РЕАЛИЗУЕТС ВО VIEW, БУДЕТ РЕАЛИЗОВАНА В JASPERSOFT STUDIO (формат  'DD/MM/YYYY HH24:MI')
+    "Contract".ctime AT TIME ZONE 'Europe/Moscow' AS "Время создания контракта",
+        CASE "Contract".isactive
+            WHEN true THEN '+'::text
+            ELSE '-'::text
+        END AS "Активен",
+    "Contract".name AS "ФИО клиента",
+    "Contract".email AS "E-mail клиента",
+    "Contract".vin AS "VIN",
+    "Contract".cardnumber AS "Номер карты",
+    "Contract".codeword AS "Кодовое слово",
+    "Contract".phone AS "Номер телефона",
+    "Contract".platenum AS "Госномер",
+    "Contract".validsince AS "Дата регистрации в программе",
+    "Contract".validuntil AS "Программа действует до (Дата)",
+    "Contract".startmileage AS "Пробег при регистрации в программ",
+    "CarMake".label AS "Марка",
+    "CarModel".label AS "Модель",
+    "Contract".makeyear::Integer AS "Год производства автомобиля",
+    "CarClass".label AS "Класс автомобиля",
+    "Contract".color AS "Цвет",
+    "Transmission".label AS "Коробка передач",
+    "Contract".enginevolume AS "Объём двигателя",
+    "Engine".label AS "Тип двигателя",
+    "Contract".buydate AS "Дата покупки",
+    p2.name AS "Дилер, продавший автомобиль",
+    p1.name AS "Дилер, у которого проходило послед",
+    "Contract".checkperiod AS "Межсервисный интервал",
+    "CheckType".label AS "Вид ТО",
+    "Contract".ordernumber AS "Номер заказ-наряда",
+    "Contract".managername AS "ФИО менеджера",
+    "Contract".comment AS "Комментарий",
+    "ProgramType".label AS "Тип программы",
+    "Program".label AS "Программа",
+    "SubProgram".label AS "Подпрограмма",
+    "LegalForm".label AS "Физическое/юридическое лицо",
+    usermetatbl.realname AS "Пользователь, внёсший данные",
+        CASE "Contract".dixi
+            WHEN true THEN '+'::text
+            ELSE '-'::text
+        END AS "Заполнен"
+   FROM "Contract"
+   LEFT JOIN "CarClass" ON "Contract".carclass = "CarClass".id
+   LEFT JOIN "CheckType" ON "Contract".checktype = "CheckType".id
+   LEFT JOIN usermetatbl ON "Contract".committer = usermetatbl.id
+   LEFT JOIN "Engine" ON "Contract".enginetype = "Engine".id
+   LEFT JOIN partnertbl p1 ON "Contract".lastcheckdealer = p1.id
+   LEFT JOIN "LegalForm" ON "Contract".legalform = "LegalForm".id
+   LEFT JOIN "CarMake" ON "Contract".make = "CarMake".id
+   LEFT JOIN "CarModel" ON "Contract".model = "CarModel".id
+   LEFT JOIN partnertbl p2 ON "Contract".seller = p2.id
+   LEFT JOIN "SubProgram" ON "Contract".subprogram = "SubProgram".id
+   LEFT JOIN "Program" ON "SubProgram".parent = "Program".id
+   LEFT JOIN "ProgramType" ON "Program".ptype = "ProgramType".id
+   LEFT JOIN "Transmission" ON "Contract".transmission = "Transmission".id
+
+   ORDER BY ctime;
+
+GRANT SELECT ON "Контракты" TO reportgen;
+GRANT ALL ON "Контракты" TO analyst;
