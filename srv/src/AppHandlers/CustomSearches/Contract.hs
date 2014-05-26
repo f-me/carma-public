8deceb978990185597395951a9b3dd915dafbd7e
Dmitry Dzhus <dima@dzhus.org>
2014-05-26 05:57:13 +0400

    Order contracts by validSince #1853

--- srv/src/AppHandlers/CustomSearches/Contract.hs
+++ srv/src/AppHandlers/CustomSearches/Contract.hs
@@ -145,8 +145,8 @@ searchContracts = do
           , "AND (? OR ? = p.?)"
           -- 2 parameters: dixi and isActive field names
           , "AND c.? and c.?"
-          -- 1 parameter: LIMIT value
-          , "ORDER BY c.id DESC LIMIT ?;"
+          -- 2 parameters: ordering field name and LIMIT value
+          , "ORDER BY c.? DESC LIMIT ?;"
           ]
       -- Fields selected from matching rows
       selectedFieldsParam =
@@ -181,7 +181,7 @@ searchContracts = do
           :. (Only $ PT $ fieldName P.ident)
           -- 2
           :. (PT $ fieldName C.dixi, PT $ fieldName C.isActive)
-          -- 1
-          :. Only limit)
+          -- 2
+          :. (PT $ fieldName C.validSince, limit))
 
   writeJSON (res :: [SearchResult])
