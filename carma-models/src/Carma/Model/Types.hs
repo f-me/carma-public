43eabbd3526e334c16cab8ab0a55a1985c0d5e57
Max Taldykin <jorpic@gmail.com>
2014-06-10 00:27:39 +0400

    Reimplement instance ToField (interval x)
    This is less efficient but more concise

--- carma-models/src/Carma/Model/Types.hs
+++ carma-models/src/Carma/Model/Types.hs
@@ -28,28 +28,21 @@ import Data.Vector (Vector, (!))
 import qualified Data.Map as Map
 
 import Data.Time
-import Data.Time.Calendar ()
-import Data.Fixed (Pico)
+import System.Locale (defaultTimeLocale)
 
 import Text.Read (readMaybe)
+import Text.Printf
 
 import Database.PostgreSQL.Simple.FromField (FromField(..)
                                             ,ResultError(..)
                                             ,typename
                                             ,returnError)
 import Database.PostgreSQL.Simple.ToField   (ToField(..), Action(..), inQuotes)
+import qualified Blaze.ByteString.Builder.Char8 as Builder
 
-import Data.ByteString.Internal (w2c)
-import Blaze.ByteString.Builder(Builder, fromByteString)
-import Blaze.ByteString.Builder.Char8(fromChar)
-import Blaze.Text.Int(integral)
-
-import Data.Typeable
-import Data.Monoid (Monoid, (<>))
-
-import Unsafe.Coerce
 import GHC.TypeLits
 import GHC.Generics
+import Data.Typeable
 
 import Data.Model
 import Data.Model.Types
@@ -59,7 +52,7 @@ import Carma.Model.LegacyTypes
 -- ISO 8601
 instance FromJSON Day where
   parseJSON (String s)
-    = case parseTime undefined "%Y-%m-%d" $ T.unpack s of
+    = case parseTime defaultTimeLocale "%Y-%m-%d" $ T.unpack s of
       Just day -> return day
       Nothing  -> fail $ "invalid date format: " ++ show s
   parseJSON v = fail $ "invalid date: " ++ show v
@@ -71,7 +64,7 @@ instance ToJSON Day where
 newtype Dict m = Dict Text
    deriving (FromField, ToField,
              FromJSON, ToJSON,
-             Typeable, Monoid, IsString)
+             Typeable, IsString)
 
 data Interval t = Interval t t deriving Typeable
 
@@ -91,13 +84,23 @@ instance (FromJSON t, Typeable t, Show t) => FromJSON (Interval t) where
 instance (Typeable t, ToJSON t, Show t) => ToJSON (Interval t) where
   toJSON (Interval begin end) = Array $ V.fromList [toJSON begin, toJSON end]
 
-instance FromField (Interval t) where fromField = undefined
+instance FromField (Interval t) where fromField = undefined -- FIXME: ?
 
 instance ToField   (Interval UTCTime) where
-  toField = Plain . inQuotes . utcTimeIntervalToBuilder
+  toField (Interval begin end)
+    = Plain $ inQuotes $ Builder.fromString
+    $ printf "[%s,%s]"
+      (formatTime defaultTimeLocale "%0Y-%m-%d %T+00" begin)
+      (formatTime defaultTimeLocale "%0Y-%m-%d %T+00" end)
 
 instance ToField   (Interval Day) where
-  toField = Plain . inQuotes . dayIntervalToBuilder
+  toField (Interval begin end)
+    = Plain $ inQuotes $ Builder.fromString
+    $ printf "[%s,%s]"
+      (formatTime defaultTimeLocale "%0Y-%m-d" begin)
+      (formatTime defaultTimeLocale "%0Y-%m-d" end)
+
+
 
 -- | Int wrapper which instructs CaRMa client to use JSON integers in
 -- commits.
@@ -123,81 +126,6 @@ instance ToJSON (IdentList m) where
 
 instance FromJSON (IdentList m) where
     parseJSON v = RL <$> parseJSON v
-
-utcTimeIntervalToBuilder :: Interval UTCTime -> Builder
-utcTimeIntervalToBuilder (Interval begin end) =
-  let (b, e) = (utcTimeToBuilder begin, utcTimeToBuilder end)
-  in fromChar '[' <> b <> fromChar ',' <> e <> fromChar ']'
-
-utcTimeToBuilder :: UTCTime -> Builder
-utcTimeToBuilder (UTCTime day time) =
-    dayToBuilder day <> fromChar ' '
-    <> timeOfDayToBuilder (timeToTimeOfDay time) <> fromByteString "+00"
-
-timeOfDayToBuilder :: TimeOfDay -> Builder
-timeOfDayToBuilder (TimeOfDay h m s) = do
-    pad2 h <> fromChar ':' <> pad2 m <> fromChar ':' <> showSeconds s
-
-showSeconds :: Pico -> Builder
-showSeconds xyz
-    | yz == 0   = pad2 x
-    | z  == 0   = pad2 x <> fromChar '.' <>  showD6 y
-    | otherwise = pad2 x <> fromChar '.' <>  pad6   y <> showD6 z
-  where
-    -- A kludge to work around the fact that Data.Fixed isn't very fast and
-    -- doesn't give me access to the MkFixed constructor.
-    (x_,yz) = (unsafeCoerce xyz :: Integer)     `quotRem` 1000000000000
-    x = fromIntegral x_ :: Int
-    (fromIntegral -> y, fromIntegral -> z) = yz `quotRem` 1000000
-
-dayIntervalToBuilder :: Interval Day -> Builder
-dayIntervalToBuilder (Interval begin end) =
-  let (b, e) = (dayToBuilder begin, dayToBuilder end)
-  in fromChar '[' <> b <> fromChar ',' <> e <> fromChar ']'
-
--- | p assumes its input is in the range [0..9]
-p :: Integral n => n -> Builder
-p n = fromChar (w2c (fromIntegral (n + 48)))
-{-# INLINE p #-}
-
--- | pad2 assumes its input is in the range [0..99]
-pad2 :: Integral n => n -> Builder
-pad2 n = let (a,b) = n `quotRem` 10 in p a <> p b
-{-# INLINE pad2 #-}
-
--- | pad4 assumes its input is positive
-pad4 :: (Integral n, Show n) => n -> Builder
-pad4 abcd | abcd >= 10000 = integral abcd
-          | otherwise     = p a <> p b <> p c <> p d
-  where (ab,cd) = abcd `quotRem` 100
-        (a,b)   = ab   `quotRem` 10
-        (c,d)   = cd   `quotRem` 10
-{-# INLINE pad4 #-}
-
-pad6 :: Int -> Builder
-pad6 xy = let (x,y) = xy `quotRem` 1000
-           in pad3 x <> pad3 y
-
-pad3 :: Int -> Builder
-pad3 abc = let (ab,c) = abc `quotRem` 10
-               (a,b)  = ab  `quotRem` 10
-            in p a <> p b <> p c
-
-showD3 :: Int -> Builder
-showD3 abc = case abc `quotRem` 100 of
-              (a, 0) -> p a
-              (a,bc) -> case bc `quotRem` 10 of
-                          (b,0) -> p a <> p b
-                          (b,c) -> p a <> p b <> p c
-
-showD6 :: Int -> Builder
-showD6 xy = case xy `quotRem` 1000 of
-              (x,0) -> showD3 x
-              (x,y) -> pad3 x <> showD3 y
-
-dayToBuilder :: Day -> Builder
-dayToBuilder (toGregorian -> (y,m,d)) = do
-    pad4 y <> fromChar '-' <> pad2 m <> fromChar '-' <> pad2 d
 
 
 -- default filed view
