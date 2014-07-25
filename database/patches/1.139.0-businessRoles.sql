65355eb9c870fecd077bb929812850d6491d81ec
cvb <peter@standalone.su>
2014-07-25 18:47:28 +0400

    add permissions on BusinessRole.roles to supervisor

--- database/patches/1.139.0-businessRoles.sql
+++ database/patches/1.139.0-businessRoles.sql
@@ -11,4 +11,8 @@ INSERT INTO "BusinessRole" (label, roles)
          ('Back Office: Заказ услуг',           '{1,14,16,23,40}'),
          ('Back Office: Заказ вторичных услуг', '{1, 14, 16, 40}'),
          ('Back Office: Контроль услуг',        '{1, 14, 16, 40}');
+
+-- Add permission on roles field to supervisor
+INSERT INTO "FieldPermission" (role, model, field, r, w)
+  VALUES (12, 'BusinessRole', 'roles', '1', '1');
 END;
