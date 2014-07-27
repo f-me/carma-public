9dc780bc1049e6ecf1ed24b6e559ea39f4a46ebf
Dmitry Dzhus <dima@dzhus.org>
2014-07-27 16:05:26 +0400

    Port SmsTemplate idents to use mkIdents helper

--- carma-models/src/Carma/Model/SmsTemplate.hs
+++ carma-models/src/Carma/Model/SmsTemplate.hs
@@ -1,11 +1,14 @@
-module Carma.Model.SmsTemplate where
+{-# LANGUAGE TemplateHaskell #-}
 
+module Carma.Model.SmsTemplate where
 
 import Data.Text (Text)
 import Data.Typeable
 
 import Data.Model
 import Data.Model.View
+import Data.Model.TH
+
 import Carma.Model.Types()
 import Carma.Model.PgTypes()
 
@@ -18,25 +21,20 @@ data SmsTemplate = SmsTemplate
   }
   deriving Typeable
 
+
+mkIdents [t|SmsTemplate|]
+ [ ("order", 1)
+ , ("cancel", 2)
+ , ("complete", 3)
+ , ("create", 13)
+ , ("parguy", 7)
+ ]
+
+
 instance Model SmsTemplate where
   type TableName SmsTemplate = "SmsTemplate"
+  idents = Carma.Model.SmsTemplate.idents
   modelInfo = mkModelInfo SmsTemplate ident
   modelView = \case
     "" -> Just $ modifyView defaultView [textarea text]
     _  -> Nothing
-
-
-order :: IdentI SmsTemplate
-order = Ident 1
-
-cancel :: IdentI SmsTemplate
-cancel = Ident 2
-
-complete :: IdentI SmsTemplate
-complete = Ident 3
-
-create :: IdentI SmsTemplate
-create = Ident 13
-
-parguy :: IdentI SmsTemplate
-parguy = Ident 7
