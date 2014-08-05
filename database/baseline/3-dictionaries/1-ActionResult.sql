dd835efc5b1b35984bff805f168b109629d5bea3
Dmitry Dzhus <dima@dzhus.org>
2014-08-05 23:50:00 +0400

    Add falseCall ActionType results #2046
    Fix ActionType/ActionResult permissions patch

--- database/baseline/3-dictionaries/1-ActionResult.sql
+++ database/baseline/3-dictionaries/1-ActionResult.sql
@@ -84,4 +84,10 @@ VALUES ("Проверено аналитиком", 26);
 INSERT INTO "ActionResult" (label, id)
 VALUES ("Претензия разрешена", 27);
 
+INSERT INTO "ActionResult" (label, id)
+VALUES ("Ложный вызов с выставлением счёта", 28);
+
+INSERT INTO "ActionResult" (label, id)
+VALUES ("Ложный вызов без выставления счёта", 29);
+
 SELECT setval(pg_get_serial_sequence('"ActionResult"', 'id'), max(id)) from "ActionResult";
