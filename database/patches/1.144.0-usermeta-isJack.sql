15b738b4d307ebd45d3227158cb6e5d5cbdf992d
Dmitry Dzhus <dima@dzhus.org>
2014-08-10 00:27:35 +0400

    Fix jack patch

--- database/patches/1.144.0-usermeta-isJack.sql
+++ database/patches/1.144.0-usermeta-isJack.sql
@@ -2,8 +2,8 @@ ALTER TABLE usermetatbl ADD COLUMN isJack bool NOT NULL DEFAULT FALSE;
 
 INSERT INTO "FieldPermission" (role, model, field, r, w)
 VALUES
-('userAdmin', 'Usermeta', 'isJack', 'true', 'true');
+(4, 'Usermeta', 'isJack', 'true', 'true');
 
 INSERT INTO "FieldPermission" (role, model, field, r, w)
 VALUES
-('userViewer', 'Usermeta', 'isJack', 'true', 'false');
+(5, 'Usermeta', 'isJack', 'true', 'false');
