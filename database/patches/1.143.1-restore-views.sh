563408025ab8936c76d5fabbe916fbc73dc8169a
Dmitry Dzhus <dima@dzhus.org>
2014-08-12 19:27:45 +0400

    Bump 1.143.1 (#2114)

--- database/patches/1.143.1-restore-views.sh
+++ database/patches/1.143.1-restore-views.sh
@@ -7,4 +7,4 @@ $PSQL -f baseline/5-views/5-ru-partners.sql
 $PSQL -f baseline/5-views/4-ru-partner-refusals.sql
 $PSQL -f baseline/5-views/2-services-view.sql
 
-#COMMIT=92c4e4938aeef50a04848e092f31e37376479850/1.143.0-restore-views.sh
+# bump
