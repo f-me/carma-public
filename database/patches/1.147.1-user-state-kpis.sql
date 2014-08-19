4c708504bcf83f6aaf5046bded070ebbdbe391f0
cvb <peter@standalone.su>
2014-08-19 14:53:08 +0400

    more kpis

--- database/patches/1.147.1-user-state-kpis.sql
+++ database/patches/1.147.1-user-state-kpis.sql
@@ -1,16 +1,29 @@
 CREATE OR REPLACE FUNCTION get_KPI_timeinstate(u_id integer[], range tstzrange)
  RETURNS TABLE (
- userid integer,
- "LoggedOut" interval,
- "Ready" interval,
- "Rest" interval,
- "Busy" interval,
- "Dinner" interval,
- "ServiceBreak" interval) AS
+ userid         integer,
+ "LoggedOut"    interval,
+ "Ready"        interval,
+ "Rest"         interval,
+ "Busy"         interval,
+ "Dinner"       interval,
+ "ServiceBreak" interval,
+ "totalRest"    interval,
+ "totalLoggedIn"   interval) AS
 $func$
 BEGIN
   RETURN QUERY
-SELECT * from crosstab( $$
+SELECT *,
+   coalesce(ct."Rest",         interval '0') +
+   coalesce(ct."Dinner",       interval '0') +
+   coalesce(ct."ServiceBreak", interval '0') as totalRest,
+
+   coalesce(ct."Rest",         interval '0') +
+   coalesce(ct."Dinner",       interval '0') +
+   coalesce(ct."ServiceBreak", interval '0') +
+   coalesce(ct."Busy",         interval '0') +
+   coalesce(ct."Ready",        interval '0') as totalLoggedIn
+   FROM
+crosstab( $$
    SELECT us.userid, us.state,
    sum( upper('$$||range||$$' * us.range)
       - lower('$$||range||$$' * us.range)) as "timeInState"
