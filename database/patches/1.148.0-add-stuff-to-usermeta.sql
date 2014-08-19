b40429bab1d370f1cb79868a539531344a09735c
cvb <peter@standalone.su>
2014-08-19 13:35:06 +0400

    use {} as default value for usermeta stuff field

--- database/patches/1.148.0-add-stuff-to-usermeta.sql
+++ database/patches/1.148.0-add-stuff-to-usermeta.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-ALTER TABLE usermetatbl ADD COLUMN stuff json;
+ALTER TABLE usermetatbl ADD COLUMN stuff json NOT NULL DEFAULT '{}';
 
 INSERT INTO "FieldPermission" (role, model, field, r, w)
   VALUES (1, 'Usermeta', 'stuff', '1', '1');
