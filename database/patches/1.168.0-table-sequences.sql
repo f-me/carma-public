13d78a45f766dacbcce2876357a11c9d95e5b454
Dmitry Dzhus <dima@dzhus.org>
2014-08-28 13:33:22 +0400

    Set id sequences to create cases, services and actions in PG #1033

---
+++ database/patches/1.168.0-table-sequences.sql
@@ -0,0 +1,17 @@
+CREATE SEQUENCE servicetbl_id_seq;
+SELECT setval('servicetbl_id_seq', max(id)) FROM servicetbl;
+ALTER TABLE servicetbl ALTER COLUMN id SET DEFAULT nextval('servicetbl_id_seq');
+GRANT ALL ON "servicetbl_id_seq" TO carma_db_sync;
+GRANT ALL ON "servicetbl_id_seq" TO carma_search;
+
+CREATE SEQUENCE casetbl_id_seq;
+SELECT setval('casetbl_id_seq', max(id)) FROM casetbl;
+ALTER TABLE casetbl ALTER COLUMN id SET DEFAULT nextval('casetbl_id_seq');
+GRANT ALL ON "casetbl_id_seq" TO carma_db_sync;
+GRANT ALL ON "casetbl_id_seq" TO carma_search;
+
+CREATE SEQUENCE actiontbl_id_seq;
+SELECT setval('actiontbl_id_seq', max(id)) FROM actiontbl;
+ALTER TABLE actiontbl ALTER COLUMN id SET DEFAULT nextval('actiontbl_id_seq');
+GRANT ALL ON "actiontbl_id_seq" TO carma_db_sync;
+GRANT ALL ON "actiontbl_id_seq" TO carma_search;
