8059d61d48093de7fd03b8222afc7bb0af4035ff
Dmitry Dzhus <dima@dzhus.org>
2014-09-08 01:42:30 +0400

    Use Text for /searchContracts query

--- srv/src/AppHandlers/CustomSearches/Contract.hs
+++ srv/src/AppHandlers/CustomSearches/Contract.hs
@@ -22,11 +22,12 @@ import           Control.Applicative
 import           Control.Monad
 
 import           Data.Aeson as A
-import qualified Data.ByteString.Char8 as B
 import           Data.List
 import           Data.Maybe
 import           Data.String (fromString)
 import           Data.Text (Text)
+import qualified Data.Text as T
+import           Data.Text.Encoding (decodeUtf8)
 import qualified Data.Vector as V
 
 import           Database.PostgreSQL.Simple
@@ -103,14 +104,16 @@ searchContracts = do
   pid <- getIntParam "program"
   sid <- getIntParam "subprogram"
   limit <- fromMaybe 100 <$> getIntParam "limit"
-  q <- fromMaybe (error "No search query provided") <$> getParam "query"
+  q <- (decodeUtf8 .
+        fromMaybe (error "No search query provided")) <$>
+       getParam "query"
   caseId <- fromMaybe (error "No case number provided") <$> getIntParam "case"
   matchType <- fromMaybe "" <$> getParam "type"
 
   let exact = matchType == "exact"
 
   ml <- gets $ searchMinLength . options
-  when (B.length q < ml && not exact) $ error "Search query is too short"
+  when (T.length q < ml && not exact) $ error "Search query is too short"
 
   -- Form query template and all of its parameters. Contract
   -- identifiers (length M) and extraContractFields (length N) define
@@ -126,7 +129,7 @@ searchContracts = do
                        else fuzzyFieldPredicate
       fieldParams = zip (map PT C.identifierNames) $ repeat q
       totalQuery = intercalate " "
-          [ "SELECT DISTINCT ON(c.id) c.id,"
+          [ "SELECT DISTINCT ON(c.?) c.?,"
           -- 4 parameters: case callDate name, contract start/end date
           -- field name, case callDate name (expiration predicate)
           , "((cs.? < ?) or (? < cs.?)),"
@@ -170,6 +173,10 @@ searchContracts = do
 
   res <- withPG pg_search $ \c -> query c (fromString totalQuery)
          (()
+          -- 2
+          :. ( fieldPT Case.ident
+             , fieldPT Case.ident
+             )
           -- 4
           :. ( fieldPT Case.callDate
              , fieldPT C.validSince
