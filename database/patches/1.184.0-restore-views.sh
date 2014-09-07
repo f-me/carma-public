d9e1fda8f1de01947c3f6a87cc17f4ccd5315070
Dmitry Dzhus <dima@dzhus.org>
2014-09-07 22:01:04 +0400

    Fix DB patch
    Minor permission updates

--- database/patches/1.184.0-restore-views.sh
+++ database/patches/1.184.0-restore-views.sh
@@ -4,7 +4,6 @@ $PSQL -f baseline/5-views/0-allservices-view.sql
 $PSQL -f baseline/5-views/3-ru-calls.sql
 $PSQL -f baseline/5-views/4-ru-partner-refusals.sql
 $PSQL -f baseline/5-views/5-ru-partners.sql
-$PSQL -f baseline/5-views/6-ru-contracts.sql
 $PSQL -f baseline/5-views/7-ru-services.sql
 $PSQL -f baseline/5-views/8-ru-services-priority.sql
 $PSQL -f baseline/2-functions/1-geo-within.sql
