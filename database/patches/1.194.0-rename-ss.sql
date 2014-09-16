3edd8e9e548e0b979e7a0e0d0b0dc3f4bd46b26a
Dmitry Dzhus <dima@dzhus.org>
2014-09-16 21:55:19 +0400

    Update indices
    Typo fix

--- database/patches/1.194.0-rename-ss.sql
+++ database/patches/1.194.0-rename-ss.sql
@@ -1,3 +1,3 @@
-UPDATE "ServiceStats" SET label='В Back Office' WHERE id=1;
+UPDATE "ServiceStatus" SET label='В Back Office' WHERE id=1;
 
-UPDATE "ServiceStats" SET label='Время выезда' WHERE id=2;
+UPDATE "ServiceStatus" SET label='Время выезда' WHERE id=2;
---
+++ database/patches/1.195.0-perf-indices.sql
@@ -0,0 +1,34 @@
+-- Speed up /actionsFor and /backoffice/caseActions queries
+CREATE INDEX ON "actiontbl" (caseid);
+
+-- Speed up <>LoggedOut user selection in assignQ
+CREATE INDEX ON "UserState" (userid, id DESC);
+
+DROP INDEX "Contract_buydate_idx1";
+DROP INDEX "Contract_carclass_idx1";
+DROP INDEX "Contract_cardnumber_idx2";
+DROP INDEX "Contract_checkperiod_idx1";
+DROP INDEX "Contract_checktype_idx1";
+DROP INDEX "Contract_codeword_idx2";
+DROP INDEX "Contract_color_idx1";
+DROP INDEX "Contract_comment_idx1";
+DROP INDEX "Contract_email_idx2";
+DROP INDEX "Contract_enginetype_idx1";
+DROP INDEX "Contract_enginevolume_idx1";
+DROP INDEX "Contract_lastcheckdealer_idx1";
+DROP INDEX "Contract_legalform_idx1";
+DROP INDEX "Contract_make_idx1";
+DROP INDEX "Contract_makeyear_idx1";
+DROP INDEX "Contract_managername_idx1";
+DROP INDEX "Contract_model_idx1";
+DROP INDEX "Contract_name_idx2";
+DROP INDEX "Contract_ordernumber_idx1";
+DROP INDEX "Contract_phone_idx2";
+DROP INDEX "Contract_platenum_idx2";
+DROP INDEX "Contract_seller_idx1";
+DROP INDEX "Contract_startmileage_idx1";
+DROP INDEX "Contract_subprogram_idx1";
+DROP INDEX "Contract_transmission_idx1";
+DROP INDEX "Contract_validsince_idx1";
+DROP INDEX "Contract_validuntil_idx1";
+DROP INDEX "Contract_vin_idx2";
