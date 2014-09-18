eb616a4e4ccc39e315fc71ab2de79767bd61b3c5
Dmitry Dzhus <dima@dzhus.org>
2014-09-18 22:45:37 +0400

    Refactor action assignment, use priorities 1..5 #2248

--- srv/src/AppHandlers/ActionAssignment.hs
+++ srv/src/AppHandlers/ActionAssignment.hs
@@ -8,27 +8,30 @@ module AppHandlers.ActionAssignment (littleMoreActionsHandler)
 
 where
 
+import Control.Applicative hiding (some)
 import Control.Monad
+import Data.Aeson as A
+import Data.Maybe
 
 import Database.PostgreSQL.Simple
 import Database.PostgreSQL.Simple.SqlQQ
 
-import Data.Model.Types (Ident(..))
+import Data.Model.Types
 import Data.Model.Sql as Sql
 
 import Carma.Model.Action as Action
 import Carma.Model.ActionType as ActionType
+import Carma.Model.Case as Case
 
 import Snaplet.Auth.PGUsers
 
 import Application
-import AppHandlers.CustomSearches
 import AppHandlers.Users
 import AppHandlers.Util
 import Util hiding (withPG)
 
 
--- | Assign a single action to a user.
+-- | Assign a single action to a user, yield action id and case id.
 --
 -- 5 parameters: usermeta ident (2), action priority class, order
 -- action types (2)
@@ -69,7 +72,7 @@ assignQ = [sql|
               AND coalesce(svc.urgentService, 'notUrgent') <> 'notUrgent') DESC,
             act.duetime ASC
           LIMIT 1)
-        RETURNING id;
+        RETURNING id, caseId;
     |]
 
 
@@ -78,45 +81,59 @@ assignQ = [sql|
 -- user is not Ready or has older due actions.
 littleMoreActionsHandler :: AppHandler ()
 littleMoreActionsHandler = logExceptions "littleMoreActions" $ do
-  Just cid <- currentUserMetaId
+  uid <- fromMaybe (error "No current user") <$> currentUserMetaId
   let orders = In [ActionType.orderService, ActionType.orderServiceAnalyst]
       -- Parameters for assignQ query
-      params n = (cid, cid, n :: Int, orders)
-      Ident cid' = cid
-      errStart = "More actions requested by user " ++ show cid'
+      params n = (uid, uid, n :: Int, orders)
+      Ident uid' = uid
 
-  -- Do not pull more actions if the user already has some
-  --
-  -- This is not a critical error: supervisor may assign the user to
-  -- actions between two polling cycles of the back office screen.
-  --
-  -- Note that a supervisor still may manually assign the user to
-  -- actions *after* this check has fired but before serving the list
-  -- of all user's actions in response.
-  myActs <- withPG pg_actass $
-    Sql.select $
-    Action.ident :.
-    Action.assignedTo `eq` (Just cid) :.
-    (isNull Action.result)
-  when (null myActs) $ do
-    -- Reject busy users (should not be on back office screen)
-    userIsReady cid >>= \v ->
-      when (not v) $ error $ errStart ++ " in non-Ready state"
+  -- Actions already assigned to the user
+  oldActions <- map (\(Only i :. Only caseId :. ()) -> (i, caseId)) <$>
+                withPG pg_actass $
+                Sql.select $
+                Action.ident :. Action.caseId :.
+                Action.assignedTo `eq` Just uid :.
+                isNull Action.result
 
-    actIds'   <- withPG pg_actass (\c -> query c assignQ (params 1))
-    actIds''  <- case actIds' of
-                   []  -> withPG pg_actass (\c -> query c assignQ (params 2))
-                   _   -> return actIds'
-    actIds''' <- case actIds'' of
-                   []  -> withPG pg_actass (\c -> query c assignQ (params 3))
-                   _   -> return actIds''
+  actions <- ((,) <$> userIsReady uid <*> return oldActions) >>= \case
+    -- Do not pull more actions if the user already has some.
+    --
+    -- This is not a critical error: supervisor may assign the user to
+    -- actions between two polling cycles of the back office screen.
+    --
+    -- Note that a supervisor still may manually assign the user to
+    -- actions *after* this check has fired but before the list of all
+    -- user's actions is served in the response, which means that in
+    -- theory more than one action may end up being assigned to one
+    -- user.
+    (_   , _:_) -> return oldActions
+    -- Reject busy users with no actions. The business process has
+    -- already been violated (should not be on back office screen).
+    (False, []) -> error $
+                   "More actions requested by user " ++ show uid' ++
+                   " in non-Ready state"
+    (True, []) -> do
+      -- Pull new actions starting from top priority until something is
+      -- pulled (or we run out of priorities to check). This is the
+      -- opposite of Maybe monad behavior.
+      let pullFurther _       []      = return []
+          pullFurther puller (pr:prs) =
+            puller pr >>= \case
+              []  -> pullFurther puller prs
+              sth -> return sth
 
-    unless (null (actIds''' :: [Only Int]))
-      $ syslogJSON Info "littleMoreActions" [ "login" .= show cid
-                                            , "actions" .= show actIds'''
-                                            ]
+      newActions <- pullFurther
+                    (\p ->
+                       withPG pg_actass (\c -> query c assignQ (params p)))
+                    [1..5]
 
-  -- Serve all due actions to client. This may include older actions
-  -- or those we've just pulled.
-  selectActions (Just "0") (Just cid) Nothing Nothing Nothing
-    >>= writeJSON
+      unless (null (newActions :: [(IdentI Action, IdentI Case)])) $
+       syslogJSON Info "littleMoreActions"
+       [ "user" .= show uid'
+       , "newActions" .= map fst newActions
+       ]
+
+      return newActions
+
+  writeJSON $ map (\(aid, caseId) ->
+                     A.object ["id" .= aid, "caseId" .= caseId]) actions
