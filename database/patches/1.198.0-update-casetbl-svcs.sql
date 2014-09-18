e6ff173322fbe08eabd5ae3f2724af712b1667f4
Dmitry Dzhus <dima@dzhus.org>
2014-09-18 19:16:06 +0400

    Forget casetbl->insurancetbl references
    See #2232
    
    As of now, prod has 24 insurance svcs:
    
    carma=> select parentId, createtime from insurancetbl order by createtime;
      parentid   |       createtime
    -------------+------------------------
     case:35607  | 2012-11-13 17:39:00+04
     case:43906  | 2012-12-24 09:30:00+04
     case:65974  | 2013-04-02 15:00:00+04
     case:71742  | 2013-05-01 22:07:23+04
     case:111598 | 2013-10-31 17:16:00+04
     case:169805 | 2014-04-10 14:54:23+04
     case:184884 | 2014-06-20 10:00:33+04
     case:184884 | 2014-06-20 10:17:12+04
     case:193029 | 2014-06-22 13:19:14+04
     case:200362 | 2014-07-18 17:39:27+04
     case:199820 | 2014-07-20 12:30:28+04
     case:199820 | 2014-07-21 16:09:09+04
     case:200362 | 2014-07-21 17:01:18+04
     case:202301 | 2014-07-25 18:22:24+04
     case:203244 | 2014-07-29 11:21:27+04
     case:202301 | 2014-07-29 20:35:23+04
     case:206406 | 2014-08-08 16:44:33+04
     case:210074 | 2014-08-21 12:08:49+04
     case:210236 | 2014-08-21 20:43:47+04
     case:211274 | 2014-08-25 19:10:33+04
     case:210045 | 2014-08-28 11:31:19+04
     case:210236 | 2014-08-30 19:44:44+04
     case:211274 | 2014-09-02 12:08:13+04
     case:25018  |
    (24 строки)
    
    master dump before this patch:
    
    carma=# with map as (select id, unnest(string_to_array(regexp_replace(services, '[^,\d]', '', 'g'), ',')::int4[]) as svcId from casetbl where services is not null) select id from map where not exists (select 1 from servicetbl s where s.id = svcId);
       id
    --------
      25018
     203244
     200362
     200362
     193029
     210074
     202301
     202301
      71742
      35607
     211274
     211274
     210045
     184884
     184884
      43906
     210236
     210236
     169805
     111598
     206406
     199820
     199820
      65974
    (24 строки)
    
    after:
    
    carma=# with map as (select id, unnest(string_to_array(regexp_replace(services, '[^,\d]', '', 'g'), ',')::int4[]) as svcId from casetbl where services is not null) select id from map where not exists (select 1 from servicetbl s where s.id = svcId);
     id
    ----
    (0 строк)

---
+++ database/patches/1.198.0-update-casetbl-svcs.sql
@@ -0,0 +1,32 @@
+ALTER TABLE servicetbl
+ADD modelName text NOT NULL DEFAULT '';
+
+-- Migrate casetbl services references
+UPDATE servicetbl SET modelName = 'Tech' where type = 1;
+UPDATE servicetbl SET modelName = 'Towage' where type =  2;
+UPDATE servicetbl SET modelName = 'Rent' where type =  3;
+UPDATE servicetbl SET modelName = 'Hotel' where type = 4;
+UPDATE servicetbl SET modelName = 'Taxi' where type = 5;
+UPDATE servicetbl SET modelName = 'SoberDriver' where type = 6;
+UPDATE servicetbl SET modelName = 'Transportation' where type = 7;
+UPDATE servicetbl SET modelName = 'DeliverCar' where type = 8;
+UPDATE servicetbl SET modelName = 'DeliverParts' where type = 9;
+UPDATE servicetbl SET modelName = 'LegalAssistance' where type =10;
+UPDATE servicetbl SET modelName = 'TechInspect' where type =11;
+UPDATE servicetbl SET modelName = 'Information' where type =12;
+UPDATE servicetbl SET modelName = 'Consultation' where type =13;
+UPDATE servicetbl SET modelName = 'Tickets' where type =14;
+UPDATE servicetbl SET modelName = 'Continue' where type =15;
+UPDATE servicetbl SET modelName = 'Bank' where type =16;
+UPDATE servicetbl SET modelName = 'DeliverClient' where type =17;
+UPDATE servicetbl SET modelName = 'AverageCommissioner' where type =18;
+UPDATE servicetbl SET modelName = modelName || ':' || id::text;
+CREATE TEMPORARY TABLE casetbl_services AS
+SELECT parentId as caseId,array_to_string(array_agg(modelName), ',') as services
+FROM servicetbl GROUP BY parentid;
+
+UPDATE casetbl SET services = null;
+
+explain UPDATE casetbl c SET services = q.services
+FROM casetbl_services q WHERE q.caseId = id;
+ALTER TABLE servicetbl DROP COLUMN modelName;
