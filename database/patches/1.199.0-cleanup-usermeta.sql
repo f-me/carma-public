27f2dc432f947ebfcfb86d398375a47443b2b437
Dmitry Dzhus <dima@dzhus.org>
2014-09-21 18:01:42 +0400

    Drop unused usermeta columns

--- database/patches/1.199.0-cleanup-usermeta.sql
+++ database/patches/1.199.0-cleanup-usermeta.sql
@@ -3,3 +3,7 @@ UPDATE usermetatbl SET subprograms = programs;
 ALTER TABLE usermetatbl DROP COLUMN programs;
 
 UPDATE "FieldPermission" SET field='subPrograms' WHERE model='Usermeta' and field='programs';
+
+ALTER TABLE usermetatbl DROP COLUMN lastactivity;
+ALTER TABLE usermetatbl DROP COLUMN lastlogout;
+ALTER TABLE usermetatbl DROP COLUMN weathercities;
