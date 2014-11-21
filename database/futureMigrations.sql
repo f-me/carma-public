229e0ddeb547d25433e13001e0de998c1563f6c3
Dmitry Dzhus <dima@dzhus.org>
2014-11-21 18:23:33 +0300

    Dump some shit

--- database/futureMigrations.sql
+++ database/futureMigrations.sql
@@ -1,2 +1,26 @@
 -- set default sender instead of RAMC
 alter table "Sms" alter column sender set default 'Scramble CRM';
+
+-- FieldPermission
+--  Права на звонок (снять r на все поля не из списка)
+
+UPDATE "FieldPermission" SET r = 'f' WHERE model='Call' AND field NOT IN
+('callDate', 'endDate', 'program', 'callerName_name', 'callerName_phone1', 'customerComment');
+
+
+-- ServiceType (удалить всё кроме двух)
+
+TRUNCATE "Program" CASCADE;
+TRUNCATE partnertbl CASCADE;
+TRUNCATE partner_servicetbl CASCADE;
+DELETE FROM "ServiceType" WHERE id NOT IN (1, 2);
+
+
+-- Поля кейса
+UPDATE "FieldPermission" SET r = 'f' WHERE model='Case' AND field NOT IN
+('id', 'callDate', 'callTaker', 'customerComment', 'contact_name', 'contact_phone1', 'program', 'subprogram', 'contractIdentifier', 'contract', 'caseAddress_address', 'services', 'comments', 'files');
+
+-- В postgresql.conf надо ставить datestyle = 'iso, dmy'
+
+UPDATE "FieldPermission" SET r = 'f' WHERE model IN ('Service', 'Tech', 'Towage') AND field NOT IN
+('id', 'type', 'parentId', 'createTime', 'times_expectedServiceStart', 'times_expectedServiceEnd', 'contractor_partner', 'contractor_partnerId', 'contractor_address', 'contractor_coords', 'status', 'payment_costTranscript', 'techType', 'towDealer_partner', 'towDealer_partnerId', 'towDealer_address', 'towDealer_coords');
