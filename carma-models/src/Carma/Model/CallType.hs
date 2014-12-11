f364aaab44004e4b29a92bcf84f3e262c52a3c9b
cvb <peter@standalone.su>
2014-12-11 23:47:59 +0300

    add idents for CallType and CallerType

--- carma-models/src/Carma/Model/CallType.hs
+++ carma-models/src/Carma/Model/CallType.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module Carma.Model.CallType where
 
 
@@ -9,14 +11,23 @@ import Carma.Model.PgTypes()
 
 import Data.Model
 import Data.Model.View
+import Data.Model.TH
 
 data CallType = CallType
   {ident   :: PK Int CallType ""
   ,label   :: F Text "label" "Название цвета"
   } deriving Typeable
 
+mkIdents [t|CallType|]
+  [ ("info", 1)
+  , ("newCase", 2)
+  , ("secondCall", 3)
+  ]
+
+
 instance Model CallType where
   type TableName CallType = "CallType"
+  idents = Carma.Model.CallType.idents
   modelInfo = mkModelInfo CallType ident
   modelView = \case
     "" -> Just defaultView
--- carma-models/src/Carma/Model/CallerType.hs
+++ carma-models/src/Carma/Model/CallerType.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Carma.Model.CallerType where
 
 
@@ -9,14 +10,24 @@ import Carma.Model.PgTypes()
 
 import Data.Model
 import Data.Model.View
+import Data.Model.TH
 
 data CallerType = CallerType
   {ident   :: PK Int CallerType ""
   ,label   :: F Text "label" "Название цвета"
   } deriving Typeable
 
+mkIdents [t|CallerType|]
+ [("client",   1)
+ ,("partner",  2)
+ ,("dealer",   3)
+ ,("employee", 4)
+ ,("other",    5)
+ ]
+
 instance Model CallerType where
   type TableName CallerType = "CallerType"
+  idents = Carma.Model.CallerType.idents
   modelInfo = mkModelInfo CallerType ident
   modelView = \case
     "" -> Just defaultView
