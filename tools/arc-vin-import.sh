bfe7ae3839f470b86b58d004f86be2ef6fd83c89
Dmitry Dzhus <dima@dzhus.org>
2014-12-13 05:09:41 +0300

    Add actual people

--- tools/arc-vin-import.sh
+++ tools/arc-vin-import.sh
@@ -37,7 +37,7 @@ PG="localhost,5432,carma_db_sync,pass,carma"
 
 # Report mail parameters
 MAIL_FROM="carma@carma.ruamc.ru"
-MAIL_TO=("robots@formalmethods.ru")
+MAIL_TO=("robots@formalmethods.ru" "Alexander.Dimakov@ruamc.ru" "Pavel.Golovnin@ruamc.ru")
 MAIL_SUBJECT="Отчёт о загрузке контрактов PSA `date +%F`"
 
 # END OF CONFIGURATION
