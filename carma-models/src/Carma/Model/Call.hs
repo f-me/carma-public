6cb13ad420ca27775e0278f1ee910f1383f84ff4
cvb <peter@standalone.su>
2014-12-15 19:13:15 +0300

    set coords back, remove unused fields from Call

--- carma-models/src/Carma/Model/Call.hs
+++ carma-models/src/Carma/Model/Call.hs
@@ -10,11 +10,7 @@ import Data.Model.View
 
 import Carma.Model.Types           (Coords)
 import Carma.Model.Program         (Program)
-import Carma.Model.SubProgram.Type (SubProgram)
-import Carma.Model.CarMake         (CarMake)
-import Carma.Model.CarModel        (CarModel)
 import Carma.Model.Case            (Case)
-import Carma.Model.City            (City)
 import Carma.Model.Usermeta        (Usermeta)
 import Carma.Model.CallType        (CallType)
 import Carma.Model.CallerType      (CallerType)
@@ -50,11 +46,7 @@ dicts :: [(Text, FieldView -> FieldView) :@ Call]
 dicts =
   [ setMeta "dictionaryParent"
     (Aeson.String $ Model.fieldName callerType) callReason
-  -- , dict carModel $ (dictOpt "CarModel")
-  --   {dictParent = Just $ Model.fieldName carMake, dictBounded = True}
   , setMeta "dictionaryLabel" (Aeson.String "realName") callTaker
-  , setMeta "dictionaryParent"
-    (Aeson.String $ Model.fieldName program) subprogram
   ]
 
 metas :: [(Text, FieldView -> FieldView) :@ Call]
@@ -66,8 +58,8 @@ metas =
     , readonly callTaker
     , required callTaker
 
-    -- , invisible coords
-    -- , invisible address
+    , invisible coords
+    , invisible address
 
     , hiddenIdent caseId
 
@@ -92,10 +84,6 @@ data Call = Call
     :: F (IdentI Usermeta) "callTaker" "Сотрудник РАМК"
   , program
     :: F (Maybe (IdentI Program)) "program" "Программа"
-  , subprogram
-    :: F (Maybe (IdentI SubProgram)) "subprogram" "Подпрограмма"
-  -- , wazzup
-  --   :: F (Maybe (IdentI Wazzup)) "wazzup" "Что случилось"
   , callerName
     :: F (Maybe Text) "callerName" "Звонящий"
   , callerPhone
@@ -106,22 +94,14 @@ data Call = Call
     :: F (Maybe (IdentI CallReason)) "callReason" "Причина обращения"
   , abuseTarget
     :: F (Maybe (IdentI AbuseTarget)) "abuseTarget" "На кого поступила жалоба"
-  -- , city
-  --   :: F (Maybe (IdentI City)) "city" "Город"
-  -- , coords
-  --   :: F (Maybe Coords) "coords" "Координаты места поломки"
-  -- , address
-  --   :: F (Maybe Text) "address" "Адрес места поломки"
-  -- , carMake
-  --   :: F (Maybe (IdentI CarMake)) "carMake" "Марка"
-  -- , carModel
-  --   :: F (Maybe (IdentI CarModel)) "carModel" "Модель"
+  , coords
+    :: F (Maybe Coords) "coords" "Координаты места поломки"
+  , address
+    :: F (Maybe Text) "address" "Адрес места поломки"
   , partner
     :: F (Maybe (IdentI Partner)) "partner" "Дилер"
-
   , customerComment
     :: F (Maybe Text) "customerComment" "Комментарий"
-
   , caseId
     :: F (Maybe (IdentI Case)) "caseId" "Связанный кейс"
   } deriving Typeable
