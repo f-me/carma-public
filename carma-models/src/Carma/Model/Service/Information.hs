e2f206a8fafabfc13975cb1aacd8affe6c7bd19f
Dmitry Dzhus <dima@dzhus.org>
2015-02-27 01:26:24 +0300

    Fix Service.Information types f-me/carma#2476

--- carma-models/src/Carma/Model/Service/Information.hs
+++ carma-models/src/Carma/Model/Service/Information.hs
@@ -13,20 +13,20 @@ data Information = Information
   { ident      :: PK Int Information ""
   , contact1   :: F (Maybe Text)
                   "contact1"   "Контакт 1"
-  , phone1     :: F Phone
-                  "phone1"     "Телефон 1"
+  , phone1     :: F (Maybe Phone)
+                  "contactphone1" "Телефон 1"
   , whatToSay1 :: F (Maybe Text)
                   "whatToSay1" "Что сказать 1"
   , contact2   :: F (Maybe Text)
                   "contact2"   "Контакт 2"
-  , phone2     :: F Phone
-                  "phone2"     "Телефон 2"
+  , phone2     :: F (Maybe Phone)
+                  "contactphone2" "Телефон 2"
   , whatToSay2 :: F (Maybe Text)
                   "whatToSay2" "Что сказать 2"
   , contact3   :: F (Maybe Text)
                   "contact3"   "Контакт 3"
-  , phone3     :: F Phone
-                  "phone3"     "Телефон 3"
+  , phone3     :: F (Maybe Phone)
+                  "contactphone3" "Телефон 3"
   , whatToSay3 :: F (Maybe Text)
                   "whatToSay3" "Что сказать 3"
   }
