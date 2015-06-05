1ec92060beba8e4be9da6911a5f9f52c3a69185a
Dmitry Dzhus <dima@dzhus.org>
2015-06-06 01:57:58 +0300

    New parameter in ProcessingConfig #2243

--- database/baseline/3-dictionaries/67-ProcessingConfig.sql
+++ database/baseline/3-dictionaries/67-ProcessingConfig.sql
@@ -2,10 +2,11 @@ CREATE TABLE "ProcessingConfig"
   ( id    SERIAL PRIMARY KEY
   , actionsFirst bool NOT NULL DEFAULT 't'
   , afterCallSeconds int4 NOT NULL DEFAULT 30
+  , callWaitSeconds int4 NOT NULL DEFAULT 30
   );
 
-COPY "ProcessingConfig" (id, actionsFirst, afterCallSeconds) FROM stdin;
-1	t	30
+COPY "ProcessingConfig" (id, actionsFirst, afterCallSeconds, callWaitSeconds) FROM stdin;
+1	t	30	30
 \.
 
 GRANT ALL ON "ProcessingConfig" TO carma_db_sync;
---
+++ database/patches/1.268.0-add-callWaitSeconds.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "ProcessingConfig" ADD callWaitSeconds int4 NOT NULL DEFAULT 30;
