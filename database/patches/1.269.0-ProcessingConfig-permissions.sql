5ed61a455af94baaa5bc798c127e05c895d3473d
Dmitry Dzhus <dima@dzhus.org>
2015-06-22 03:28:27 +0300

    Add read permissions for ProcessingConfig

---
+++ database/patches/1.269.0-ProcessingConfig-permissions.sql
@@ -0,0 +1,17 @@
+INSERT INTO "FieldPermission" (role, model, field, r, w) VALUES
+(2, 'ProcessingConfig', 'actionsFirst', 't', 'f');
+
+INSERT INTO "FieldPermission" (role, model, field, r, w) VALUES
+(2, 'ProcessingConfig', 'afterCallSeconds', 't', 'f');
+
+INSERT INTO "FieldPermission" (role, model, field, r, w) VALUES
+(2, 'ProcessingConfig', 'callWaitSeconds', 't', 'f');
+
+INSERT INTO "FieldPermission" (role, model, field, r, w) VALUES
+(14, 'ProcessingConfig', 'actionsFirst', 't', 'f');
+
+INSERT INTO "FieldPermission" (role, model, field, r, w) VALUES
+(14, 'ProcessingConfig', 'afterCallSeconds', 't', 'f');
+
+INSERT INTO "FieldPermission" (role, model, field, r, w) VALUES
+(14, 'ProcessingConfig', 'callWaitSeconds', 't', 'f');
