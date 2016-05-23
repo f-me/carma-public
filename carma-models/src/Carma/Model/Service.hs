98e60e6205c10fe6f60597faac7eebe12de299ae
Max Taldykin <jorpic@gmail.com>
2016-05-23 23:17:45 +0300

    Service.contactor_partnerLegacy f-me/carma#2646

--- carma-models/src/Carma/Model/Service.hs
+++ carma-models/src/Carma/Model/Service.hs
@@ -82,6 +82,8 @@ data Service = Service
 
   , contractor_partner           :: F (Maybe Text) "contractor_partner"
                                  "Партнёр"
+  , contractor_partnerLegacy     :: F Aeson.Value "contractor_partnerLegacy"
+                                 "Партнёр"
   , contractor_partnerId         :: F (Maybe (IdentI Partner)) "contractor_partnerId"
                                  "Партнёр"
   , contractor_address           :: F (Maybe Text) "contractor_address"
@@ -135,6 +137,7 @@ svcMod =
     ,setType "dictionary" contractor_partnerId
     ,setMeta "group-widget" "partner" contractor_partner
     ,invisible contractor_coords
+    ,invisible contractor_partnerLegacy
     ,hiddenIdent parentId
     ,readonly status
     ,clientCancelReason `completeWith` CRR.label
