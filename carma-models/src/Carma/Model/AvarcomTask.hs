a27f73a316faf2e90adea217c4d6d4f76e49bf45
Max Taldykin <jorpic@gmail.com>
2016-10-16 15:40:12 +0300

    AvarcomTask dictionary #2672

---
+++ carma-models/src/Carma/Model/AvarcomTask.hs
@@ -0,0 +1,24 @@
+
+module Carma.Model.AvarcomTask where
+
+import Data.Text
+import Data.Model
+import Data.Typeable
+
+import Data.Model.View
+
+
+data AvarcomTask = AvarcomTask
+  {ident    :: PK Int AvarcomTask ""
+  ,label    :: F  Text "label"  "Наименование задачи"
+  ,isAcitve :: F  Bool "isActive" "Активно"
+  } deriving Typeable
+
+
+instance Model AvarcomTask where
+  type TableName AvarcomTask = "AvarcomTask"
+  modelInfo = mkModelInfo AvarcomTask ident
+  modelView = \case
+    "" -> Just defaultView
+    _  -> Nothing
+
