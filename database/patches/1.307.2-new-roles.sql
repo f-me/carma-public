23caf653bde2c8162882ab306771285e6bdb48a0
Max Taldykin <jorpic@gmail.com>
2016-10-17 13:06:16 +0300

    New roles #2674 #2683

---
+++ database/patches/1.307.2-new-roles.sql
@@ -0,0 +1,5 @@
+
+insert into "Role" (id, value, label, isback) values
+  (64, 'bo_orderRefs', 'БО: Заказ услуги (Сбор справок)', true)
+, (65, 'bo_orderAvarcom', 'БО: Заказ услуги (Аварком)', true)
+;
