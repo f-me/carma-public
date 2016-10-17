63d03b56bf8acff04922a9db4e09c1efc9a1ff14
Max Taldykin <jorpic@gmail.com>
2016-10-17 23:04:45 +0300

    Avarcom.tasks in case history #2676

--- database/baseline/5-views/12-case-history.sql
+++ database/baseline/5-views/12-case-history.sql
@@ -21,7 +21,12 @@ FROM (
             "ActionResult".label AS actionResult,
             actiontbl.comment AS actionComment,
             s.id AS serviceId,
-            "ServiceType".label AS serviceLabel
+            "ServiceType".label AS serviceLabel,
+            (select tasks from averagecommissionertbl avc
+                where avc.id = actiontbl.serviceid
+                  and actiontbl.type = 1
+                  and (actiontbl.result = 1 or actiontbl.result = 2)
+            ) as tasks
         FROM "ActionResult",
             "ActionType",
             actiontbl
