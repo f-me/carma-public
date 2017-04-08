9a150466f1f952778ef21501e1e74d458841107f
Max Taldykin <jorpic@gmail.com>
2017-04-08 11:25:55 +0300

    DiagTree: preliminary version of tree

--- srv/resources/assets/react-components/src/DiagTree/Editor.js
+++ srv/resources/assets/react-components/src/DiagTree/Editor.js
@@ -1,12 +1,17 @@
-import Immutable from 'immutable'
+
 import React from 'react'
 import { Grid, Row, Col } from 'react-bootstrap'
+import { Button, ButtonToolbar, Glyphicon } from 'react-bootstrap'
+import { TreeList }  from 'react-treeview-mui'
 
-import SlideTree from './SlideTree'
+import injectTapEventPlugin from 'react-tap-event-plugin'
+import Immutable from 'immutable'
+
 import SlideEditor from './SlideEditor'
 import './DiagTree.css'
 
 
+injectTapEventPlugin();
 
 // FIXME: error if there is no slides
 export default class Editor extends React.Component {
@@ -15,7 +20,8 @@ export default class Editor extends React.Component {
 
     this.state = {
       slides: null,
-      selectedId: null
+      selectedIx: 0,
+      expandedListItems: [],
     }
 
     this._loadSlides()
@@ -27,12 +33,45 @@ export default class Editor extends React.Component {
       type: 'GET',
       url: '/_/DiagSlide',
       dataType: 'json',
-      success: slides => this.setState({
-        slides: Immutable.Map(
-          slides.reduce((m, s) => {m[String(s.id)] = s; return m;}, {})),
-        selectedId: this.state.selectedId || slides.find(x => x.isRoot).id
-      })
+      success: slides => {
+        const slidesMap = Immutable.Map(
+          slides.reduce((m, s) => {m[String(s.id)] = s; return m;}, {}));
+        const listItems = this._prepareListItems(slidesMap);
+        this.setState({
+          slides: slidesMap,
+          listItems,
+          selectedIx: this.state.selectedIx || 1
+        })
+      }
     })
+
+  _prepareListItems = slides => {
+    const root = [{depth:0, children: []}];
+    const listItems = root.concat(slides.valueSeq().toArray());
+
+    const id2ix = {};
+    listItems.forEach((it, i) => {id2ix[it.id] = i});
+
+    listItems.forEach((it, i) => {
+      if (i > 0) {
+        it.ix = i;
+        it.children = it.answers.map(z => id2ix[z.nextSlide]);
+        it.children.forEach(c => listItems[c].parentIndex = i);
+        if(it.isRoot) {
+          it.depth = 1;
+          it.parentIndex = 0;
+        }
+      }
+    })
+
+    listItems.forEach((it, i) => {
+      it.depth = it.parentIndex !== undefined
+        ? listItems[it.parentIndex].depth + 1
+        : 0;
+    })
+
+    return listItems;
+  }
 
 
   newSlide = () => {
@@ -54,7 +93,7 @@ export default class Editor extends React.Component {
         Object.assign(slide, res);
         this.setState({
           slides: this.state.slides.set(String(slide.id), slide),
-          selectedId: slide.id
+          selectedId: slide.id,
         })
       }
     })
@@ -85,24 +124,49 @@ export default class Editor extends React.Component {
   }
 
 
+  _handleTreeTap = (item, index) => {
+    const {expandedListItems} = this.state;
+    if (expandedListItems.indexOf(index) === -1) {
+      this.setState({
+        expandedListItems: [index].concat(expandedListItems),
+        selectedIx: index
+      })
+    } else {
+      this.setState({
+        expandedListItems: expandedListItems.filter(x => x != index),
+        selectedIx: index
+      })
+    }
+  }
+
+
   render() {
-    const {slides, selectedId} = this.state;
-    if (selectedId === null) return (<span>Loading...</span>);
+    const {slides, listItems, expandedListItems, selectedIx} = this.state;
+
+    if (slides === null) return (<span>Loading...</span>);
 
     return (
       <Grid className="Editor">
         <Row>
           <Col md={4}>
-            <SlideTree
-              slides={slides}
-              selectedId={selectedId}
-              onSelect={s => this.setState({selectedId: s})}
-              onAddSlide={this.newSlide}
-            />
+            <TreeList
+              listItems={listItems}
+              contentKey={'header'}
+              haveSearchbar={true}
+              handleTouchTap={this._handleTreeTap}
+              expandedListItems={expandedListItems}
+              activeListItem={selectedIx}
+              >
+                <ButtonToolbar>
+                  <Button bsStyle='success' onClick={this.newSlide}>
+                    <Glyphicon glyph="plus" /> Новое дерево
+                  </Button>
+                </ButtonToolbar>
+            </TreeList>
           </Col>
           <Col md={8}>
             <SlideEditor
-              slide={slides.get(String(selectedId))}
+              slide={slides.get(String(listItems[selectedIx].id))}
               onChange={this.saveSlide}
               saveMsg={this.state.saveMsg}
             />
