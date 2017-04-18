48941857cb59f9708d25a73b3d0eb0568c8937e9
Max Taldykin <jorpic@gmail.com>
2017-04-18 16:14:56 +0300

    DiagTree: improve search usability

--- srv/resources/assets/react-components/src/DiagTree/Tree.js
+++ srv/resources/assets/react-components/src/DiagTree/Tree.js
@@ -18,16 +18,38 @@ export default class Tree extends React.Component {
   _onClick = it => {
     const {expandedItems, searchText} = this.state;
 
-    let isExpanded = expandedItems.get(it.id);
-    isExpanded = !isExpanded || searchText.trim() !== '';
-    this.setState({
-      expandedItems: expandedItems.set(it.id, isExpanded)
-    })
+    if (searchText.trim() === '') {
+      const isExpanded = expandedItems.get(it.id);
+      this.setState({
+        expandedItems: expandedItems.set(it.id, !isExpanded)
+      })
+    }
+
     this.props.onSelect(it)
   }
 
 
-  _onSearch = e => this.setState({searchText: e.target.value})
+  _onSearch = e => {
+    const searchText = e.target.value;
+    const {items, selected} = this.props;
+    let {expandedItems} = this.state;
+
+    if (searchText === '') { // expand all ancestors of the selected node
+      const ancestor = items.valueSeq().reduce(
+        (m, x) => {
+          x.answers.forEach(y => {m[y.nextSlide] = x.id});
+          return m;
+        }, {});
+      let id = ancestor[selected];
+      while (id !== undefined) {
+        console.log(id);
+        expandedItems = expandedItems.set(id, true);
+        id = ancestor[id];
+      }
+    }
+
+    this.setState({searchText, expandedItems});
+  }
 
 
   _renderItem = (it, depth, ans) => {
@@ -40,29 +62,43 @@ export default class Tree extends React.Component {
     };
 
     const needle = searchText.trim();
-    const isVisible = depth === 0
-      || needle === ''
-      || it.header.search(needle) >= 0;
-    const isExpanded = needle !== '' || expandedItems.get(it.id);
+    const isSearchMode = needle !== '';
+    const searchRes = isSearchMode ? this._highlight(it.header, needle)  : null;
 
     return (
       <div key={it.id}>
         <div
           onClick={() => this._onClick(it)}
           style={{...itemStyle,
-            display: isVisible ? undefined : 'none',
+            display: (!isSearchMode || it.isRoot || searchRes !== null) ? undefined : 'none',
             paddingLeft: '4px',
             marginLeft: (depth * 16) + 'px',
             borderLeft: '5px solid '+ (it.id === selected ? '#2B95fD' : '#fff')
           }}
         >
           <div style={{color: 'grey'}}>{ans}</div>
-          <div>{it.header}</div>
+          {searchRes || it.header}
         </div>
-        {isExpanded ? this._renderChildren(it, depth+1) : ''}
+        {isSearchMode || expandedItems.get(it.id)
+          ? this._renderChildren(it, depth+1) : ''
+        }
       </div>
     );
   }
+
+  _highlight(text, search) {
+    const ix = text.search(new RegExp(search, "i"));
+    if (ix === -1) return null;
+    return (
+      <div>
+        {text.substr(0, ix)}
+        <span style={{backgroundColor: "yellow"}}>
+          {text.substr(ix, search.length)}
+        </span>
+        {text.substr(ix + search.length)}
+      </div>)
+  }
+
 
   _renderChildren = (it, depth) => {
     const {items} = this.props;
