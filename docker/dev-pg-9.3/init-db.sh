eb3995ef27c54c1dff6e1198dcbc887e162a6b9a
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2017-07-18 01:44:41 +0500

    feature/dockerized-pg-9.3: `set -e` for scripts

--- docker/dev-pg-9.3/init-db.sh
+++ docker/dev-pg-9.3/init-db.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
-initdb -D /pgdata                              || exit $?
-{ postgres -D /pgdata -h localhost -p 5432 & } || exit $?
+set -e
+
+initdb -D /pgdata
+postgres -D /pgdata -h localhost -p 5432 &
 PGPID=$!
 sleep 2
 
@@ -8,13 +10,13 @@ echo $'
 	CREATE USER carma         WITH SUPERUSER PASSWORD \'pass\';
 	CREATE USER carma_db_sync WITH SUPERUSER PASSWORD \'pass\';
 	ALTER  USER postgres      WITH           PASSWORD \'pass\';
-' | psql -h localhost || exit $?
+' | psql -h localhost
 
 echo 'CREATE DATABASE carma;' \
-	| psql -h localhost -U carma_db_sync -d postgres || exit $?
+	| psql -h localhost -U carma_db_sync -d postgres
 
-kill -TERM "$PGPID" || exit $?
+kill -TERM "$PGPID"
 sleep 2
-kill -KILL "$PGPID" 2>/dev/null
+kill -KILL "$PGPID" 2>/dev/null || true
 sleep 1
-chmod go-rwx -R /pgdata || exit $?
+chmod go-rwx -R /pgdata
--- docker/dev-pg-9.3/start-pg.sh
+++ docker/dev-pg-9.3/start-pg.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
+set -e
 
-IP=`hostname -i` || exit $?
+IP=`hostname -i`
 
 echo "
 local   all   all                  trust
 host    all   all   127.0.0.1/32   trust
 host    all   all   $IP/24         md5
-" > /pgdata/pg_hba.conf || exit $?
+" > /pgdata/pg_hba.conf
 
-chmod go-rwx -R /pgdata                || exit $?
-postgres -D /pgdata -h 0.0.0.0 -p 5432 || exit $?
+chmod go-rwx -R /pgdata
+postgres -D /pgdata -h 0.0.0.0 -p 5432
