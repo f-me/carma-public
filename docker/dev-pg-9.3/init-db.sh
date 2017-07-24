df2ac9816056f4fa60df6a5dc02c8d2f3c0c7cca
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2017-07-24 17:11:39 +0500

    dockerized-pg-9.3: move volume mount path to '/var/lib/postgresql/data'
    because it's already declared in 'postgres' image and every container
    run creates new empty volume.

--- docker/dev-pg-9.3/init-db.sh
+++ docker/dev-pg-9.3/init-db.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
-initdb -D /pgdata
-postgres -D /pgdata -h localhost -p 5432 &
+initdb -D "$PGDATA"
+perl -p -i -e 's/^unix_socket_directories = .*\n//' "$PGDATA/postgresql.conf"
+echo $'unix_socket_directories = \'/tmp\'' >> "$PGDATA/postgresql.conf"
+postgres -D "$PGDATA" -h 127.0.0.1 -p 5432 &
 PGPID=$!
 sleep 2
 
@@ -10,13 +12,13 @@ echo $'
 	CREATE USER carma         WITH SUPERUSER PASSWORD \'pass\';
 	CREATE USER carma_db_sync WITH SUPERUSER PASSWORD \'pass\';
 	ALTER  USER postgres      WITH           PASSWORD \'pass\';
-' | psql -h localhost
+' | psql -h 127.0.0.1
 
 echo 'CREATE DATABASE carma;' \
-	| psql -h localhost -U carma_db_sync -d postgres
+	| psql -h 127.0.0.1 -U carma_db_sync -d postgres
 
 kill -TERM "$PGPID"
 sleep 2
 kill -KILL "$PGPID" 2>/dev/null || true
 sleep 1
-chmod go-rwx -R /pgdata
+chmod go-rwx -R "$PGDATA"
--- docker/dev-pg-9.3/start-pg.sh
+++ docker/dev-pg-9.3/start-pg.sh
@@ -7,7 +7,9 @@ echo "
 local   all   all                  trust
 host    all   all   127.0.0.1/32   trust
 host    all   all   $IP/24         md5
-" > /pgdata/pg_hba.conf
+" > "$PGDATA/pg_hba.conf"
 
-chmod go-rwx -R /pgdata
-postgres -D /pgdata -h 0.0.0.0 -p 5432
+perl -p -i -e 's/^unix_socket_directories = .*\n//' "$PGDATA/postgresql.conf"
+echo $'unix_socket_directories = \'/tmp\'' >> "$PGDATA/postgresql.conf"
+chmod go-rwx -R "$PGDATA"
+postgres -D "$PGDATA" -h 0.0.0.0 -p 5432
