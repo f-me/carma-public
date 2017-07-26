e20afdd4f196b6b8d191682ff4f8a95a966577cf
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2017-07-27 02:15:27 +0500

    dockerized-pg-9.3: creating more roles in "init-db.sh"

--- docker/dev-pg-9.3/init-db.sh
+++ docker/dev-pg-9.3/init-db.sh
@@ -9,16 +9,25 @@ PGPID=$!
 sleep 2
 
 echo $'
-	CREATE USER carma         WITH SUPERUSER PASSWORD \'pass\';
-	CREATE USER carma_db_sync WITH SUPERUSER PASSWORD \'pass\';
-	ALTER  USER postgres      WITH           PASSWORD \'pass\';
+
+ALTER  USER postgres      WITH           PASSWORD \'pass\';
+CREATE USER carma         WITH SUPERUSER PASSWORD \'pass\';
+CREATE USER carma_db_sync WITH SUPERUSER PASSWORD \'pass\';
+
+CREATE USER carma_geo;
+CREATE USER carma_sms;
+CREATE USER mail_svc;
+CREATE USER fmuser;
+CREATE USER "pavel.golovnin";
+CREATE USER reportgen;
+
 ' | psql -h 127.0.0.1
 
-echo 'CREATE DATABASE carma;' \
-	| psql -h 127.0.0.1 -U carma_db_sync -d postgres
+echo 'CREATE DATABASE carma;' | psql -h 127.0.0.1 -U carma_db_sync -d postgres
 
 kill -TERM "$PGPID"
 sleep 2
 kill -KILL "$PGPID" 2>/dev/null || true
 sleep 1
+
 chmod go-rwx -R "$PGDATA"
