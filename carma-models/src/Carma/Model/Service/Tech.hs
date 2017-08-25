4b57038f6913d9a6b8d1f349c43be8b351ae3d2d
Dmitry Dzhus <dima@dzhus.org>
2017-08-25 10:16:36 +0100

    feat: Drop Tech.checkN fields #2842
    This reverts e8e840b7c4a6960711bf62ce84a70775603a5900

--- carma-models/src/Carma/Model/Service/Tech.hs
+++ carma-models/src/Carma/Model/Service/Tech.hs
@@ -17,19 +17,7 @@ data Tech = Tech
   { ident          :: PK Int Tech ""
   , techType       :: F (Maybe (IdentI TechType)) "techType" "Услуга"
   , orderNumber    :: F (Maybe Text) "orderNumber" "Номер заказ-наряда"
-  , check1         :: F (Maybe Checkbox) "check1"
-                      "Капот открывается"
-  , check2         :: F (Maybe Checkbox) "check2"
-                      "Наличие запасного колеса"
-  , check3         :: F (Maybe Checkbox) "check3"
-                      "Наличие секреток"
-  , check4         :: F (Maybe Checkbox) "check4"
-                      "Запасной ключ имеется"
-  , check5         :: F (Maybe Checkbox) "check5"
-                      "Документы на автомобиль на руках"
-  , check6         :: F (Maybe Checkbox) "check6"
-                      "Не открывается лючок бензобака"
-  , isCountryRide       :: F Bool "isCountryRide" "За городом"
+  , isCountryRide  :: F Bool "isCountryRide" "За городом"
 
   -- Naming scheme convention: complNpM, where N is the id of a
   -- TechType dictionary entry. Actual visibility rules are set via
@@ -105,25 +93,8 @@ instance Model Tech where
     Nothing -> Nothing
     Just mv ->
       Just $ modifyView (mv {mv_title = "Техпомощь"})
-        [setMeta "filterBy" "isActive" techType
-        ,setMeta "visibleIf" (object
-          ["techType" .= [TechType.charge, TechType.chargeRuamc, TechType.customTech_27]
-          ]) check1
-        ,setMeta "visibleIf" (object
-          ["techType" .= [TechType.wheel, TechType.wheelTower,  TechType.wheelRuamc, TechType.customTech_28]
-          ]) check2
-        ,setMeta "visibleIf" (object
-          ["techType" .= [TechType.wheel, TechType.wheelTower,  TechType.wheelRuamc, TechType.customTech_28]
-          ]) check3
-        ,setMeta "visibleIf" (object
-          ["techType" .= [TechType.hack, TechType.hackByRuamc]
-          ]) check4
-        ,setMeta "visibleIf" (object
-          ["techType" .= [TechType.hack, TechType.hackByRuamc]
-          ]) check5
-        ,setMeta "visibleIf" (object
-          ["techType" .= [TechType.fuel, TechType.fuelTower, TechType.fuelRuamc]
-          ]) check6
+        [ setMeta "filterBy" "isActive" techType
+
         , setMeta "visibleIf" (object ["isCountryRide" .= [True]]) suburbanMilage
         , setMeta "visibleIf" (object ["isCountryRide" .= [True]]) totalMilage
         , setMeta "visibleIf" (object ["isCountryRide" .= [True]]) partnerWarnedInTime
