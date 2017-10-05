81e5c4b8bcb0ad293687dfe09da2a42ed25b61bb
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2017-10-05 23:50:09 +0500

    bump for database/patches/1.339.*.sh

--- database/patches/1.339.2-get-partner-payment-function.sh
+++ database/patches/1.339.2-get-partner-payment-function.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
+$PSQL -c 'DROP VIEW     IF EXISTS "PartnerPayment"'       # drop old view
 $PSQL -c 'DROP FUNCTION IF EXISTS GetPartnerPayment(INT)' # drop old function
 $PSQL -f baseline/2-functions/3-get-partner-payment.sql
+
+# BUMP
