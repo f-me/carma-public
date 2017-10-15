f-me/carma-public

commit ebf5c30d42ccefa797832a68cf44cc95aaab6f1e

feat: Provide default PG settings

srv/src/ApplicationInit.hs
@@ -16,7 +16,7 @@ import Snap.Snaplet
 import Snap.Snaplet.Heist
 import Snap.Snaplet.Auth hiding (session)
 import Snap.Snaplet.Auth.Backends.PostgresqlSimple
-import Snap.Snaplet.PostgresqlSimple (pgsInit)
+import Snap.Snaplet.PostgresqlSimple (pgsDefaultConfig, pgsInit')
 import Snap.Snaplet.Session.Backends.CookieSession
 import Snap.Util.FileServe ( serveFile
                            , simpleDirectoryConfig
@@ -190,7 +190,8 @@ appInit = makeSnaplet "app" "Forms application" Nothing $ do
        initCookieSessionManager sesKey "_session" Nothing
 
   -- DB
-  ad <- nestSnaplet "auth_db" db pgsInit
+  ad <- nestSnaplet "auth_db" db $ pgsInit' $ pgsDefaultConfig
+        "host=localhost port=5432 dbname=carma user=carma_db_sync password=pass"
 
   authMgr <- nestSnaplet "auth" auth $ initPostgresAuth session ad
 

commit 5a322d08179e3623381717b22244e0cf83e729aa

fix: Use postgresql-simple snaplet config
(regression in ebf5c30d42)

srv/src/ApplicationInit.hs
@@ -17,7 +17,7 @@ import Snap.Snaplet
 import Snap.Snaplet.Heist
 import Snap.Snaplet.Auth hiding (session)
 import Snap.Snaplet.Auth.Backends.PostgresqlSimple
-import Snap.Snaplet.PostgresqlSimple (pgsDefaultConfig, pgsInit')
+import Snap.Snaplet.PostgresqlSimple (pgsInit)
 import Snap.Snaplet.Session.Backends.CookieSession
 import Snap.Util.FileServe ( serveFile
                            , simpleDirectoryConfig
@@ -193,8 +193,7 @@ appInit = makeSnaplet "app" "Forms application" Nothing $ do
        initCookieSessionManager sesKey "_session" Nothing
 
   -- DB
-  ad <- nestSnaplet "db" db $ pgsInit' $ pgsDefaultConfig
-        "host=localhost port=5432 dbname=carma user=carma_db_sync password=pass"
+  ad <- nestSnaplet "db" db $ pgsInit
 
   authMgr <- nestSnaplet "auth" auth $ initPostgresAuth session ad
 