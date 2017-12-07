86974f67b36a8b731868c8b903edb7eca124989f
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2017-12-07 23:14:26 +0500

    change 'fax' to 'phone number for sms' on 'partner' screen #2865

--- carma-models/src/Carma/Model/Partner.hs
+++ carma-models/src/Carma/Model/Partner.hs
@@ -28,24 +28,45 @@ data Partner = Partner
   , isMobile :: F Bool         "isMobile" "Мобильный партнёр"
   , isFree   :: F Bool         "isFree"   "Свободен"
   , name     :: F Text         "name"     "Название"
-  , synonyms :: F (Vector Text)"synonyms" "Синонимы"
   , code     :: F (Maybe Text) "code"     "Код"
-  , city     :: F (Maybe (IdentI City)) "city" "Город"
-  , makes    :: F (Vector (IdentI CarMake)) "makes" "Обслуживаемые марки"
+  , addrs    :: F A.Value      "addrs"    "Адреса"
+  , emails   :: F A.Value      "emails"   "E-mail"
   , services :: F A.Value      "services" "Услуги"
   , phones   :: F A.Value      "phones"   "Телефоны"
-  , coords   :: F (Maybe Coords)"coords"  "Координаты фактического адреса"
-  , addrs    :: F A.Value       "addrs"   "Адреса"
-  , emails   :: F A.Value       "emails"  "E-mail"
+
+  , synonyms :: F (Vector Text)
+                "synonyms"
+                "Синонимы"
+
+  , city     :: F (Maybe (IdentI City))
+                "city"
+                "Город"
+
+  , makes    :: F (Vector (IdentI CarMake))
+                "makes"
+                "Обслуживаемые марки"
+
+  , coords   :: F (Maybe Coords)
+                "coords"
+                "Координаты фактического адреса"
+
   , personInCharge
              :: F (Maybe Text) "personInCharge" "Ответственное лицо"
+
   , taxScheme
-             :: F (Maybe (IdentI TaxScheme)) "taxScheme" "Форма налогообложения"
+             :: F (Maybe (IdentI TaxScheme))
+                "taxScheme"
+                "Форма налогообложения"
+
   , isPayBackConfirmed
-             :: F Bool         "isPayBackConfirmed" "Соглашение о вознаграждении"
+             :: F Bool
+                "isPayBackConfirmed"
+                "Соглашение о вознаграждении"
+
   , foreignIdent
              :: F (Maybe Text) "foreignIdent" "Внешний код партнёра"
-  , mtime    :: F UTCTime      "mtime" ""
+
+  , mtime    :: F UTCTime      "mtime"   ""
   , comment  :: F Text         "comment" "Комментарий"
   }
   deriving Typeable
@@ -55,40 +76,40 @@ instance Model Partner where
   modelInfo = mkModelInfo Partner ident
   modelView = \case
     "" -> Just $ modifyView defaultView
-      [required makes
-      ,setMeta "widget"           "partner_services" services
+      [ required makes
+      , setMeta "widget"           "partner_services" services
 
-      ,setMeta "noteLabel"        "Время работы"     phones
-      ,setMeta "showNote"         (A.Bool True)      phones
-      ,regexp regexpPhone                            phones
-      ,setMeta "addLabel"         "Добавить телефон и время работы"
-                                                     phones
-      ,setMeta "jsonSchema"       "dict-objects"     phones
-      ,setMeta "dictionaryName"   "PhoneTypes"       phones
-      ,setMeta "widget"           "dict-objects"     phones
+      , setMeta "noteLabel"        "Время работы"     phones
+      , setMeta "showNote"         (A.Bool True)      phones
+      , regexp regexpPhone                            phones
+      , setMeta "addLabel"         "Добавить телефон и время работы"
+                                                      phones
+      , setMeta "jsonSchema"       "dict-objects"     phones
+      , setMeta "dictionaryName"   "PhoneTypes"       phones
+      , setMeta "widget"           "dict-objects"     phones
 
-      ,setMeta "widget"           "picker"           coords
-      ,setMeta "cityField"        "city"             coords
-      ,setMeta "infoText"         "coords"           coords
-      ,setMeta "required"         (A.Bool True)      coords
-      ,setMeta "targetCoords"     "coords"           coords
-      ,setMeta "targetAddr"       "factAddr"         coords
-      ,setMeta "picker"           "mapPicker"        coords
-      ,setMeta "currentBlipType"  "partner"          coords
+      , setMeta "widget"           "picker"           coords
+      , setMeta "cityField"        "city"             coords
+      , setMeta "infoText"         "coords"           coords
+      , setMeta "required"         (A.Bool True)      coords
+      , setMeta "targetCoords"     "coords"           coords
+      , setMeta "targetAddr"       "factAddr"         coords
+      , setMeta "picker"           "mapPicker"        coords
+      , setMeta "currentBlipType"  "partner"          coords
 
-      ,setMeta "addLabel"         "Добавить адрес"   addrs
-      ,setMeta "jsonSchema"       "dict-objects"     addrs
-      ,setMeta "dictionaryName"   "AddressTypes"     addrs
-      ,setMeta "widget"           "dict-objects"     addrs
+      , setMeta "addLabel"         "Добавить адрес"   addrs
+      , setMeta "jsonSchema"       "dict-objects"     addrs
+      , setMeta "dictionaryName"   "AddressTypes"     addrs
+      , setMeta "widget"           "dict-objects"     addrs
 
-      ,regexp regexpEmail                            emails
-      ,setMeta "addLabel"         "Добавить e-mail"  emails
-      ,setMeta "jsonSchema"       "dict-objects"     emails
-      ,setMeta "dictionaryName"   "EmailTypes"       emails
-      ,setMeta "widget"           "dict-objects"     emails
+      , regexp regexpEmail                            emails
+      , setMeta "addLabel"         "Добавить e-mail"  emails
+      , setMeta "jsonSchema"       "dict-objects"     emails
+      , setMeta "dictionaryName"   "EmailTypes"       emails
+      , setMeta "widget"           "dict-objects"     emails
 
-      ,textarea comment
-      ,invisible mtime
+      , textarea comment
+      , invisible mtime
       ]
     _  -> Nothing
 
--- srv/resources/assets/script/screens/partners.coffee
+++ srv/resources/assets/script/screens/partners.coffee
@@ -10,56 +10,56 @@ template = require "carma-tpl/screens/partner.pug"
 modelSetup = (modelName, viewName, args) ->
   permEl = "#{modelName}-permissions"
   focusClass = "focusable"
-  refs = [field: "services"
-         ,forest: "partner-services-references"
-         ]
+  refs = [{field: "services", forest: "partner-services-references"}]
   slotsee = ["map-address"]
   options = {permEl, focusClass, refs, slotsee}
   main.modelSetup(modelName) viewName, args, options
 
 objsToRows = (objs) ->
   cities = utils.newModelDict "City", true
-  rows = for obj in objs
-    [obj.id
-    ,obj.name       || ''
-    ,(cities.getLab obj.city) || ''
-    ,obj.comment    || ''
-    ]
+  rows = for obj in objs then [
+    obj.id
+    obj.name                 or ''
+    (cities.getLab obj.city) or ''
+    obj.comment              or ''
+  ]
 
 screenSetup = (viewName, args) ->
   modelName = "Partner"
   kvm = modelSetup modelName, viewName, args
 
   tableParams =
-    tableName: "partner"
-    objURL: "/_/Partner?limit=5000"
+    tableName : "partner"
+    objURL    : "/_/Partner?limit=5000"
 
-  table = screenMan.addScreen(modelName, -> )
+  table = screenMan
+    .addScreen(modelName, ->)
     .addTable(tableParams)
     .setObjsToRowsConverter(objsToRows)
   table
-    .on("click.datatable", "tr", ->
-      if (table.dataTable.fnGetPosition this) != null
+    .on "click.datatable", "tr", ->
+      if (table.dataTable.fnGetPosition this) isnt null
         id = @children[0].innerText
         modelSetup modelName, viewName, {id}
-    )
+
   screenMan.showScreen modelName
 
   $('#partner-permissions').find('.btn-success').on 'click', ->
+    addrDeFacto = _.filter kvm["addrsObjects"](), (svm) -> svm.key() is "fact"
+
     obj =
-      addrDeFacto:
-            _.filter(kvm["addrsObjects"](),
-                    (svm) -> svm.key() == "fact")[0]?.value()
-      city: kvm.city()
-      comment: kvm.comment()
-      id: kvm.id()
-      isDealer: kvm.isDealer()
-      isMobile: kvm.isMobile()
-      name: kvm.name()
+      addrDeFacto : addrDeFacto[0]?.value()
+      city        : kvm.city()
+      comment     : kvm.comment()
+      id          : kvm.id()
+      isDealer    : kvm.isDealer()
+      isMobile    : kvm.isMobile()
+      name        : kvm.name()
+
     table.dataTable.fnAddData objsToRows [obj]
 
 
-module.exports =
-  { constructor: screenSetup
-  , template
-  }
+module.exports = {
+  constructor: screenSetup
+  template
+}
