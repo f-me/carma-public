7f543263af6b4bf3355eab3f7a4c094d1cf1cc73
Dmitry Dzhus <dima@dzhus.org>
2017-12-20 15:36:42 +0000

    refactor: Delete unused LegacyModel.recode

--- srv/src/Utils/LegacyModel.hs
+++ srv/src/Utils/LegacyModel.hs
@@ -4,7 +4,6 @@ module Utils.LegacyModel where
 import           Data.Maybe
 import           Data.Text (Text)
 import qualified Data.Text as T
-import           Data.Aeson
 import           Data.Model
 
 import           Util
@@ -12,11 +11,6 @@ import           Util
 
 readIdent :: Model m => Text -> IdentI m
 readIdent s = fromMaybe (error "readIdent: no integer") $ fvIdent s
-
-recode :: (FromJSON t, ToJSON f) => f -> t
-recode o = case decode $ encode o of
-             Just v -> v
-             Nothing -> error "recode: JSON conversion failed"
 
 mkIdentTopic :: forall m.Model m => IdentI m -> Text
 mkIdentTopic idt = T.concat [mdl, ":", T.pack $ show $ identVal idt]
