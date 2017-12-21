45cc37b541d0566f432657f5b8b34d4c694b6d11
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2017-12-21 18:17:01 +0500

    separated sms status label getting to own function #2867
    to be able to use it later in case-history view

--- database/baseline/5-views/10-sms-report.sql
+++ database/baseline/5-views/10-sms-report.sql
@@ -3,24 +3,15 @@ BEGIN;
 DROP VIEW IF EXISTS "SMS";
 
 CREATE VIEW "SMS" AS
-  SELECT
-    s.ctime::DATE AS "Дата Отправки",
-    s.ctime::TIME AS "Время Отправки",
-
-    CASE
-      WHEN s.status = 'sent'::TEXT        THEN 'Отправлено'::TEXT
-      WHEN s.status = 'please-send'::TEXT THEN 'В очереди на отправку'::TEXT
-      WHEN s.status = 'draft'::TEXT       THEN 'Черновик'::TEXT
-      WHEN s.status = 'error'::TEXT       THEN 'Ошибка'::TEXT
-      WHEN s.status = 'processing'::TEXT  THEN 'Отправляется'::TEXT
-    END AS "Статус",
-
-    s.phone                       AS "Номер телефона",
-    s.sender                      AS "Отправитель",
-    t.label                       AS "Шаблон",
-    s.msgtext                     AS "Текст",
-    COALESCE(s.caseref::TEXT, '') AS "Номер Кейса",
-    u.login                       AS "Пользователь, отправивший СМС"
+  SELECT s.ctime::DATE                 AS "Дата Отправки"
+       , s.ctime::TIME                 AS "Время Отправки"
+       , GetSmsStatusLabel(s.status)   AS "Статус"
+       , s.phone                       AS "Номер телефона"
+       , s.sender                      AS "Отправитель"
+       , t.label                       AS "Шаблон"
+       , s.msgtext                     AS "Текст"
+       , COALESCE(s.caseref::TEXT, '') AS "Номер Кейса"
+       , u.login                       AS "Пользователь, отправивший СМС"
 
   FROM "Sms" s
     LEFT JOIN "SmsTemplate" t ON s.template = t.id
--- database/patches/1.344.3-update-views-for-sms.sh
+++ database/patches/1.344.3-update-views-for-sms.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+$PSQL -f baseline/2-functions/5-get-sms-status-label.sql
 $PSQL -f baseline/5-views/10-sms-report.sql
