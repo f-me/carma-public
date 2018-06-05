64d83ea0cc38dff3540243d187032c50fdd98307
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2018-06-06 02:28:49 +0500

    feature/era-glonass-integration:
    prototype of `carma-era-glonass-integration` package

---
+++ carma-era-glonass-integration/README.md
@@ -0,0 +1,19 @@
+# carma-era-glonass-integration *microservice*
+
+Integration between CaRMa and Era Glonass.
+
+## What is Ega Glonass?
+
+Wikipedia article (russian one, there's no english version for now):
+- https://ru.wikipedia.org/wiki/%D0%AD%D0%A0%D0%90-%D0%93%D0%9B%D0%9E%D0%9D%D0%90%D0%A1%D0%A1
+
+It's kinda like russian version of the ECall system:
+- https://ru.wikipedia.org/wiki/ECall
+
+Short and simplified description is:
+
+A system that is integrated to a car to provide rapid emergency assistance.
+
+## TODO
+
+- Add more description to this document
---
+++ carma-era-glonass-integration/src/Carma/EraGlonass/Routes.hs
@@ -0,0 +1,46 @@
+{-# LANGUAGE ExplicitNamespaces #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DataKinds #-}
+
+module Carma.EraGlonass.Routes
+     ( type IncomingRoutes
+     , type OutcomingRoutes
+     ) where
+
+import           Servant
+
+
+-- Routes which CaRMa provides for outside requests by Era Glonass.
+type IncomingRoutes
+   = -- Integration point with code "EG.CRM.01".
+     --
+     -- Era Glonass supposed to upload data about its "call card" to this route.
+     -- Route path written this way to look just as their route used for working
+     -- with "call cards".
+     --
+     -- POST /calls/status
+     --
+     "calls" :> "status" :> Post '[JSON] ()
+
+
+type OutcomingRoutes
+    =  -- Integration point with code "CRM.EG.02".
+       --
+       -- Uploading CaRMa contracts to Era Glonass
+       -- (ones which marked as taking part in integration with Era Glonass).
+       --
+       -- DELETE, PUT, POST /providers/vehicles
+       "providers" :> "vehicles" :> (    Delete '[JSON] ()
+                                    :<|> Put    '[JSON] ()
+                                    :<|> Post   '[JSON] ()
+                                    )
+
+       -- Integration point with code "CRM.EG.03".
+       --
+       -- Uploading new CaRMa `Case` data
+       -- (for "call card" of Era Glonass service).
+       --
+       -- PUT, POST /calls/status
+  :<|> "calls" :> "status" :> (    Put  '[JSON] ()
+                              :<|> Post '[JSON] ()
+                              )
