3abe27f2838fc1e62936375a5783179a9772f6bc
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2018-06-27 05:30:54 +0500

    feature/era-glonass-integration:
    added prototype of the microservice's config file

--- carma-era-glonass-integration/app/Main.hs
+++ carma-era-glonass-integration/app/Main.hs
@@ -1,8 +1,12 @@
 {-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Main (main) where
 
 import           Data.Function ((&))
+import qualified Data.Configurator as Conf
 import           Data.String (fromString)
 import           Text.InterpolatedString.QM
 
@@ -13,11 +17,13 @@ import           Carma.EraGlonass.Server (serverApplicaton)
 
 main :: IO ()
 main = do
+  cfg <- Conf.load [Conf.Required "app.cfg"]
+
+  !(port :: Warp.Port) <- Conf.require cfg "port"
+  !(host :: String)    <- Conf.lookupDefault "127.0.0.1" cfg "host"
+
   putStrLn [qm| Running incoming server on http://{host}:{port}... |]
   runIncomingServer port $ fromString host
-
-  where port = 8166
-        host = "127.0.0.1"
 
 
 runIncomingServer :: Warp.Port -> Warp.HostPreference -> IO ()
