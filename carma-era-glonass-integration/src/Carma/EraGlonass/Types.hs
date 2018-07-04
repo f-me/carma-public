7e27f1e9503175a662bc07cb529ec033fa4f2a53
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2018-07-05 03:25:39 +0500

    feature/era-glonass-integration:
    progress on implementing EG.CRM.01 request model

--- carma-era-glonass-integration/src/Carma/EraGlonass/Types.hs
+++ carma-era-glonass-integration/src/Carma/EraGlonass/Types.hs
@@ -27,6 +27,7 @@ data AppContext
    }
 
 
+-- TODO restructure looking at JSON example from EG documentation
 data EraGlonassCreateCallCardRequest
    = EraGlonassCreateCallCardRequest
    { requestId :: RequestId
@@ -56,4 +57,50 @@ data EraGlonassCreateCallCardRequest
    , callerFullName :: Text
        -- ^ A string of 50 chars.
        --   CaRMa field: "contact_name"
+
+   , callerPhoneNumber :: Text
+       -- ^ A phone number of a customer (where "atPhoneNumber" could be some
+       --   EG operator's phone I believe, TODO need to clarify this).
+       --   A string from 1 to 18 chars of numbers which could be prefixed with
+       --   '+'.
+       --   CaRMa field: "contact_phone1"
+
+   , locationDescription :: Text
+       -- ^ String with max length of 180 symbols.
+       --   CaRMa field: "caseAddress_comment"
+
+   , vin :: Text
+       -- ^ A car's VIN ("Alphanumeric")
+       --   CaRMa field: "car_vin"
+       --   TODO need to ask why "contractIdentifier" is proposed alongwith
+       --        "car_vin".
+
+   , propulsion :: Text
+       -- ^ Enum of:
+       --     "HYDROGEN"
+       --     "ELECTRICITY"
+       --     "LPG" - Liquefied Petroleum Gas (propane)
+       --     "LNG" - Liquefied Natural Gas
+       --     "DIESEL"
+       --     "GASOLINE"
+       --   CaRMa field: "car_engine"
+       --   TODO extend "Engine" model with missed engine types
+
+   , color :: Text
+       -- ^ A color of a car (string with max length of 50 symbols).
+       --   CaRMa field: "car_color"
+
+   , registrationNumber :: Text
+       --   CaRMa field: "car_plateNum"
+
+   , regionName :: Text
+       --   CaRMa field: "caseAddress_address"
+   , settlementName :: Text
+       --   CaRMa field: "caseAddress_address"
+   , streetName :: Text
+       --   CaRMa field: "caseAddress_address"
+   , building :: Text
+       -- ^ Number of a building as a string.
+       --   CaRMa field: "caseAddress_address"
+
    } deriving (Generic, ToSchema, FromJSON)
