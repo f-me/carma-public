a8a380dbe7c99fd2411f8df59f2a633857e4b4e8
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2018-08-29 06:25:21 +0300

    feature/diag-tree-slide-persistent-model:
    implemented persistent "DiagSlide" model

---
+++ carma-models/src/Carma/Model/DiagSlide/Persistent.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE GADTs, GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE QuasiQuotes, TemplateHaskell #-}
+
+module Carma.Model.DiagSlide.Persistent where
+
+import           Data.Typeable
+import           Data.Text (Text)
+import           Data.Time (UTCTime)
+import           Data.Aeson (Value)
+
+import           Database.Persist.TH
+
+import           Database.Persist.Postgresql.JSON ()
+                 -- ^ @PersistFieldSql Value@ instance
+
+import           Carma.Model.LegacyTypes (Reference)
+
+
+mkPersist sqlSettings [persistLowerCase|
+DiagSlide sql=DiagSlide
+  ctime      UTCTime          sql=ctime
+  header     Text             sql=header
+  body       Text             sql=body
+  resources  Value            sql=resources
+  answers    Value            sql=answers
+  actions    Value            sql=actions
+  isRoot     Bool             sql=isroot
+  isActive   Bool             sql=isactive
+  files      Reference Maybe  sql=files
+  deriving Typeable Show
+|]
