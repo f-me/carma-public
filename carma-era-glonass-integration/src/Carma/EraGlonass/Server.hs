9e6d1605ac44513cc4041a4d9be2b78a780a6e27
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2018-09-16 23:03:19 +0300

    feature/era-glonass-integration:
    separated `Server` module helpers to own module

--- carma-era-glonass-integration/src/Carma/EraGlonass/Server.hs
+++ carma-era-glonass-integration/src/Carma/EraGlonass/Server.hs
@@ -16,26 +16,21 @@ module Carma.EraGlonass.Server
 import           Data.Proxy
 import           Data.Swagger (Swagger)
 import           Text.InterpolatedString.QM
-import           Data.Monoid ((<>))
-import           Data.String (fromString)
 import           Data.Text (Text)
 import           Data.Aeson (toJSON)
 
-import           Control.Monad
 import           Control.Monad.Reader (MonadReader, asks, runReaderT, ReaderT)
 import           Control.Monad.Error.Class (MonadError, throwError, catchError)
-import           Control.Monad.Random.Class (MonadRandom (..))
-import           Control.Exception (SomeException)
+import           Control.Monad.Random.Class (MonadRandom)
 import           Control.Concurrent.STM.TVar
 
 import           Servant
 import           Servant.Swagger (toSwagger)
 
 import           Database.Persist ((==.))
-import           Database.Persist.Sql (SqlBackend, fromSqlKey)
+import           Database.Persist.Sql (fromSqlKey)
 import           Database.Persist.Types
 
-import           Carma.Utils.Operators
 import           Carma.Monad.STM
 import           Carma.Monad.MVar
 import           Carma.Monad.Clock
@@ -54,6 +49,7 @@ import           Carma.EraGlonass.Types
 import           Carma.EraGlonass.Model.CaseEraGlonassFailure.Types
 import           Carma.EraGlonass.Model.CaseEraGlonassFailure.Persistent
 import           Carma.EraGlonass.Model.CaseEraGlonassCreateRequest.Persistent
+import           Carma.EraGlonass.Server.Helpers
 
 
 type FaliuresAPI
@@ -424,58 +420,3 @@ getBackgroundTasksCount = do
   result <- asks backgroundTasksCounter >>= atomically . readTVar
   logDebug [qm| Background tasks count: {result} |]
   pure result
-
-
-inBackground
-  :: ( MonadReader AppContext m
-     , MonadError ServantErr m
-     , MonadThread m
-     , MonadSTM m
-     )
-  => m ()
-  -> m ()
-
-inBackground m = do
-  counter <- asks backgroundTasksCounter
-  atomically $ modifyTVar' counter succ
-  void $ fork $ do
-    m `catchError` \_ -> pure () -- ^ Ignore @ServantErr@ exception
-    atomically $ modifyTVar' counter pred
-
-
-runSqlProtected
-  :: ( MonadReader AppContext m
-     , MonadLoggerBus m
-     , MonadPersistentSql m
-     , MonadError ServantErr m
-     )
-  => Text -- ^ Fail message
-  -> ReaderT SqlBackend m a
-  -> m a
-
-runSqlProtected errMsg =
-  runSqlInTime >=> \case
-    Right x -> pure x
-    Left  e -> do
-      let logMsg = [qmb| Database request is failed: {errMsg}
-                         Exception: {e} |]
-
-      logError [qm| {logMsg} |]
-      throwError err500 { errBody = logMsg }
-
-
-runSqlInTime
-  :: (MonadReader AppContext m, MonadPersistentSql m)
-  => ReaderT SqlBackend m a
-  -> m (Either SomeException a)
-
-runSqlInTime m = asks dbRequestTimeout >>= flip runSqlTimeout m
-
-
-getRandomResponseId :: MonadRandom m => m Text
-getRandomResponseId =
-  getRandomRs (0, pred $ length randomChars)
-    <&> fmap (randomChars !!) ? take responseIdLength ? fromString
-  where
-    randomChars = ['a'..'z'] <> ['0'..'9'] :: String
-    responseIdLength = 50 -- ^ Chars count
---
+++ carma-era-glonass-integration/src/Carma/EraGlonass/Server/Helpers.hs
@@ -0,0 +1,89 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE QuasiQuotes #-}
+
+module Carma.EraGlonass.Server.Helpers
+     ( inBackground
+     , runSqlProtected
+     , runSqlInTime
+     , getRandomResponseId
+     ) where
+
+import           Data.Monoid ((<>))
+import           Data.String (fromString)
+import           Data.Text (Text)
+import           Text.InterpolatedString.QM
+
+import           Control.Monad
+import           Control.Monad.Error.Class (MonadError, throwError, catchError)
+import           Control.Monad.Reader (MonadReader, asks, ReaderT)
+import           Control.Monad.Random.Class (MonadRandom (..))
+import           Control.Exception (SomeException)
+import           Control.Concurrent.STM.TVar
+
+import           Servant
+
+import           Database.Persist.Sql (SqlBackend)
+
+import           Carma.Utils.Operators
+import           Carma.Monad.STM
+import           Carma.Monad.Thread
+import           Carma.Monad.LoggerBus
+import           Carma.Monad.PersistentSql
+import           Carma.EraGlonass.Types
+import           Carma.EraGlonass.Instances ()
+
+
+inBackground
+  :: ( MonadReader AppContext m
+     , MonadError ServantErr m
+     , MonadThread m
+     , MonadSTM m
+     )
+  => m ()
+  -> m ()
+
+inBackground m = do
+  counter <- asks backgroundTasksCounter
+  atomically $ modifyTVar' counter succ
+  void $ fork $ do
+    m `catchError` \_ -> pure () -- ^ Ignore @ServantErr@ exception
+    atomically $ modifyTVar' counter pred
+
+
+runSqlProtected
+  :: ( MonadReader AppContext m
+     , MonadLoggerBus m
+     , MonadPersistentSql m
+     , MonadError ServantErr m
+     )
+  => Text -- ^ Fail message
+  -> ReaderT SqlBackend m a
+  -> m a
+
+runSqlProtected errMsg =
+  runSqlInTime >=> \case
+    Right x -> pure x
+    Left  e -> do
+      let logMsg = [qmb| Database request is failed: {errMsg}
+                         Exception: {e} |]
+
+      logError [qm| {logMsg} |]
+      throwError err500 { errBody = logMsg }
+
+
+runSqlInTime
+  :: (MonadReader AppContext m, MonadPersistentSql m)
+  => ReaderT SqlBackend m a
+  -> m (Either SomeException a)
+
+runSqlInTime m = asks dbRequestTimeout >>= flip runSqlTimeout m
+
+
+getRandomResponseId :: MonadRandom m => m Text
+getRandomResponseId =
+  getRandomRs (0, pred $ length randomChars)
+    <&> fmap (randomChars !!) ? take responseIdLength ? fromString
+  where
+    randomChars = ['a'..'z'] <> ['0'..'9'] :: String
+    responseIdLength = 50 -- ^ Chars count
