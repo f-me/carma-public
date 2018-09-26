261e5a7e11ddf7bc07f00dd0978f660d1fd6b0cd
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2018-09-26 18:51:46 +0300

    feature/era-glonass-integration:
    added comments about each method to `Carma.EraGlonass.Routes` module

--- carma-era-glonass-integration/src/Carma/EraGlonass/Routes.hs
+++ carma-era-glonass-integration/src/Carma/EraGlonass/Routes.hs
@@ -12,18 +12,19 @@ import           Servant
 import           Carma.EraGlonass.Types
 
 
--- | Routes which CaRMa provides for outside requests by Era Glonass.
---
--- * Integration point with code __EG.CRM.01__.
---
---   @POST \/calls/status@
---
---   Era Glonass supposed to upload data about its \"call card" to this
---   route. Route path written this way to look just as their route used for
---   working with \"call cards".
---
---   Triggered at any time (when related action happens) by Era Glonass.
---
+{-|
+Routes which CaRMa provides for outside requests by Era Glonass.
+
+* Integration point with code __EG.CRM.01__.
+
+  @POST \/calls/status@
+
+  Era Glonass supposed to upload data about its \"call card" to this
+  route. Route path written this way to look just as their route used for
+  working with \"call cards".
+
+  Triggered at any time (when related action happens) by Era Glonass.
+-}
 type IncomingAPI
    = -- EG.CRM.01
      -- POST /calls/status
@@ -31,27 +32,36 @@ type IncomingAPI
                          :> Post    '[JSON] EGCreateCallCardResponse
 
 
--- | Routes which CaRMa uses to make requests to Era Glonass.
---
--- * Integration point with code __CRM.EG.02__.
---
---   @DELETE, PUT, POST \/providers/vehicles@
---
---   Uploading CaRMa 'Carma.Model.Contract.Persistent.Contract's to Era Glonass.
---   Only those that are have 'Carma.Model.SubProgram.Persistent.SubProgram'
---   which is __/@eraGlonassParticipant@/__.
---
---   Triggered daily on schedule by CaRMa.
---
--- * Integration point with code __CRM.EG.03__.
---
---   @PUT, POST \/calls/status@
---
---   Uploading new CaRMa 'Carma.Model.Case.Persistent.Case' data
---   (for \"call card" of Era Glonass service).
---
---   Triggered at any time (when related action happens) by CaRMa.
---
+{-|
+Routes which CaRMa uses to make requests to Era Glonass.
+
+* Integration point with code __CRM.EG.02__.
+
+  * @DELETE \/providers/vehicles@ -
+      To delete a VIN from list of VINs handled by CaRMa;
+
+  * @PUT \/providers/vehicles@ -
+      To add a VIN to list of VINs handled by CaRMa;
+
+  * @POST \/providers/vehicles@ -
+      To check if VIN is added and handled by CaRMa.
+
+  Uploading CaRMa 'Carma.Model.Contract.Persistent.Contract's to Era Glonass.
+  Only those that are have 'Carma.Model.SubProgram.Persistent.SubProgram'
+  which is __/@eraGlonassParticipant@/__.
+
+  Triggered daily on schedule by CaRMa.
+
+* Integration point with code __CRM.EG.03__.
+
+  * @PUT \/calls/status@ - To add data about customer, to close a Call Card;
+  * @POST \/calls/status@ - To update status of a Call Card.
+
+  Uploading new CaRMa 'Carma.Model.Case.Persistent.Case' data
+  (for \"call card" of Era Glonass service).
+
+  Triggered at any time (when related action happens) by CaRMa.
+-}
 type OutcomingAPI
     =  -- CRM.EG.02
        -- DELETE, PUT, POST /providers/vehicles
