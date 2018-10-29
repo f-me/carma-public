af6eaab7c0253c22943c069d96c71e4bc533fce1
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2018-10-30 01:24:23 +0300

    feature/era-glonass-integration:
    progress on VIN synchronizer

--- carma-era-glonass-integration/src/Carma/EraGlonass/Model/EraGlonassSynchronizedContract/Persistent.hs
+++ carma-era-glonass-integration/src/Carma/EraGlonass/Model/EraGlonassSynchronizedContract/Persistent.hs
@@ -20,11 +20,17 @@ EraGlonassSynchronizedContract json sql=EraGlonassSynchronizedContract
   ctime UTCTime sql=ctime default=CURRENT_TIME
   contract ContractId sql=contractid
 
-  vin Text sql=vin -- ^ VIN which have been used to synchronize a @Contract@.
-                   -- Could be useful for debugging in case some @Contract@'s
-                   -- data have been changed after a synchronization.
+  vin Text sql=vin
+    -- ^ VIN which have been used to synchronize a @Contract@.
+    -- Could be useful for debugging in case some @Contract@'s
+    -- data have been changed after a synchronization.
 
   isSynchronized Bool sql=issynchronized
+    -- ^ Indicates if a contract (VIN) is handled by CaRMa
+    -- which means Era Glonass service is notified about that.
+    -- @False@ means that a @Contract@ was used to be handled earlier.
+    -- TODO FIXME rename to \"isHandledByCarma"
+
   lastStatusChangeTime UTCTime Maybe sql=laststatuschangetime
 
   deriving Typeable Show
--- carma-era-glonass-integration/src/Carma/EraGlonass/VinSynchronizer.hs
+++ carma-era-glonass-integration/src/Carma/EraGlonass/VinSynchronizer.hs
@@ -1,8 +1,7 @@
-{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE ViewPatterns #-}
-{-# LANGUAGE FlexibleContexts, ConstraintKinds #-}
-{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE ScopedTypeVariables, FlexibleContexts, ConstraintKinds #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE QuasiQuotes, ViewPatterns #-}
 
 -- To add docs for every type or function defined in the module.
 {-# OPTIONS_HADDOCK ignore-exports #-}
@@ -15,25 +14,34 @@ module Carma.EraGlonass.VinSynchronizer
      ) where
 
 import           Data.Function (fix)
+import           Data.Time.Clock (utctDay)
 import           Data.Time.LocalTime (TimeZone, utcToZonedTime)
 import           Data.Time.Format
 import           Data.Text (Text)
 import           Text.InterpolatedString.QM
 import           Text.Printf (printf)
+import           Data.List (intersect)
+import           Data.Maybe (catMaybes)
 
 import           Control.Arrow
 import           Control.Monad
 import           Control.Monad.Reader (MonadReader, ReaderT, asks)
 import           Control.Monad.Catch
+import           Control.Monad.Trans.Class (lift)
 import           Control.Exception (SomeException, fromException)
 
+import           Database.Persist ((==.), (!=.), (>=.), (<-.), (/<-.), (||.))
 import           Database.Persist.Sql (SqlBackend)
+import           Database.Persist.Types (entityVal)
 
 import           Carma.Utils.Operators
 import           Carma.Monad
+import           Carma.Model.SubProgram.Persistent
+import           Carma.Model.Contract.Persistent
 import           Carma.EraGlonass.Instances ()
 import           Carma.EraGlonass.Instance.Persistent (TimeoutException (..))
 import           Carma.EraGlonass.Types
+import           Carma.EraGlonass.Model.EraGlonassSynchronizedContract.Persistent
 
 
 -- | VIN synchronizer monad constraint.
@@ -131,6 +139,81 @@ runVinSynchronizer tz = do
 synchronizeVins :: VinSynchronizerMonad m => ReaderT SqlBackend m ()
 synchronizeVins = do
   logInfo [qn| Synchronizing VINs... |]
+
+  -- Getting list of currently handled VINs to check if some of them
+  -- should be marked for EG service as not handled by CaRMa anymore.
+  handledContracts <-
+    selectList [ EraGlonassSynchronizedContractIsSynchronized ==. True ] []
+
+  -- Getting list of active EG participants @SubProgram@s.
+  egSubPrograms <-
+    selectKeysList [ SubProgramActive                ==. True
+                   , SubProgramEraGlonassParticipant ==. True
+                   ] []
+
+  nowDay <- utctDay <$> lift getCurrentTime
+
+  let handledVINs =
+        handledContracts <&>
+          Just . eraGlonassSynchronizedContractVin . entityVal
+
+  -- Getting list of @Contract@s (VINs actually) which are used to be handled.
+  -- Now we need to notify EG service that they aren't handled by CaRMa anymore.
+  contractsToUnmarkAsHandled <- let
+
+    selectFilter
+      = -- @Contract@'s VIN is handled by CaRMa.
+        ( ContractVin <-. handledVINs )
+
+      : (   -- @Contract@ has been deactivated.
+            [ ContractIsActive ==. False ]
+
+        ||. -- @SubProgram@ of a @Contract@ has been unmarked as EG participant.
+            [ ContractSubprogram /<-. (Just <$> egSubPrograms) ]
+
+        ||. -- Validity date of a @Contract@ has been expired.
+            [ ContractValidUntil !=. Nothing
+            , ContractValidUntil >=. Just nowDay
+            ]
+        )
+
+    in selectList selectFilter []
+
+  -- Searching for incorrect data, when some VIN is handled by CaRMa but no
+  -- longer represented in @Contract@s list.
+  -- Usually it doesn't happen but if for instance you change @vin@ field value
+  -- of a @Contract@ and previous @vin@ have been marked as handled by CaRMa for
+  -- EG service we supposed to notify EG that we don't handle these anymore.
+  ephemeralVINsToUnmarkAsHandled <-
+    selectList [ ContractVin <-. handledVINs ] []
+      <&> fmap (entityVal ? contractVin) ? catMaybes -- Extracting only VINs
+      <&> \contractVINs ->
+            let vinLens = entityVal ? eraGlonassSynchronizedContractVin
+
+                f acc x =
+                  if vinLens x `notElem` contractVINs
+                     then x : acc
+                     else acc
+
+                in foldl f [] handledContracts
+
+  -- Checking for data correctness.
+  unless ( let vinsA =
+                 catMaybes $
+                   contractsToUnmarkAsHandled <&> entityVal ? contractVin
+
+               vinsB =
+                 ephemeralVINsToUnmarkAsHandled <&>
+                   entityVal ? eraGlonassSynchronizedContractVin
+
+               in null $ vinsA `intersect` vinsB ) $
+
+    fail [qns|
+      Ephemeral VINs and outdated/deactivated VINs cannot intersect,
+      something wrong...
+    |]
+
+  logDebug [qn| testing... |]
 
 
 {-|
