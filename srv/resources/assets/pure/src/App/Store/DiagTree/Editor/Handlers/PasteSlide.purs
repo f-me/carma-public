97708cd09d6818b9ce1838bd25f7ee829e910106
Alexander Danilov <alexander.a.danilov@gmail.com>
2018-11-06 13:05:23 +0300

    UI: copy/move operation activate relocated slide on success.

--- srv/resources/assets/pure/src/App/Store/DiagTree/Editor/Handlers/PasteSlide.purs
+++ srv/resources/assets/pure/src/App/Store/DiagTree/Editor/Handlers/PasteSlide.purs
@@ -6,7 +6,13 @@ import Prelude
 import Data.Array (null)
 
 import App.Store (AppContext)
-import App.Store.DiagTree.Editor.Actions (DiagTreeEditorAction(LoadSlidesRequest, PasteSlideSuccess, PasteSlideFailure))
+import App.Store.DiagTree.Editor.Actions
+       ( DiagTreeEditorAction ( LoadSlidesRequest
+                              , PasteSlideSuccess
+                              , PasteSlideFailure
+                              , SelectSlide
+                              )
+       )
 import App.Store.DiagTree.Editor.Handlers.Helpers (errLog, sendAction)
 import App.Store.DiagTree.Editor.Reducers (DiagTreeEditorState)
 import App.Store.DiagTree.Editor.Types (DiagTreeSlideId)
@@ -16,13 +22,14 @@ import Control.Monad.Eff.Console (CONSOLE)
 import Control.Monad.Eff.Exception (message, stack)
 import Control.Monad.Error.Class (catchError)
 import Data.Argonaut.Core as A
-import Data.Foreign (Foreign)
-import Data.Int (toNumber)
-import Data.Maybe (maybe, fromMaybe)
+import Data.Foreign (Foreign, unsafeFromForeign)
+import Data.Int (toNumber, fromNumber)
+import Data.Maybe (Maybe(..), maybe, fromMaybe, fromJust)
 import Data.Tuple (Tuple (Tuple))
 import Data.StrMap as StrMap
 import Network.HTTP.Affjax (AJAX, AffjaxResponse, affjax)
 import Utils.Affjax (postRequest)
+import Partial.Unsafe (unsafePartial)
 
 
 pasteSlide
@@ -35,21 +42,34 @@ pasteSlide
 pasteSlide appCtx state destinationSlidePath = flip catchError handleError $ do
   if null sourceSlidePath
     then act $ PasteSlideFailure sourceSlidePath
-    else do (res :: AffjaxResponse Foreign) <- affjax $ postRequest url jsonRequest
+    else do (newPathResponse :: AffjaxResponse Foreign) <-
+              affjax $ postRequest url jsonRequest
+            let newPathJSON = unsafeFromForeign newPathResponse.response
+            let selectedSlidePath =
+                  case A.toArray newPathJSON of
+                    Just a -> map decodeInt a
+                    Nothing -> destinationSlidePath
             act $ PasteSlideSuccess destinationSlidePath
             act LoadSlidesRequest -- Reload slides again
+            act $ SelectSlide selectedSlidePath
 
   where
     url = if state.copyPasteBuffer.cutting
           then "/diag/slide/move"
           else "/diag/slide/copy"
 
-    jsonRequest = A.fromObject $
-                  StrMap.fromFoldable [ Tuple "source" $ A.fromArray $ map A.fromNumber $ map toNumber sourceSlidePath
-                                      , Tuple "destination" $ A.fromArray $ map A.fromNumber $ map toNumber destinationSlidePath
-                                      ]
+    jsonRequest =
+      A.fromObject $
+      StrMap.fromFoldable [ Tuple "source" $ pathToJSON sourceSlidePath
+                          , Tuple "destination" $ pathToJSON destinationSlidePath
+                          ]
 
     sourceSlidePath = fromMaybe [] state.copyPasteBuffer.branch
+
+    pathToJSON = A.fromArray <<< map A.fromNumber <<< map toNumber
+
+    decodeInt x = unsafePartial $ fromJust $ fromNumber $
+                  unsafePartial $ fromJust $ A.toNumber x
 
     act = sendAction appCtx
 
