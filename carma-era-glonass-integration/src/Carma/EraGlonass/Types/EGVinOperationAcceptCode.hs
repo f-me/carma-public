b73c1aa0afbea8a66fa57272dcd0e0c403fbc009
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2018-11-08 02:00:36 +0300

    feature/era-glonass-integration:
    fixed `EGVinOperationAcceptCode` and `EGVinOperationFailureAcceptCode`
    `ToSchema` instances

--- carma-era-glonass-integration/src/Carma/EraGlonass/Types/EGVinOperationAcceptCode.hs
+++ carma-era-glonass-integration/src/Carma/EraGlonass/Types/EGVinOperationAcceptCode.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE OverloadedStrings, DuplicateRecordFields #-}
-{-# LANGUAGE DeriveGeneric, DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric, FlexibleContexts  #-}
+{-# LANGUAGE QuasiQuotes, LambdaCase #-}
 
 -- | Type of the "acceptCode" field for Call Card operations requests.
 module Carma.EraGlonass.Types.EGVinOperationAcceptCode
@@ -7,18 +8,24 @@ module Carma.EraGlonass.Types.EGVinOperationAcceptCode
      , EGVinOperationFailureAcceptCode (..)
      ) where
 
-import           GHC.Generics (Generic)
+import           GHC.Generics (Generic, Rep)
 
+import           Text.InterpolatedString.QM
+import           Data.Proxy
+import           Data.Swagger.Declare
+import           Data.Swagger.Internal.Schema
 import           Data.Aeson
 import           Data.Aeson.Types (typeMismatch)
 import           Data.Swagger
+
+import           Carma.Utils.Operators
 
 
 data EGVinOperationAcceptCode
    = OK
    | IncorrectFormat
    | VinNotFound
-     deriving (Eq, Enum, Bounded, Show, Generic, ToSchema)
+     deriving (Eq, Enum, Bounded, Show, Generic)
 
 instance FromJSON EGVinOperationAcceptCode where
   -- Producing list of all values to reduce human-factor mistakes,
@@ -33,6 +40,9 @@ instance ToJSON EGVinOperationAcceptCode where
   toJSON IncorrectFormat = String "INCORRECT_FORMAT"
   toJSON VinNotFound     = String "VIN_NOT_FOUND"
 
+instance ToSchema EGVinOperationAcceptCode where
+  declareNamedSchema = stringyEnumNamedSchema
+
 
 -- | An additional type for "EGVinOperationAcceptCode" when @OK@ is excluded.
 --
@@ -40,7 +50,7 @@ instance ToJSON EGVinOperationAcceptCode where
 data EGVinOperationFailureAcceptCode
    = FailureIncorrectFormat
    | FailureVinNotFound
-     deriving (Eq, Enum, Bounded, Show, Generic, ToSchema)
+     deriving (Eq, Enum, Bounded, Show, Generic)
 
 instance FromJSON EGVinOperationFailureAcceptCode where
   -- Producing list of all values to reduce human-factor mistakes,
@@ -54,3 +64,38 @@ instance FromJSON EGVinOperationFailureAcceptCode where
 instance ToJSON EGVinOperationFailureAcceptCode where
   toJSON FailureIncorrectFormat = toJSON IncorrectFormat
   toJSON FailureVinNotFound     = toJSON VinNotFound
+
+instance ToSchema EGVinOperationFailureAcceptCode where
+  declareNamedSchema = stringyEnumNamedSchema
+
+
+stringyEnumNamedSchema
+  :: (GToSchema (Rep a), ToJSON a, Enum a, Bounded a)
+  => proxy a
+  -> Declare (Definitions Schema) NamedSchema
+
+stringyEnumNamedSchema p = do
+  NamedSchema name' schema' <-
+    gdeclareNamedSchema defaultSchemaOptions (repProxy p) mempty
+
+  pure $ NamedSchema name' schema'
+    { _schemaParamSchema = (_schemaParamSchema schema')
+        { _paramSchemaType = SwaggerString
+        , _paramSchemaEnum = Just $ wholeEnum p
+        }
+    }
+
+  where
+    repProxy :: proxy p -> Proxy (Rep p)
+    repProxy _ = Proxy
+
+    wholeEnum :: (ToJSON a, Enum a, Bounded a) => proxy a -> [Value]
+    wholeEnum p' =
+       enum' p' <&> toJSON <&> \case
+         y@(String _) -> y
+         _ -> error [qms| "stringyEnumNamedSchema":
+                          Every constructor must be resolved to
+                          "String" by using "toJSON" |]
+
+      where enum' :: (Enum a, Bounded a) => proxy a -> [a]
+            enum' _ = [minBound..maxBound]
