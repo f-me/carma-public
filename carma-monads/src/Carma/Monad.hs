0f12ee184a5f86e15a045a8f88ae8041e8b047ac
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2018-12-08 03:55:18 +0300

    feature/era-glonass-integration-r2:
    `carma-monads`: implemented `MonadConcurrently` based on `lifted-async`

--- carma-monads/src/Carma/Monad.hs
+++ carma-monads/src/Carma/Monad.hs
@@ -2,6 +2,7 @@ module Carma.Monad
      ( module Carma.Monad.IORefWithCounter
      , module Carma.Monad.ServantClient
      , module Carma.Monad.PersistentSql
+     , module Carma.Monad.Concurrently
      , module Carma.Monad.LoggerBus
      , module Carma.Monad.Thread
      , module Carma.Monad.Clock
@@ -14,6 +15,7 @@ module Carma.Monad
 import           Carma.Monad.IORefWithCounter
 import           Carma.Monad.ServantClient
 import           Carma.Monad.PersistentSql
+import           Carma.Monad.Concurrently
 import           Carma.Monad.LoggerBus
 import           Carma.Monad.Thread
 import           Carma.Monad.Clock
