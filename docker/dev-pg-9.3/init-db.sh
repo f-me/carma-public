4436f83a2db7a5b37e7683d9bf67c961f395f009
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2018-12-14 05:19:39 +0300

    docker/dev-pg-9.3: fixed postgresql config
    patching locales and `datestyle`

--- docker/dev-pg-9.3/init-db.sh
+++ docker/dev-pg-9.3/init-db.sh
@@ -2,13 +2,13 @@
 set -e
 
 initdb -D "$PGDATA"
-perl -p -i -e 's/^unix_socket_directories = .*\n//' "$PGDATA/postgresql.conf"
-echo $'unix_socket_directories = \'/tmp\'' >> "$PGDATA/postgresql.conf"
+/patch-pg-config.sh
 postgres -D "$PGDATA" -h 127.0.0.1 -p 5432 &
 PGPID=$!
 sleep 2
 
 echo $'
+BEGIN;
 
 ALTER  USER postgres      WITH           PASSWORD \'pass\';
 CREATE USER carma         WITH SUPERUSER PASSWORD \'pass\';
@@ -21,6 +21,7 @@ CREATE USER fmuser;
 CREATE USER "pavel.golovnin";
 CREATE USER reportgen;
 
+COMMIT;
 ' | psql -h 127.0.0.1
 
 echo 'CREATE DATABASE carma;' | psql -h 127.0.0.1 -U carma_db_sync -d postgres
