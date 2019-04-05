e48db5cf9913a68cdfe1b063e792a7cccff0502f
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2019-04-06 00:41:35 +0300

    "pure" frontend: store: abstracted reducer thread failure handler

--- srv/resources/assets/pure/src/ApplicationInit.purs
+++ srv/resources/assets/pure/src/ApplicationInit.purs
@@ -4,12 +4,16 @@ import Prelude
 
 import Data.Maybe (Maybe (..))
 
+import Control.Monad.Error.Class (throwError)
+
 import Effect (Effect)
 import Effect.Exception.Unsafe (unsafeThrow)
 import Effect.Aff (launchAff_)
+import Effect.Exception (message)
+import Effect.Console (error)
 
 import Web.HTML (window)
-import Web.HTML.Window (document)
+import Web.HTML.Window (document, alert)
 import Web.HTML.HTMLDocument (toDocument)
 import Web.DOM.NonElementParentNode (getElementById)
 import Web.DOM.Document (toNonElementParentNode)
@@ -39,9 +43,19 @@ runApplication = do
              Nothing -> unsafeThrow "#app element not found"
              Just el -> pure el
 
-  store <- createStore appReducer appInitialState
+  store <- createStore reducerThreadFailureHandler appReducer appInitialState
   initRouter $ launchAff_ <<< dispatch store <<< Navigate
   launchAff_ $ subscribeHandlers store
   void $ flip render appDOMEl $ appEl { store }
 
-  where appEl = createLeafElement app
+  where
+    appEl = createLeafElement app
+
+    reducerThreadFailureHandler err = do
+      error $ "Store reducer thread is failed with exception: " <> message err
+
+      window >>= alert
+        "Что-то пошло не так! Настоятельно рекомендуется перезагрузить\
+        \ страницу для продолжения нормальной работы системы!"
+
+      throwError err
